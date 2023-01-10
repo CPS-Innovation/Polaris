6d9dbc90d3672b60493ba34a2137e859a7fa3ce1
Stef Stachow <stef@stachow.co.uk>
2023-01-10 17:45:14 +0000

    #17647 - split urns; #17645,#17646 - empty doc types

--- polaris-gateway/polaris-gateway/Domain/CaseData/Args/UrnArg.cs
+++ polaris-gateway/polaris-gateway/Domain/CaseData/Args/UrnArg.cs
@@ -4,13 +4,5 @@ namespace PolarisGateway.Domain.CaseData.Args
     public class UrnArg : BaseCaseDataArg
     {
         public string Urn { get; set; }
-
-        public string UrlSafeUrn
-        {
-            get
-            {
-                return WebUtility.UrlEncode(Urn);
-            }
-        }
     }
 }
