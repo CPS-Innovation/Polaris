4dd9e32248bcb5dcd691e4dd8dba88b7b131bcb8
Stef Stachow <stef@stachow.co.uk>
2023-01-31 10:26:19 +0000

    proxy tweak

--- polaris-terraform/ui-terraform/nginx.js
+++ polaris-terraform/ui-terraform/nginx.js
@@ -1,8 +1,8 @@
-async function fetch (r) {
-  const endpoint = process.env.API_ENDPOINT ?? ''
-  const cookie = encodeURIComponent(r.headersIn.Cookie ?? '')
-  const referer = encodeURIComponent(r.headersIn.Referer ?? '')
-  const q = r.args.q ?? ''
+async function fetch(r) {
+  const endpoint = process.env.API_ENDPOINT ?? ""
+  const cookie = encodeURIComponent(r.headersIn.Cookie ?? "")
+  const referer = encodeURIComponent(r.headersIn.Referer ?? "")
+  const q = encodeURIComponent(r.args.q ?? "")
   r.return(
     302,
     `https://${endpoint}/init?cookie=${cookie}&referer=${referer}&q=${q}`
