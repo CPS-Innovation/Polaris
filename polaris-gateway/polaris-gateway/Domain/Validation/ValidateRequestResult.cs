c61e1cd94a4aa2a06743b4a6bed5a034d26ae09d
Stef Stachow <stef@stachow.co.uk>
2023-02-02 14:12:51 +0000

    tidyup

--- polaris-gateway/polaris-gateway/Domain/Validation/ValidateRequestResult.cs
+++ polaris-gateway/polaris-gateway/Domain/Validation/ValidateRequestResult.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 
 namespace PolarisGateway.Domain.Validation;
 
-public class ValidateRequestResult : ValidateAuthRequestResult
+public class ValidateRequestResult
 {
     public Guid CurrentCorrelationId { get; set; }
 
     public string CmsAuthValues { get; set; }
+
+    public IActionResult InvalidResponseResult { get; set; }
+
+    public StringValues AccessTokenValue { get; set; }
 }
