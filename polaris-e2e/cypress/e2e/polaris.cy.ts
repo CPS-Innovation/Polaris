20c47a5c80c7dc9d19d65207cc565887cdcf261e
Stef Stachow <stef@stachow.co.uk>
2023-02-06 21:04:31 +0000

    #18174 - build in CMS auth to cypress tests

--- polaris-e2e/cypress/e2e/polaris.cy.ts
+++ polaris-e2e/cypress/e2e/polaris.cy.ts
@@ -1,43 +1,45 @@
 /// <reference types="cypress" />
 
 const {
-    USERNAME,
-    TARGET_URN,
-    TARGET_DEFENDANT_NAME,
-    TARGET_DOCUMENT_ID,
-    TARGET_DOCUMENT_NAME,
-    TARGET_DOCUMENT_TEXT_FRAGMENT,
-    TARGET_SEARCH_TEXT,
-} = Cypress.env();
+  AD_USERNAME,
+  TARGET_URN,
+  TARGET_DEFENDANT_NAME,
+  TARGET_DOCUMENT_ID,
+  TARGET_DOCUMENT_NAME,
+  TARGET_DOCUMENT_TEXT_FRAGMENT,
+  TARGET_SEARCH_TEXT,
+} = Cypress.env()
 
 describe("Polaris", () => {
-    it("can view a case", () => {
-        // have we logged in OK?
-        cy.login().contains(USERNAME);
+  it("can view a case", () => {
+    cy.on("uncaught:exception", () => false)
+    // have we logged in OK?
+    cy.loginToAD()
+      .visit(`/case-search-results?urn=${TARGET_URN}`)
+      .contains("CMS_AUTH_ERROR")
+      .loginToCms()
 
-        // search for our URN
-        cy.findByTestId("input-search-urn").type(`${TARGET_URN}{enter}`);
+    cy.visit("/")
+    cy.findByTestId("input-search-urn").type(`${TARGET_URN}{enter}`)
 
-        // open case details page
-        cy.findByTestId(`link-${Cypress.env().TARGET_URN}`).click();
+    // // open case details page
+    cy.findByTestId(`link-${TARGET_URN}`).click()
 
-        // is our defendant correct
-        cy.findByTestId("txt-defendant-name").contains(TARGET_DEFENDANT_NAME);
+    // // is our defendant correct
+    cy.findByTestId("txt-defendant-name").contains(TARGET_DEFENDANT_NAME)
 
-        // open our target document
-        cy.findByTestId("btn-accordion-open-close-all").click();
-        cy.findByTestId(`link-document-${TARGET_DOCUMENT_ID}`).click();
+    // open our target document
+    cy.findByTestId("btn-accordion-open-close-all").click()
+    cy.findByTestId(`link-document-${TARGET_DOCUMENT_ID}`).click()
 
-        cy.findByTestId("btn-open-pdf").contains(TARGET_DOCUMENT_NAME);
-        cy.get("span").contains(TARGET_DOCUMENT_TEXT_FRAGMENT);
+    cy.findByTestId("btn-open-pdf").contains(TARGET_DOCUMENT_NAME)
+    cy.get("span").contains(TARGET_DOCUMENT_TEXT_FRAGMENT)
 
-        cy.findByTestId("input-search-case").type(
-            `${TARGET_SEARCH_TEXT}{enter}`
-        );
-        cy.findByTestId(`link-result-document-${TARGET_DOCUMENT_ID}`).click();
+    cy.findByTestId("input-search-case").type(`${TARGET_SEARCH_TEXT}{enter}`)
+    cy.findByTestId(`link-result-document-${TARGET_DOCUMENT_ID}`).click()
 
-        cy.findByTestId("tabs").contains(
-            `1 match for "${TARGET_SEARCH_TEXT}" in ${TARGET_DOCUMENT_NAME}`
-        );
-    });
-});
+    cy.findByTestId("tabs").contains(
+      `1 match for "${TARGET_SEARCH_TEXT}" in ${TARGET_DOCUMENT_NAME}`
+    )
+  })
+})
