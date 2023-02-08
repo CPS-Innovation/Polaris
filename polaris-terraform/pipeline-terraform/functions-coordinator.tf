5a2d5880ed1bb68d61e02a1276c9c8f863e0e9f8
aceltuk <mark.jones3@cps.gov.uk>
2023-02-08 11:06:55 +0000

    Corrected DNS entries for SCM records

--- polaris-terraform/pipeline-terraform/functions-coordinator.tf
+++ polaris-terraform/pipeline-terraform/functions-coordinator.tf
@@ -228,33 +228,12 @@ resource "azurerm_private_dns_a_record" "pipeline_coordinator_dns_a" {
   tags                = local.common_tags
 }
 
-# Create a second Private Endpoint to point to the SCM for deployments
-resource "azurerm_private_endpoint" "pipeline_coordinator_scm_pe" {
-  name                = "${azurerm_linux_function_app.fa_coordinator.name}-scm-pe"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  subnet_id           = data.azurerm_subnet.polaris_apps_subnet.id
-  tags                = local.common_tags
-
-  private_dns_zone_group {
-    name                 = data.azurerm_private_dns_zone.dns_zone_apps.name
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_apps.id]
-  }
-
-  private_service_connection {
-    name                           = "${azurerm_linux_function_app.fa_coordinator.name}-scm-psc"
-    private_connection_resource_id = azurerm_linux_function_app.fa_coordinator.id
-    is_manual_connection           = false
-    subresource_names              = ["sites"]
-  }
-}
-
-# Create DNS A to match for SCM record
+# Create DNS A to match for SCM record for SCM deployments
 resource "azurerm_private_dns_a_record" "pipeline_coordinator_scm_dns_a" {
   name                = "${azurerm_linux_function_app.fa_coordinator.name}.scm"
   zone_name           = data.azurerm_private_dns_zone.dns_zone_apps.name
   resource_group_name = "rg-${var.networking_resource_name_suffix}"
   ttl                 = 300
-  records             = [azurerm_private_endpoint.pipeline_coordinator_scm_pe.private_service_connection.0.private_ip_address]
+  records             = [azurerm_private_endpoint.pipeline_coordinator_pe.private_service_connection.0.private_ip_address]
   tags                = local.common_tags
 }
