cda9e643930bf2925c7982e2f4b461e4f36e5750
Stef Stachow <stef@stachow.co.uk>
2023-02-14 14:20:58 +0000

    temporarily serialize PolarisDocumentId as documentId in gateway tracker response

--- polaris-gateway/polaris-gateway/Domain/PolarisPipeline/TrackerDocument.cs
+++ polaris-gateway/polaris-gateway/Domain/PolarisPipeline/TrackerDocument.cs
@@ -6,7 +6,7 @@ namespace PolarisGateway.Domain.PolarisPipeline
 {
     public class TrackerDocument
     {
-        [JsonProperty("polarisDocumentId")]
+        [JsonProperty("documentId")]
         public Guid PolarisDocumentId { get; set; }
 
         [JsonProperty("cmsDocumentId")]
