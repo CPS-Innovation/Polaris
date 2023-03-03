cb39e6750044cbcfd4d16e7c6e35663808c60dd3
Stef Stachow <stef@stachow.co.uk>
2023-03-03 14:08:53 +0000

    added role assignments

--- polaris-terraform/pipeline-events-terraform/storage-account.tf
+++ polaris-terraform/pipeline-events-terraform/storage-account.tf
@@ -33,3 +33,9 @@ resource "azurerm_role_assignment" "ra_blob_data_reader_computer_vision_account"
   role_definition_name = "Storage Blob Data Reader"
   principal_id         = data.azuread_service_principal.computer_vision_sp.object_id
 }
+
+resource "azurerm_role_assignment" "coordinator_blob_data_contributor" {
+  scope                = data.azurerm_storage_container.pipeline_storage_container.resource_manager_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_linux_function_app.fa_coordinator.identity[0].principal_id
+}
