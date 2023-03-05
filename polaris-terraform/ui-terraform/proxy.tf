20149adeb160b258bb93c9f3123bde78694e5a85
aceltuk <mark.jones3@cps.gov.uk>
2023-03-05 15:53:16 +0000

    Upgraded SPA to use latest terraform definition and added a new service plan

--- polaris-terraform/ui-terraform/proxy.tf
+++ polaris-terraform/ui-terraform/proxy.tf
@@ -1,9 +1,10 @@
 resource "azurerm_linux_web_app" "polaris_proxy" {
   name                      = "${local.resource_name}-cmsproxy"
   resource_group_name       = azurerm_resource_group.rg_polaris.name
-  location                  = azurerm_service_plan.asp_polaris.location
+  location                  = azurerm_resource_group.rg_polaris.location
   service_plan_id           = azurerm_service_plan.asp_polaris.id
   virtual_network_subnet_id = data.azurerm_subnet.polaris_proxy_subnet.id
+
   app_settings = {
     "WEBSITE_CONTENTOVERVNET"                  = "1"
     "WEBSITE_DNS_SERVER"                       = var.dns_server
