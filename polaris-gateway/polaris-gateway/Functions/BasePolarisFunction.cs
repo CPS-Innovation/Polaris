24a897031bd8db985ae4143926bdd4c6be625fd9
Stef Stachow <stef@stachow.co.uk>
2023-03-09 14:40:39 +0000

    wip

--- polaris-gateway/polaris-gateway/Functions/BasePolarisFunction.cs
+++ polaris-gateway/polaris-gateway/Functions/BasePolarisFunction.cs
@@ -150,32 +150,5 @@ namespace PolarisGateway.Functions
         {
             _logger.LogMethodFlow(correlationId, loggerSource, message);
         }
-
-        private void RegisterCriticalTelemetry(string userName, Guid correlationId)
-        {
-            Activity activity = Activity.Current;
-            if (activity == null)
-            {
-                throw new CriticalTelemetryException("System.Diagnostics.Activity.Current was expected but found to be null");
-            }
-
-            try
-            {
-                activity.AddTag(TelemetryConstants.UserCustomDimensionName, userName);
-            }
-            catch (Exception exception)
-            {
-                throw new CriticalTelemetryException($"Unable to set {TelemetryConstants.UserCustomDimensionName}", exception);
-            }
-
-            try
-            {
-                activity.AddTag(TelemetryConstants.CorrelationIdCustomDimensionName, correlationId.ToString());
-            }
-            catch (Exception exception)
-            {
-                throw new CriticalTelemetryException($"Unable to set {TelemetryConstants.CorrelationIdCustomDimensionName}", exception);
-            }
-        }
     }
 }
