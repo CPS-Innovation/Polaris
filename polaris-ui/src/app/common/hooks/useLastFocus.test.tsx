26764b40164ba6636896db8adb1475ce616336b5
RENJITH ABBY <renjithabby@renjiths-mbp.home>
2023-03-14 13:27:37 +0000

    added unit test for useLastFocus hook

---
+++ polaris-ui/src/app/common/hooks/useLastFocus.test.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { useLastFocus } from "./useLastFocus";
+
+describe("useLastFocus hook", () => {
+  const testSetUp = () => {
+    const TestComponent = () => {
+      const [showModal, setShowModal] = useState(false);
+      const [showModalOpenButton, setShowModalOpenButton] = useState(true);
+      return (
+        <div>
+          {showModalOpenButton && (
+            <button onClick={() => setShowModal(true)}>Open Modal</button>
+          )}
+          {showModal && (
+            <ModalComponent
+              handleClose={setShowModal}
+              setShowModalOpenButton={setShowModalOpenButton}
+            />
+          )}
+          <button id="default-focus-button">Default Focus</button>
+        </div>
+      );
+    };
+
+    type ModalComponentProps = {
+      handleClose: (value: boolean) => void;
+      setShowModalOpenButton: (value: boolean) => void;
+    };
+    const ModalComponent: React.FC<ModalComponentProps> = ({
+      handleClose,
+      setShowModalOpenButton,
+    }) => {
+      const defaultFocus = document.querySelector(
+        "#default-focus-button"
+      ) as HTMLElement;
+      useLastFocus(defaultFocus);
+
+      return (
+        <div>
+          <button onClick={() => handleClose(false)}>Close Modal</button>
+          <button onClick={() => setShowModalOpenButton(false)}>
+            Hide Open Modal Button
+          </button>
+        </div>
+      );
+    };
+
+    render(<TestComponent />);
+  };
+
+  it("On unmounting the Modal component , should put the focus back to the last active element if it is present in the parent component", () => {
+    testSetUp();
+
+    const openModalButtonElement = screen.getByText("Open Modal");
+    openModalButtonElement.focus();
+    fireEvent.click(openModalButtonElement);
+
+    expect(document.activeElement).toBe(openModalButtonElement);
+    const closeModalButtonElement = screen.getByText("Close Modal");
+    closeModalButtonElement.focus();
+    expect(document.activeElement).toBe(closeModalButtonElement);
+
+    fireEvent.click(closeModalButtonElement);
+    expect(document.activeElement).toBe(openModalButtonElement);
+  });
+
+  it("On unmounting the Modal component, should put the focus back to defaultFocus element if the last active element in not present in the  parent component", () => {
+    testSetUp();
+
+    const openModalButtonElement = screen.getByText("Open Modal");
+    openModalButtonElement.focus();
+    fireEvent.click(openModalButtonElement);
+    expect(document.activeElement).toBe(openModalButtonElement);
+
+    const hideModalButtonElement = screen.getByText("Hide Open Modal Button");
+    hideModalButtonElement.focus();
+    expect(document.activeElement).toBe(hideModalButtonElement);
+
+    fireEvent.click(hideModalButtonElement);
+    const closeModalButtonElement = screen.getByText("Close Modal");
+    closeModalButtonElement.focus();
+    expect(document.activeElement).toBe(closeModalButtonElement);
+    fireEvent.click(closeModalButtonElement);
+
+    const defaultFocus = screen.getByText("Default Focus");
+    expect(document.activeElement).toBe(defaultFocus);
+  });
+});
