46df952a6da9221ac3da97117a5507d1c32020d7
Alastair Pandelus <alastair.pandelus@cps.gov.uk>
2023-03-22 11:39:23 +0000

    removed unused usings

--- polaris-pipeline/coordinator/Functions/ClientFunctions/BaseClientFunction.cs
+++ polaris-pipeline/coordinator/Functions/ClientFunctions/BaseClientFunction.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-using PolarisGateway.Domain.DocumentExtraction;
-using System.Reflection.Metadata;
 using System;
 using System.Net.Http;
 using Common.Constants;
