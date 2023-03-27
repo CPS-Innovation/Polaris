5f68b40960c064737ffcdb4d95e4d5146362d69b
aceltuk <mark.jones3@cps.gov.uk>
2023-03-27 10:52:43 +0100

    Amendments to support resource group lookup for networking events. Customised dedicated networking devops pipelines to publish networking only changes via the new pr, build and release flow.

--- polaris-terraform/networking-events-terraform/resource-group.tf
+++
@@ -1,7 +0,0 @@
-#################### Resource Group ####################
-
-resource "azurerm_resource_group" "rg_networking" {
-  name     = "rg-${var.resource_name_prefix}"
-  location = var.location
-  tags     = local.common_tags
-}
