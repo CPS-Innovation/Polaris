fdd92d6c2a7c9d1345415d830bc752509355539f
Alastair Pandelus <alastair.pandelus@cps.gov.uk>
2023-03-30 15:15:29 +0100

    PolarisProblemDetails WIP

---
+++ polaris-pipeline/Common/Error/PolarisProblemDetails.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Common.Error
+{
+    public class PolarisProblemDetails : ProblemDetails
+    {
+        public PolarisProblemDetails InnerProblem { get; set; }
+    }
+}
