4a34c6ea4f81f8a60806dc6e808abc9c82ab7693
Alastair Pandelus <alastair.pandelus@cps.gov.uk>
2023-04-05 11:12:30 +0100

    restore status != null

--- polaris-pipeline/coordinator/Functions/Orchestation/Client/Case/CaseClient.cs
+++ polaris-pipeline/coordinator/Functions/Orchestation/Client/Case/CaseClient.cs
@@ -98,8 +98,9 @@ namespace coordinator.Functions.Orchestation.Client.Case
 
                     case "DELETE":
                         var status = await orchestrationClient.GetStatusAsync(caseId);
-                        if( status.RuntimeStatus == OrchestrationRuntimeStatus.Running ||
-                            status.RuntimeStatus == OrchestrationRuntimeStatus.Suspended )
+                        if( status != null &&
+                            (status.RuntimeStatus == OrchestrationRuntimeStatus.Running || status.RuntimeStatus == OrchestrationRuntimeStatus.Suspended)
+                          )
                             await orchestrationClient.TerminateAsync(status.InstanceId, $"{loggingName} - terminated via DELETE");
 
                         await orchestrationClient.StartNewAsync(nameof(DeleteCaseOrchestrator), caseId, casePayload);
