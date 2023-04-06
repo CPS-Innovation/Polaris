84f9fe5d140eb050b7ce80c8fc4cece3d7b9ec30
Stef Stachow <stef@stachow.co.uk>
2023-04-06 17:37:11 +0100

    wip

--- polaris-pipeline/Common/Clients/SearchIndexClient.cs
+++ polaris-pipeline/Common/Clients/SearchIndexClient.cs
@@ -14,26 +14,26 @@ using Microsoft.Extensions.Logging;
 namespace Common.Clients
 {
     public class SearchIndexClient : ISearchIndexClient
-    {
-        private readonly SearchClient _searchClient;
-        private readonly IStreamlinedSearchResultFactory _streamlinedSearchResultFactory;
-        private readonly ILogger<SearchIndexClient> _logger;
+	{
+		private readonly SearchClient _searchClient;
+		private readonly IStreamlinedSearchResultFactory _streamlinedSearchResultFactory;
+		private readonly ILogger<SearchIndexClient> _logger;
 
         public SearchIndexClient(ISearchClientFactory searchClientFactory, IStreamlinedSearchResultFactory streamlinedSearchResultFactory, ILogger<SearchIndexClient> logger)
-        {
-            _searchClient = searchClientFactory.Create();
-            _streamlinedSearchResultFactory = streamlinedSearchResultFactory;
-            _logger = logger;
-        }
+		{
+			_searchClient = searchClientFactory.Create();
+			_streamlinedSearchResultFactory = streamlinedSearchResultFactory;
+			_logger = logger;
+		}
 
-        public async Task<IList<StreamlinedSearchLine>> Query(int caseId, List<TrackerDocumentDto> documents, string searchTerm, Guid correlationId)
-        {
-            _logger.LogMethodEntry(correlationId, nameof(Query), $"CaseId '{caseId}', searchTerm '{searchTerm}'");
+		public async Task<IList<StreamlinedSearchLine>> Query(int caseId, List<TrackerDocumentDto> documents, string searchTerm, Guid correlationId)
+		{
+			_logger.LogMethodEntry(correlationId, nameof(Query), $"CaseId '{caseId}', searchTerm '{searchTerm}'");
 
-            var filter = GetSearchQuery(caseId, documents);
-            var searchOptions = new SearchOptions
-            {
-                Filter = filter
+			var filter = GetSearchQuery(caseId, documents);
+			var searchOptions = new SearchOptions
+			{
+				Filter = filter
             };
 			
 			var searchResults = await _searchClient.SearchAsync<SearchLine>(searchTerm, searchOptions);
@@ -46,49 +46,39 @@ namespace Common.Clients
             }
 			}
 
-            var searchResults = await _searchClient.SearchAsync<SearchLine>(searchTerm, searchOptions);
-            var searchLines = new List<SearchLine>();
-            await foreach (var searchResult in searchResults.Value.GetResultsAsync())
-            {
-                if (IsLiveDocumentResult(documents, searchResult.Document))
-                {
-                    searchLines.Add(searchResult.Document);
-                }
-            }
-
-            _logger.LogMethodFlow(correlationId, nameof(Query), $"Found {searchLines.Count} results, building streamlined search results");
+			_logger.LogMethodFlow(correlationId, nameof(Query), $"Found {searchLines.Count} results, building streamlined search results");
             var results = BuildStreamlinedResults(searchLines, searchTerm, correlationId);
             _logger.LogMethodExit(correlationId, nameof(Query), string.Empty);
             return results;
-        }
+		}
 
         private string GetSearchQuery(int caseId, List<TrackerDocumentDto> documents)
         {
             var stringBuilder = new StringBuilder($"caseId eq {caseId}");
 
-            if (documents.Any())
-            {
-                stringBuilder.Append(" and (");
-                stringBuilder.Append($"versionId eq {documents[0].CmsVersionId}");
-                for (var i = 1; i < documents.Count; i++)
-                {
+			if(documents.Any())
+			{
+				stringBuilder.Append(" and (");
+				stringBuilder.Append($"versionId eq {documents[0].CmsVersionId}");
+				for( var i = 1;  i < documents.Count; i++ )
+				{
                     stringBuilder.Append($" or versionId eq {documents[i].CmsVersionId}");
                 }
                 stringBuilder.Append(")");
             }
-            return stringBuilder.ToString();
+			return stringBuilder.ToString();
         }
 
         public IList<StreamlinedSearchLine> BuildStreamlinedResults(IList<SearchLine> searchResults, string searchTerm, Guid correlationId)
         {
-            _logger.LogMethodEntry(correlationId, nameof(BuildStreamlinedResults), string.Empty);
-
+	        _logger.LogMethodEntry(correlationId, nameof(BuildStreamlinedResults), string.Empty);
+	        
             var streamlinedResults = new List<StreamlinedSearchLine>();
             if (searchResults.Count == 0)
                 return streamlinedResults;
 
             streamlinedResults.AddRange(searchResults
-                .Select(searchResult => _streamlinedSearchResultFactory.Create(searchResult, searchTerm, correlationId)));
+	            .Select(searchResult => _streamlinedSearchResultFactory.Create(searchResult, searchTerm, correlationId)));
 
             _logger.LogMethodExit(correlationId, nameof(BuildStreamlinedResults), string.Empty);
             return streamlinedResults;
