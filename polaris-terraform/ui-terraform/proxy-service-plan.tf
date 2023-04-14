21d21ac63f7ff2b60d74ae3a5a7b9338946e73cf
aceltuk <mark.jones3@cps.gov.uk>
2023-04-14 11:30:52 +0100

    Corrected new proxy service plan name

--- polaris-terraform/ui-terraform/proxy-service-plan.tf
+++ polaris-terraform/ui-terraform/proxy-service-plan.tf
@@ -1,7 +1,7 @@
 ﻿#################### App Service Plan ####################
 
 resource "azurerm_service_plan" "asp_polaris_proxy" {
-  name                = "asp-${local.resource_name}"
+  name                = "asp-${local.resource_name}-proxy"
   location            = azurerm_resource_group.rg_polaris.location
   resource_group_name = azurerm_resource_group.rg_polaris.name
   os_type             = "Linux"
