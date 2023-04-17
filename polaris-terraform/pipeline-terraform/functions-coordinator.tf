865dbd7d9e6171884cad0a2b3d02cea935408366
Alastair Pandelus <alastair.pandelus@cps.gov.uk>
2023-04-17 12:35:06 +0100

    remove self reference to coordinator in terraform

--- polaris-terraform/pipeline-terraform/functions-coordinator.tf
+++ polaris-terraform/pipeline-terraform/functions-coordinator.tf
@@ -35,8 +35,6 @@ resource "azurerm_linux_function_app" "fa_coordinator" {
     "DdeiAccessKey"                            = data.azurerm_function_app_host_keys.fa_ddei_host_keys.default_function_key
     "PolarisPipelineRedactPdfBaseUrl"          = "https://fa-${local.resource_name}-pdf-generator.azurewebsites.net/api/"
     "PolarisPipelineRedactPdfFunctionAppKey"   = data.azurerm_function_app_host_keys.ak_pdf_generator.default_function_key
-    "PolarisPipelineCoordinatorBaseUrl"        = "https://fa-${local.pipeline_resource_name}-coordinator.azurewebsites.net/api/"
-    "PolarisPipelineCoordinatorFunctionAppKey" = data.azurerm_function_app_host_keys.fa_pipeline_coordinator_host_keys.default_function_key
   }
   https_only = true
 
