a7ce87a7ff1d3a68f40b08235b71c104d255ce51
Stef Stachow <stef@stachow.co.uk>
2023-04-17 21:44:28 +0100

    add internal proxy routes

--- polaris-terraform/ui-terraform/app-service.tf
+++ polaris-terraform/ui-terraform/app-service.tf
@@ -19,7 +19,7 @@ resource "azurerm_linux_web_app" "as_web_polaris" {
     "REACT_APP_TENANT_ID"                      = data.azurerm_client_config.current.tenant_id
     "REACT_APP_GATEWAY_BASE_URL"               = "https://${local.resource_name}-cmsproxy.azurewebsites.net"
     "REACT_APP_GATEWAY_SCOPE"                  = "https://CPSGOVUK.onmicrosoft.com/${azurerm_linux_function_app.fa_polaris.name}/user_impersonation"
-    "REACT_APP_REAUTH_REDIRECT_URL"            = "https://${local.resource_name}-cmsproxy.azurewebsites.net/polaris?polaris-ui-url="
+    "REACT_APP_REAUTH_REDIRECT_URL"            = "https://${local.resource_name}-cmsproxy.azurewebsites.net/polaris-redirect?polaris-ui-url="
   }
 
   site_config {
