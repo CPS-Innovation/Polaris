a0efa3a06faaa4b47da58f18b1c620df99e05749
aceltuk <mark.jones3@cps.gov.uk>
2023-04-18 13:41:01 +0100

    Refined durable state cleardown to first terminate any running or pending instances and then purge the durable instance history so that each day begins afresh. A status value of "Unknown" will be left for further investigation.

--- polaris-pipeline/coordinator/Functions/Orchestration/Functions/Maintenance/ResetDurableState.cs
+++ polaris-pipeline/coordinator/Functions/Orchestration/Functions/Maintenance/ResetDurableState.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Common.Logging;
-using DurableTask.Core;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
@@ -13,6 +14,10 @@ public class ResetDurableState
 {
     private readonly ILogger<ResetDurableState> _logger;
     private const string LoggingName = $"{nameof(ResetDurableState)} - {nameof(RunAsync)}";
+    private const int DefaultPageSize = 100;
+
+    // {second} {minute} {hour} {day} {month} {day-of-week}
+    private const string TimerStartTime = "0 0 3 * * *";
 
     public ResetDurableState(ILogger<ResetDurableState> logger)
     {
@@ -20,27 +25,87 @@ public class ResetDurableState
     }
     
     [FunctionName("ResetDurableState")]
-    public async Task RunAsync([TimerTrigger("0 0 3 * * *")] TimerInfo myTimer, [DurableClient] IDurableClient client)
+    public async Task RunAsync(
+        [TimerTrigger(TimerStartTime)] TimerInfo myTimer, 
+        [DurableClient] IDurableOrchestrationClient client)
     {
         var correlationId = Guid.NewGuid();
-
         try
         {
-            _logger.LogMethodEntry(correlationId, LoggingName, string.Empty);
-            
-            await client.PurgeInstanceHistoryAsync(
-                DateTime.MinValue,
-                DateTime.UtcNow,  
-                new List<OrchestrationStatus>
-                {
-                    OrchestrationStatus.Completed,
-                    OrchestrationStatus.Failed,
-                    OrchestrationStatus.Terminated
-                });
+            await TerminateActiveOrchestrations(client, correlationId);
+
+            await PurgeOrchestrationHistory(client, correlationId);
         }
         catch (Exception ex)
         {
             _logger.LogMethodError(correlationId, LoggingName, ex.Message, ex);
         }
+    }
+
+    private async Task TerminateActiveOrchestrations(IDurableOrchestrationClient client, Guid correlationId)
+    {
+        _logger.LogMethodFlow(correlationId, LoggingName, "Overnight clear-down - first, terminate running durable instances");
+
+        var runningInstances = new HashSet<string>();
+        var terminateCondition = CreateOrchestrationQuery(new[]
+        {
+            OrchestrationRuntimeStatus.Running,
+            OrchestrationRuntimeStatus.Pending,
+            OrchestrationRuntimeStatus.Suspended,
+            OrchestrationRuntimeStatus.ContinuedAsNew
+        });
+        
+        do
+        {
+            var statusQueryResult = await client.ListInstancesAsync(terminateCondition, CancellationToken.None);
+            terminateCondition.ContinuationToken = statusQueryResult.ContinuationToken;
+
+            var instancesToTerminate = statusQueryResult.DurableOrchestrationState.Select(o => o.InstanceId).ToHashSet();
+            runningInstances.UnionWith(instancesToTerminate);
+            
+            await Task.WhenAll(instancesToTerminate.Select(async instanceId => await client.TerminateAsync(instanceId, "Forcibly terminated by overnight clear-down")));
+        } while (terminateCondition.ContinuationToken != null);
+        
+        _logger.LogMethodFlow(correlationId, LoggingName, $"Overnight clear-down - {runningInstances.Count} active durable instances forcibly terminated");
+    }
+
+    private async Task PurgeOrchestrationHistory(IDurableOrchestrationClient client, Guid correlationId)
+    {
+        _logger.LogMethodFlow(correlationId, LoggingName, "Overnight clear-down - second, purge durable instance history that is older than one week");
+        
+        var orchestrationInstances = new HashSet<string>();
+        var purgeCondition = CreateOrchestrationQuery(new[]
+        {
+            OrchestrationRuntimeStatus.Completed,
+            OrchestrationRuntimeStatus.Canceled,
+            OrchestrationRuntimeStatus.Failed,
+            OrchestrationRuntimeStatus.Terminated
+        });
+        
+        do
+        {
+            var statusQueryResult = await client.ListInstancesAsync(purgeCondition, CancellationToken.None);
+            purgeCondition.ContinuationToken = statusQueryResult.ContinuationToken;
+            
+            var instancesToPurge = statusQueryResult.DurableOrchestrationState.Select(o => o.InstanceId).ToHashSet();
+            orchestrationInstances.UnionWith(instancesToPurge);
+
+            await Task.WhenAll(instancesToPurge.Select(async instanceId => await client.PurgeInstanceHistoryAsync(instanceId)));
+        } while (purgeCondition.ContinuationToken != null);
+            
+        _logger.LogMethodFlow(correlationId, LoggingName, $"Overnight clear-down - {orchestrationInstances.Count} durable orchestration runs purged from history.");
+    }
+
+    private OrchestrationStatusQueryCondition CreateOrchestrationQuery(IEnumerable<OrchestrationRuntimeStatus> runtimeStatuses)
+    {
+        var condition = new OrchestrationStatusQueryCondition
+        {
+            CreatedTimeFrom = DateTime.MinValue,
+            CreatedTimeTo = DateTime.UtcNow,
+            RuntimeStatus = runtimeStatuses,
+            PageSize = DefaultPageSize,
+            ContinuationToken = null
+        };
+        return condition;
     }
 }
