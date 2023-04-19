18073b77e44877c05ebf61788cfcef5d4d562afa
Stef Stachow <stef@stachow.co.uk>
2023-04-19 17:56:08 +0100

    try waiting for 404s

--- polaris-e2e/cypress/e2e/api/refresh.cy.ts
+++ polaris-e2e/cypress/e2e/api/refresh.cy.ts
@@ -20,6 +20,17 @@ describe("Refresh", () => {
   it("the pipeline can clear then process a case to completion", () => {
     cy.api(routes.TRACKER_CLEAR(REFRESH_TARGET_URN, REFRESH_TARGET_CASE_ID))
       .wait(2000)
+      .waitUntil(
+        () =>
+          cy
+            .api<PipelineResults>({
+              ...routes.GET_TRACKER(REFRESH_TARGET_URN, REFRESH_TARGET_CASE_ID),
+              failOnStatusCode: false,
+            })
+            .its("status")
+            .then((status) => status === 404),
+        WAIT_UNTIL_OPTIONS
+      )
       .api(
         routes.TRACKER_START(
           REFRESH_TARGET_URN,
