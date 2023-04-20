81c9f64521cb59618725dc31540917a80ea30605
Stef Stachow <stef@stachow.co.uk>
2023-04-20 21:01:08 +0100

    touch

--- polaris-ui/public/run-substitution.sh
+++ polaris-ui/public/run-substitution.sh
@@ -1,3 +1,4 @@
+#
 REACT_APP_CLIENT_ID=3649c1c8-00cf-4b8f-a671-304bc074937c \
 REACT_APP_GATEWAY_BASE_URL= \
 REACT_APP_GATEWAY_SCOPE=https://CPSGOVUK.onmicrosoft.com/fa-polaris-dev-gateway/user_impersonation \
