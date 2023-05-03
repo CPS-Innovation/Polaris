504f3306f9cdb918bb29b648d9f8e63b376d1d97
renjithabby <renjithabby@gmail.com>
2023-05-03 15:38:18 +0100

    added the specimen landscape pdf

--- polaris-ui/src/mock-api/data/pipelinePdfResults.assurance.ts
+++ polaris-ui/src/mock-api/data/pipelinePdfResults.assurance.ts
@@ -14,9 +14,9 @@ const pipelinePdfResult: PipelineResults = {
     {
       documentId: "1",
       cmsDocumentId: "1",
-      pdfBlobName: "specimen",
+      pdfBlobName: "specimen_landscape",
       status: "Indexed",
-      cmsOriginalFileName: "specimen",
+      cmsOriginalFileName: "specimen_landscape",
       cmsMimeType: "application/pdf",
       cmsFileCreatedDate: "2020-06-01",
       cmsDocCategory: "MGForm",
