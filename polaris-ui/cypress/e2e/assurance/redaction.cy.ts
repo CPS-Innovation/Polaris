fcdbf36c7bd2d2bfe0895be485726e6981b06950
Stef Stachow <stef@stachow.co.uk>
2023-05-03 09:35:41 +0100

    wip

--- polaris-ui/cypress/e2e/assurance/redaction.cy.ts
+++ polaris-ui/cypress/e2e/assurance/redaction.cy.ts
@@ -1,13 +1,9 @@
 import { SAVE_REDACTION_ROUTE } from "../../../src/mock-api/routes";
+// import pdfs from "../../../src/mock-api/data/pdfs/pdf-strings.json";
+
 describe("redaction assurance", () => {
   it("redaction assurance", () => {
     const fileNameWithoutExtension = `1`;
-
-    cy.overrideRoute(
-      SAVE_REDACTION_ROUTE,
-      { type: "break", httpStatusCode: 500 },
-      "get"
-    );
 
     cy.overrideRoute(
       SAVE_REDACTION_ROUTE,
@@ -17,11 +13,11 @@ describe("redaction assurance", () => {
 
     cy.visit("/case-details/12AB1111111/13401");
 
-    const documentRoute = `${Cypress.env(
-      "REACT_APP_GATEWAY_BASE_URL"
-    )}/api/urns/12AB1111111/cases/13401/documents/1?v=1`;
+    // const documentRoute = `${Cypress.env(
+    //   "REACT_APP_GATEWAY_BASE_URL"
+    // )}/api/urns/12AB1111111/cases/13401/documents/1?v=1`;
 
-    cy.log(documentRoute);
+    // cy.log(documentRoute);
 
     cy.findByTestId("btn-accordion-open-close-all").click();
     cy.findByTestId("link-document-1").click();
