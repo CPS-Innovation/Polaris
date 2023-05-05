3baf812e902e309c6b23173ed90d59cf1096d912
renjithabby <renjithabby@gmail.com>
2023-05-05 13:43:56 +0100

    added the assertion of result json properties with some tolerance

--- polaris-ui/cypress/e2e/assurance/redaction.cy.ts
+++ polaris-ui/cypress/e2e/assurance/redaction.cy.ts
@@ -7,6 +7,40 @@ const getFileBase64 = (blobName: string) => {
   return fileBase64;
 };
 
+const compareNumberValuesWithTolerance = (
+  val1: number,
+  val2: number,
+  tolerance: number = 1
+) => {
+  return Math.abs(val1 - val2) <= tolerance;
+};
+
+const redactionsAsserts = (currentRedactions: any, expectedRedactions: any) => {
+  currentRedactions.forEach((current: any, index: number) => {
+    const expectedCurrent = expectedRedactions[index];
+    Object.keys(current).forEach((key) => {
+      if (typeof current[key] === "number") {
+        expect(
+          compareNumberValuesWithTolerance(current[key], expectedCurrent[key])
+        ).to.equal(true);
+      }
+      if (Array.isArray(current[key])) {
+        current[key].forEach((coordinates: any, subIndex: number) => {
+          const expectedCoordinates = expectedCurrent[key][subIndex];
+          Object.keys(coordinates).forEach((subKey) => {
+            expect(
+              compareNumberValuesWithTolerance(
+                coordinates[subKey],
+                expectedCoordinates[subKey]
+              )
+            ).to.equal(true);
+          });
+        });
+      }
+    });
+  });
+};
+
 describe("redaction assurance", () => {
   it("should redact pdfs and export the coordinates file", () => {
     cy.visit("/case-details/12AB1111111/13401");
@@ -24,9 +58,8 @@ describe("redaction assurance", () => {
         type: "returnFile",
         body: _base64ToArrayBuffer(getFileBase64(input.fileName)),
       });
-      //cy.viewport(1500, 2500);
+      cy.viewport(1500, 1000);
 
-      // cy.findByTestId("div-pdfviewer-0").should("exist").contains("1");
       cy.wait(2000);
 
       input.coordinates.forEach((coordinate) => {
@@ -34,26 +67,27 @@ describe("redaction assurance", () => {
           `page-${coordinate.pageIndex}`
         );
         cy.get(`@page-${coordinate.pageIndex}`).scrollIntoView();
-        cy.get(`@page-${coordinate.pageIndex}`).screenshot(
-          `${input.fileName}-page${coordinate.pageIndex}-pre`,
-          {
-            overwrite: true,
-            capture: "fullPage",
-          }
-        );
+        // cy.get(`@page-${coordinate.pageIndex}`).screenshot(
+        //   `${input.fileName}-page${coordinate.pageIndex}-pre`,
+        //   {
+        //     overwrite: true,
+        //     capture: "fullPage",
+        //   }
+        // );
+
         cy.get(`@page-${coordinate.pageIndex}`)
           .realMouseDown({ position: { x: coordinate.x0, y: coordinate.y0 } })
           .realMouseUp({ position: { x: coordinate.x1, y: coordinate.y1 } });
 
-        cy.findByTestId("btn-redact").click();
+        cy.findByTestId("btn-redact").click({ force: true });
 
-        cy.get(`@page-${coordinate.pageIndex}`).screenshot(
-          `${input.fileName}-page${coordinate.pageIndex}-post`,
-          {
-            overwrite: true,
-            capture: "fullPage",
-          }
-        );
+        // cy.get(`@page-${coordinate.pageIndex}`).screenshot(
+        //   `${input.fileName}-page${coordinate.pageIndex}-post`,
+        //   {
+        //     overwrite: true,
+        //     capture: "fullPage",
+        //   }
+        // );
       });
 
       cy.findByTestId("btn-save-redaction-0").click();
@@ -65,53 +99,14 @@ describe("redaction assurance", () => {
             `cypress/assurance-output/${input.fileName}.json`,
             JSON.stringify(JSON.parse(reqBody!.toString()), null, 2)
           );
+          console.log(
+            "JSON.parse(reqBody!.toString()",
+            JSON.parse(reqBody!.toString())
+          );
+
+          const currentRedactions = JSON.parse(reqBody!.toString()).redactions;
+          redactionsAsserts(currentRedactions, input.redactions);
         });
     });
   });
-
-  //old test case
-  // it("redaction assurance", () => {
-  //   const fileNameWithoutExtension = `1`;
-
-  //   cy.overrideRoute(
-  //     SAVE_REDACTION_ROUTE,
-  //     { type: "writeRequest", fileName: fileNameWithoutExtension },
-  //     "put"
-  //   );
-  //   //cy.viewport(1500, 2500);
-  //   cy.visit("/case-details/12AB1111111/13401");
-
-  //   cy.findByTestId("btn-accordion-open-close-all").click();
-  //   cy.findByTestId("link-document-1").click();
-  //   cy.findByTestId("div-pdfviewer-0").should("exist").contains("1");
-
-  //   cy.get("body").first().screenshot(`${fileNameWithoutExtension}-pre`, {
-  //     overwrite: true,
-  //     capture: "fullPage",
-  //   });
-
-  //   // cy.findByTestId("div-pdfviewer-1")
-  //   //   .realMouseDown({ position: { x: 207, y: 130 } })
-  //   //   .realMouseUp({ position: { x: 525, y: 365 } });
-
-  //   cy.get(".page")
-  //     .first()
-  //     .realMouseDown({ position: { x: 60, y: 60 } })
-  //     .realMouseUp({ position: { x: 285, y: 190 } });
-  //   cy.findByTestId("btn-redact").click({ force: true });
-
-  //   cy.get(".page")
-  //     .last()
-  //     .realMouseDown({ position: { x: 60, y: 50 } })
-  //     .realMouseUp({ position: { x: 285, y: 180 } });
-
-  //   cy.findByTestId("btn-redact").click({ force: true });
-
-  //   cy.get("body").first().screenshot(`${fileNameWithoutExtension}-post`, {
-  //     overwrite: true,
-  //     capture: "fullPage",
-  //   });
-
-  //   cy.findByTestId("btn-save-redaction-0").click();
-  // });
 });
--- polaris-ui/scripts/cy-ci.assurance.sh
+++ polaris-ui/scripts/cy-ci.assurance.sh
@@ -2,4 +2,4 @@ node cypress/assurance-input/convertToStrings &&
 BUILD_PATH='./build-cypress' env-cmd -f .env.cypress react-scripts build &&
 cypress verify &&
 cypress info &&
-start-server-and-test 'serve -l 3000 -s build-cypress' http-get://localhost:3000 'env-cmd -f .env.assurance cypress run --spec "cypress/e2e/assurance/**/*"'
+start-server-and-test 'serve -l 3000 -s build-cypress' http-get://localhost:3000 'env-cmd -f .env.assurance cypress run  --browser chrome  --spec "cypress/e2e/assurance/**/*"'
