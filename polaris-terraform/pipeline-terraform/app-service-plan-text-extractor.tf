7a1fa154d99c458f1c4737d90c642f54ec73a18f
Mark Jones <aceltuk@users.noreply.github.com>
2023-05-18 18:46:21 +0100

    Removed alt terraform. Changed coordinator to P1v3 and manually scaled to 1 instance. Performance is increased via throttling. (#282)

--- polaris-terraform/pipeline-terraform/app-service-plan-text-extractor.tf
+++ polaris-terraform/pipeline-terraform/app-service-plan-text-extractor.tf
@@ -1,10 +1,10 @@
 #################### App Service Plan ####################
 
-resource "azurerm_service_plan" "asp-windows-ep" {
-  name                         = "asp-windows-${local.resource_name}"
+resource "azurerm_service_plan" "asp_polaris_text_extractor" {
+  name                         = "asp-text-extractor-${local.resource_name}"
   location                     = azurerm_resource_group.rg.location
   resource_group_name          = azurerm_resource_group.rg.name
-  os_type                      = "Windows"
+  os_type                      = "Linux"
   sku_name                     = var.app_service_plan_sku
   tags                         = local.common_tags
   maximum_elastic_worker_count = 10
