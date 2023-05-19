544c6a2ed6f530fae790a4dd9dc5058afdca051e
Renjith Abby <renjithabby@gmail.com>
2023-05-19 11:39:57 +0100

    Added feedback form link (#275)
    * added feedback form link
    
    * added the survey link as enviornment variable

--- polaris-ui/src/app/config.ts
+++ polaris-ui/src/app/config.ts
@@ -3,6 +3,7 @@ export const GATEWAY_SCOPE = process.env.REACT_APP_GATEWAY_SCOPE!;
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID!;
 export const TENANT_ID = process.env.REACT_APP_TENANT_ID!;
 export const BUILD_NUMBER = process.env.REACT_APP_BUILD_NUMBER || "development";
+export const SURVEY_LINK = process.env.REACT_APP_SURVEY_LINK;
 export const PIPELINE_POLLING_DELAY = Number(
   process.env.REACT_APP_PIPELINE_POLLING_DELAY || 2000
 );
