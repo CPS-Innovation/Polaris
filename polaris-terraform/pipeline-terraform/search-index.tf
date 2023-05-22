a3921da175cd140c1ec7e23e3f45f9742e97f158
aceltuk <mark.jones3@cps.gov.uk>
2023-05-22 16:27:43 +0100

    Added dependency chain for search service

--- polaris-terraform/pipeline-terraform/search-index.tf
+++ polaris-terraform/pipeline-terraform/search-index.tf
@@ -15,5 +15,9 @@ resource "restapi_object" "definition" {
   path         = "/indexes"
   query_string = "api-version=2021-04-30-Preview"
   data         = file("search-index-definition.json")
-  depends_on   = [azurerm_search_service.ss]
+  depends_on   = [
+    azurerm_search_service.ss, 
+    azurerm_private_endpoint.pipeline_search_service_pe, 
+    azurerm_private_dns_a_record.pipeline_search_service_dns_a
+  ]
 }
