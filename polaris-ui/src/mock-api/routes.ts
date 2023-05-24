336c2d5e6e18a92e438f8b57fcae09c451b84351
renjithabby <renjithabby@gmail.com>
2023-05-24 16:55:31 +0100

    fixed prettier issue

--- polaris-ui/src/mock-api/routes.ts
+++ polaris-ui/src/mock-api/routes.ts
@@ -5,8 +5,7 @@ export const TRACKER_ROUTE = "api/urns/:urn/cases/:caseId/tracker";
 export const FILE_ROUTE = "api/urns/:urn/cases/:caseId/documents/:documentId";
 export const GET_SAS_URL_ROUTE =
   "api/urns/:urn/cases/:caseId/documents/:documentId/sasUrl";
-export const TEXT_SEARCH_ROUTE =
-  "api/urns/:urn/cases/:caseId/search?query=";
+export const TEXT_SEARCH_ROUTE = "api/urns/:urn/cases/:caseId/search?query=";
 export const DOCUMENT_CHECKOUT_ROUTE =
   "/api/urns/:urn/cases/:caseId/documents/:documentId/checkout";
 export const DOCUMENT_CHECKIN_ROUTE =
