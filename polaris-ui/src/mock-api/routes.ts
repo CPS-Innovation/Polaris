bc8c23ce7c54d98e228fbce43f6dcd4bdf694ae9
Alastair Pandelus <alastair.pandelus@cps.gov.uk>
2023-05-24 15:31:37 +0100

    fix front end URLS to match changes to backend

--- polaris-ui/src/mock-api/routes.ts
+++ polaris-ui/src/mock-api/routes.ts
@@ -6,7 +6,7 @@ export const FILE_ROUTE = "api/urns/:urn/cases/:caseId/documents/:documentId";
 export const GET_SAS_URL_ROUTE =
   "api/urns/:urn/cases/:caseId/documents/:documentId/sasUrl";
 export const TEXT_SEARCH_ROUTE =
-  "api/urns/:urn/cases/:caseId/documents/search?query=";
+  "api/urns/:urn/cases/:caseId/search?query=";
 export const DOCUMENT_CHECKOUT_ROUTE =
   "/api/urns/:urn/cases/:caseId/documents/:documentId/checkout";
 export const DOCUMENT_CHECKIN_ROUTE =
