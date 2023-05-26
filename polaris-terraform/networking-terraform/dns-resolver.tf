7ffe9983f3d419f9eb1b20f05d1c41000b5114d8
aceltuk <mark.jones3@cps.gov.uk>
2023-05-26 11:09:33 +0100

    Resolve circular dependency

--- polaris-terraform/networking-terraform/dns-resolver.tf
+++ polaris-terraform/networking-terraform/dns-resolver.tf
@@ -3,8 +3,6 @@ resource "azurerm_private_dns_resolver" "polaris_private_dns_resolver" {
   resource_group_name = azurerm_resource_group.rg_networking.name
   location            = azurerm_resource_group.rg_networking.location
   virtual_network_id  = azurerm_virtual_network.vnet_networking.id
-
-  depends_on = [azurerm_virtual_network.vnet_networking]
 }
 
 resource "azurerm_private_dns_resolver_inbound_endpoint" "polaris_private_dns_resolver_inbound_endpoint" {
@@ -16,7 +14,6 @@ resource "azurerm_private_dns_resolver_inbound_endpoint" "polaris_private_dns_re
     subnet_id                    = azurerm_subnet.sn_polaris_dns_resolve_subnet.id
   }
 
-  depends_on = [azurerm_private_dns_resolver.polaris_private_dns_resolver]
   tags = {
     environment = var.environment.name
   }
--- polaris-terraform/networking-terraform/vnet.tf
+++ polaris-terraform/networking-terraform/vnet.tf
@@ -3,16 +3,8 @@ resource "azurerm_virtual_network" "vnet_networking" {
   location            = azurerm_resource_group.rg_networking.location
   resource_group_name = azurerm_resource_group.rg_networking.name
   address_space       = [var.vnetAddressSpace]
-  dns_servers = [
-    azurerm_private_dns_resolver_inbound_endpoint.polaris_private_dns_resolver_inbound_endpoint.ip_configurations[0].private_ip_address
-  ]
-
+  
   tags = {
     environment = var.environment.name
   }
-  
-  depends_on = [
-    azurerm_private_dns_resolver.polaris_private_dns_resolver,
-    azurerm_private_dns_resolver_inbound_endpoint.polaris_private_dns_resolver_inbound_endpoint
-  ]
 }
