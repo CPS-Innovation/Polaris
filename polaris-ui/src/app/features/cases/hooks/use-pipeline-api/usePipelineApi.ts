59cbfd20cc53a30d4539cd21a8829bb2f979d20b
renjithabby <renjithabby@gmail.com>
2023-05-30 10:06:27 +0100

    minor refactor

--- polaris-ui/src/app/features/cases/hooks/use-pipeline-api/usePipelineApi.ts
+++ polaris-ui/src/app/features/cases/hooks/use-pipeline-api/usePipelineApi.ts
@@ -4,7 +4,7 @@ import { PipelineResults } from "../../domain/gateway/PipelineResults";
 import { initiateAndPoll } from "./initiate-and-poll";
 import { PIPELINE_POLLING_DELAY } from "../../../../config";
 import { CombinedState } from "../../domain/CombinedState";
-import * as HEADERS from "../../../cases/api/header-factory";
+import { generateGuid } from "../../../cases/api/generate-guid";
 
 export const usePipelineApi = (
   urn: string,
@@ -21,7 +21,7 @@ export const usePipelineApi = (
 
   useEffect(() => {
     if (pipelineRefreshData.startRefresh) {
-      const correlationId = HEADERS.correlationId()["Correlation-Id"];
+      const correlationId = generateGuid();
       //get correlationID here and add it ot the setPipelineResults and remove it from gateway
       initiateAndPoll(
         urn,
