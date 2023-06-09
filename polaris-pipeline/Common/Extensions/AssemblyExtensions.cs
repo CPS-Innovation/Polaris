90141c63061321a6dea08b2c41d6183168b7dc77
aceltuk <mark.jones3@cps.gov.uk>
2023-06-09 16:42:07 +0100

    Added exposure of source version (commitid) to status endpoints and introduced new tagging system into the main pipelines

--- polaris-pipeline/Common/Extensions/AssemblyExtensions.cs
+++ polaris-pipeline/Common/Extensions/AssemblyExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Net;
 using System.Reflection;
 using Common.Health.Status;
@@ -15,11 +16,14 @@ public static class AssemblyExtensions
             return new JsonResult(new {status = "Assembly version could not be retrieved"}) {StatusCode = (int) HttpStatusCode.BadRequest};
         
         var assemblyName = currentAssembly.GetName();
+        var infoVerAttr =(AssemblyInformationalVersionAttribute) currentAssembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute))
+            .FirstOrDefault();
         
         var response = new AssemblyStatus
         {
             Name = assemblyName.Name,
-            Version = assemblyName.Version?.ToString(),
+            BuildVersion = assemblyName.Version?.ToString(),
+            SourceVersion = GetShortHash(infoVerAttr), 
             LastBuilt = GetBuildDate(currentAssembly)
         };
         return new JsonResult(response) {StatusCode = (int) HttpStatusCode.OK};
@@ -39,5 +43,16 @@ public static class AssemblyExtensions
         value = value[(index + buildVersionMetadataPrefix.Length)..];
         return DateTime.TryParseExact(value, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result) 
             ? result : default;
+    }
+    
+    private static string GetShortHash(AssemblyInformationalVersionAttribute infoVerAttr)
+    {
+        var version = "1.0.0+LOCALBUILD"; // Dummy version for local dev
+        if (infoVerAttr == null || infoVerAttr.InformationalVersion.Length <= 6) return version;
+        
+        // Hash is embedded in the version after a '+' symbol, e.g. 1.0.0+a34a913742f8845d3da5309b7b17242222d41a21
+        version = infoVerAttr.InformationalVersion;
+        var longHash = version[(version.IndexOf('+') + 1)..];
+        return longHash.Substring(longHash.Length - 6, 6);
     }
 }
