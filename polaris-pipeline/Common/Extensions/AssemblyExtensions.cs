619107862388096394845dd566f29770ab3acced
Mark Jones <aceltuk@users.noreply.github.com>
2023-06-13 19:55:35 +0100

    Feature/21774 improve diff checks using tags (#331)
    * Added exposure of source version (commitid) to status endpoints and introduced new tagging system into the main pipelines
    
    * Added missing checkout task to the update-tag stage in the manual pipelines
    
    * Corrected build date retrieval for status endpoints
    
    * Added QA e2e tests into main release pipeline
    
    * Added QA e2e tests into main release pipeline and added all e2e test child pipelines into checks for all manual pipelines. The order of QA and PROD both made to depend on the DEV stages passing so the personal releasing can opt to deploy to PROD only, bypassing QA if it is not in-use.
    
    * Removed obsolete SourceRevisionId setting from all csproj files and ensured the short git has is the first 8 chars
    
    * Temp disable tests
    
    * Removed e2e tests from manual pipelines because of unreliability. They can be re-added later.
    
    * Restored PDF Generator tests following new (temp) Aspose licence

--- polaris-pipeline/Common/Extensions/AssemblyExtensions.cs
+++ polaris-pipeline/Common/Extensions/AssemblyExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Reflection;
 using Common.Health.Status;
@@ -15,11 +16,14 @@ public static class AssemblyExtensions
             return new JsonResult(new {status = "Assembly version could not be retrieved"}) {StatusCode = (int) HttpStatusCode.BadRequest};
         
         var assemblyName = currentAssembly.GetName();
+        var infoVerAttr =(AssemblyInformationalVersionAttribute) currentAssembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute))
+            .FirstOrDefault();
         
         var response = new AssemblyStatus
         {
             Name = assemblyName.Name,
-            Version = assemblyName.Version?.ToString(),
+            BuildVersion = assemblyName.Version?.ToString(),
+            SourceVersion = GetShortHash(infoVerAttr),
             LastBuilt = GetBuildDate(currentAssembly)
         };
         return new JsonResult(response) {StatusCode = (int) HttpStatusCode.OK};
@@ -27,17 +31,19 @@ public static class AssemblyExtensions
     
     private static DateTime GetBuildDate(Assembly assembly)
     {
-        const string buildVersionMetadataPrefix = "+build";
+        var creationDate = new FileInfo(assembly.Location).CreationTimeUtc;
+        var britishZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+        return TimeZoneInfo.ConvertTime(creationDate, TimeZoneInfo.Utc, britishZone);
+    }
 
-        var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-        if (attribute?.InformationalVersion == null) return default;
+    private static string GetShortHash(AssemblyInformationalVersionAttribute infoVerAttr)
+    {
+        var version = "1.0.0+LOCALBUILD"; // Dummy version for local dev
+        if (infoVerAttr == null || infoVerAttr.InformationalVersion.Length <= 6) return version;
         
-        var value = attribute.InformationalVersion;
-        var index = value.IndexOf(buildVersionMetadataPrefix, StringComparison.Ordinal);
-        if (index <= 0) return default;
-        
-        value = value[(index + buildVersionMetadataPrefix.Length)..];
-        return DateTime.TryParseExact(value, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result) 
-            ? result : default;
+        // Hash is embedded in the version after a '+' symbol, e.g. 1.0.0+a34a913742f8845d3da5309b7b17242222d41a21
+        version = infoVerAttr.InformationalVersion;
+        var longHash = version[(version.IndexOf('+') + 1)..];
+        return longHash[..8];
     }
 }
