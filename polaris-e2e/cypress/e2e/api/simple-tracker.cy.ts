41ba3d6c26a2800aa471641189a31d6018bef181
Stef Stachow <stef@stachow.co.uk>
2023-06-14 20:55:32 +0100

    wip

--- polaris-e2e/cypress/e2e/api/simple-tracker.cy.ts
+++ polaris-e2e/cypress/e2e/api/simple-tracker.cy.ts
@@ -3,7 +3,6 @@ import "cypress-wait-until"
 import { PipelineResults } from "../../../gateway/PipelineResults"
 import { ApiRoutes, makeApiRoutes } from "./helpers/make-routes"
 import { WAIT_UNTIL_OPTIONS } from "../../support/options"
-import { ApiTextSearchResult } from "../../../gateway/ApiTextSearchResult"
 
 const { TARGET_URN, TARGET_CASE_ID } = Cypress.env()
 
@@ -16,7 +15,7 @@ describe("Simple Tracker", () => {
     })
   })
 
-  it("run a tracker through to all documents being indexed", () => {
+  it("run a trackerthrough to all documents being indexed", () => {
     cy.api(routes.TRACKER_CLEAR(TARGET_URN, TARGET_CASE_ID))
       .waitUntil(
         () =>
@@ -56,12 +55,5 @@ describe("Simple Tracker", () => {
             }),
         WAIT_UNTIL_OPTIONS
       )
-      .api<ApiTextSearchResult[]>(
-        routes.GET_SEARCH(TARGET_URN, TARGET_CASE_ID, "aspose")
-      )
-      .its("body")
-      .then((results) => {
-        expect(results.length).to.equal(0)
-      })
   })
 })
