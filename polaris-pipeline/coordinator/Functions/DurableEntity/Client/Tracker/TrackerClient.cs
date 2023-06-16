93eea96512cd97b52c4aad4b773f8dc778cc7575
Alastair Pandelus <alastair.pandelus@cps.gov.uk>
2023-06-16 10:49:36 +0100

    remove unused using

--- polaris-pipeline/coordinator/Functions/DurableEntity/Client/Tracker/TrackerClient.cs
+++ polaris-pipeline/coordinator/Functions/DurableEntity/Client/Tracker/TrackerClient.cs
@@ -15,7 +15,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace coordinator.Functions.DurableEntity.Client.Tracker
 {
