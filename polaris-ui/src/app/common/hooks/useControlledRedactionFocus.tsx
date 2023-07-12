031e77dceb855b64c0074e8c3270f54775628d13
renjithabby <renjithabby@gmail.com>
2023-07-12 11:11:57 +0100

    minor refactor

--- polaris-ui/src/app/common/hooks/useControlledRedactionFocus.tsx
+++ polaris-ui/src/app/common/hooks/useControlledRedactionFocus.tsx
@@ -56,6 +56,133 @@ export const useControlledRedactionFocus = (
 
   const activeButtonIndex = useRef(0);
 
+  const handleTabKeyPress = (
+    tabbableElements: Element[],
+    removeBtn: Element | null,
+    removeAllRedactionBtn: Element | null,
+    entryButton: Element | null,
+    e: KeyboardEvent
+  ) => {
+    if (document.activeElement === entryButton) {
+      (tabbableElements[0] as HTMLElement).focus();
+      (tabbableElements[0] as HTMLElement).scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+      activeButtonIndex.current = 0;
+      if (removeBtn) {
+        (removeBtn as HTMLElement).style.visibility = "visible";
+      }
+      e.preventDefault();
+      return;
+    }
+    if (
+      document.activeElement === removeBtn &&
+      activeButtonIndex.current < tabbableElements.length - 1
+    ) {
+      activeButtonIndex.current = activeButtonIndex.current + 1;
+      (tabbableElements[activeButtonIndex.current] as HTMLElement).focus();
+      (
+        tabbableElements[activeButtonIndex.current] as HTMLElement
+      ).scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+      e.preventDefault();
+      return;
+    }
+    if (
+      Array.from(tabbableElements).includes(document.activeElement!) &&
+      removeBtn
+    ) {
+      activeButtonIndex.current = Array.from(tabbableElements).indexOf(
+        document.activeElement!
+      );
+      (removeBtn as HTMLElement).focus();
+      (removeBtn as HTMLElement).scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+      e.preventDefault();
+      return;
+    }
+    if (
+      document.activeElement === removeBtn &&
+      activeButtonIndex.current === tabbableElements.length - 1
+    ) {
+      (removeAllRedactionBtn as HTMLElement).focus();
+      (removeAllRedactionBtn as HTMLElement).scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+      (removeBtn as HTMLElement).style.visibility = "hidden";
+      e.preventDefault();
+      return;
+    }
+  };
+
+  const handleShiftTabKeyPress = (
+    tabbableElements: Element[],
+    removeBtn: Element | null,
+    removeAllRedactionBtn: Element | null,
+    entryButton: Element | null,
+    e: KeyboardEvent
+  ) => {
+    if (document.activeElement === removeAllRedactionBtn) {
+      (tabbableElements[tabbableElements.length - 1] as HTMLElement).focus();
+      (
+        tabbableElements[tabbableElements.length - 1] as HTMLElement
+      ).scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+      activeButtonIndex.current = tabbableElements.length - 1;
+      if (removeBtn) {
+        (removeBtn as HTMLElement).style.visibility = "visible";
+      }
+      e.preventDefault();
+      return;
+    }
+    if (document.activeElement === removeBtn) {
+      (tabbableElements[activeButtonIndex.current] as HTMLElement).focus();
+      (
+        tabbableElements[activeButtonIndex.current] as HTMLElement
+      ).scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+      e.preventDefault();
+      return;
+    }
+    if (
+      Array.from(tabbableElements).includes(document.activeElement!) &&
+      activeButtonIndex.current > 0
+    ) {
+      activeButtonIndex.current = activeButtonIndex.current - 1;
+      (tabbableElements[activeButtonIndex.current] as HTMLElement).focus();
+      (
+        tabbableElements[activeButtonIndex.current] as HTMLElement
+      ).scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+      e.preventDefault();
+      return;
+    }
+    if (document.activeElement === tabbableElements[0]) {
+      (entryButton as HTMLElement).focus();
+      (entryButton as HTMLElement).scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+      if (removeBtn) {
+        (removeBtn as HTMLElement).style.visibility = "hidden";
+      }
+      e.preventDefault();
+      return;
+    }
+  };
+
   const keyDownHandler = useCallback(
     (e: KeyboardEvent) => {
       if (
@@ -76,125 +203,22 @@ export const useControlledRedactionFocus = (
           : reportIssueBtn;
 
         if (!e.shiftKey) {
-          if (document.activeElement === entryButton) {
-            (tabbableElements[0] as HTMLElement).focus();
-            (tabbableElements[0] as HTMLElement).scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-            activeButtonIndex.current = 0;
-            if (removeBtn) {
-              (removeBtn as HTMLElement).style.visibility = "visible";
-            }
-            e.preventDefault();
-            return;
-          }
-          if (
-            document.activeElement === removeBtn &&
-            activeButtonIndex.current < tabbableElements.length - 1
-          ) {
-            activeButtonIndex.current = activeButtonIndex.current + 1;
-            (
-              tabbableElements[activeButtonIndex.current] as HTMLElement
-            ).focus();
-            (
-              tabbableElements[activeButtonIndex.current] as HTMLElement
-            ).scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-            e.preventDefault();
-            return;
-          }
-          if (
-            Array.from(tabbableElements).includes(document.activeElement) &&
-            removeBtn
-          ) {
-            activeButtonIndex.current = Array.from(tabbableElements).indexOf(
-              document.activeElement
-            );
-            (removeBtn as HTMLElement).focus();
-            (removeBtn as HTMLElement).scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-            e.preventDefault();
-            return;
-          }
-          if (
-            document.activeElement === removeBtn &&
-            activeButtonIndex.current === tabbableElements.length - 1
-          ) {
-            (removeAllRedactionBtn as HTMLElement).focus();
-            (removeAllRedactionBtn as HTMLElement).scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-            (removeBtn as HTMLElement).style.visibility = "hidden";
-            e.preventDefault();
-            return;
-          }
+          handleTabKeyPress(
+            tabbableElements,
+            removeBtn,
+            removeAllRedactionBtn,
+            entryButton,
+            e
+          );
         }
         if (e.shiftKey) {
-          if (document.activeElement === removeAllRedactionBtn) {
-            (
-              tabbableElements[tabbableElements.length - 1] as HTMLElement
-            ).focus();
-            (
-              tabbableElements[tabbableElements.length - 1] as HTMLElement
-            ).scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-            activeButtonIndex.current = tabbableElements.length - 1;
-            if (removeBtn) {
-              (removeBtn as HTMLElement).style.visibility = "visible";
-            }
-            e.preventDefault();
-            return;
-          }
-          if (document.activeElement === removeBtn) {
-            (
-              tabbableElements[activeButtonIndex.current] as HTMLElement
-            ).focus();
-            (
-              tabbableElements[activeButtonIndex.current] as HTMLElement
-            ).scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-            e.preventDefault();
-            return;
-          }
-          if (
-            Array.from(tabbableElements).includes(document.activeElement) &&
-            activeButtonIndex.current > 0
-          ) {
-            activeButtonIndex.current = activeButtonIndex.current - 1;
-            (
-              tabbableElements[activeButtonIndex.current] as HTMLElement
-            ).focus();
-            (
-              tabbableElements[activeButtonIndex.current] as HTMLElement
-            ).scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-            e.preventDefault();
-            return;
-          }
-          if (document.activeElement === tabbableElements[0]) {
-            (entryButton as HTMLElement).focus();
-            (entryButton as HTMLElement).scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-            if (removeBtn) {
-              (removeBtn as HTMLElement).style.visibility = "hidden";
-            }
-            e.preventDefault();
-            return;
-          }
+          handleShiftTabKeyPress(
+            tabbableElements,
+            removeBtn,
+            removeAllRedactionBtn,
+            entryButton,
+            e
+          );
         }
       }
     },
