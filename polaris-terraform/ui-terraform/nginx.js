2ef2f90f36935194281e00a7580b8aba2df06224
Stef Stachow <stef@stachow.co.uk>
2023-08-08 11:49:33 +0100

    remove double url encoding

--- polaris-terraform/ui-terraform/nginx.js
+++ polaris-terraform/ui-terraform/nginx.js
@@ -3,7 +3,7 @@ async function polarisAuthRedirect(r) {
   const cookie = encodeURIComponent(r.headersIn.Cookie ?? "")
   const referer = encodeURIComponent(r.headersIn.Referer ?? "")
   const polarisUiUrl = encodeURIComponent(r.args["polaris-ui-url"] ?? "")
-  const q = encodeURIComponent(r.args["q"] ?? "")
+  const q = r.args["q"] ?? ""
   r.return(
     302,
     `${redirectHostAddress}/api/init?cookie=${cookie}&referer=${referer}&polaris-ui-url=${polarisUiUrl}&q=${q}`
