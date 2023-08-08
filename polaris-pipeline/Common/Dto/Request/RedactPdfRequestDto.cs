6beb907811cc1fffd158273c027ab042f48c3cbe
Stef <stef@stachow.co.uk>
2023-08-08 14:26:10 +0100

    Bugfix/22626 upload doc size too large - add telemetry (#464)
    * #22626 add original file byte count into doc conversion telemetry

--- polaris-pipeline/Common/Dto/Request/RedactPdfRequestDto.cs
+++ polaris-pipeline/Common/Dto/Request/RedactPdfRequestDto.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using Common.Dto.Request.Redaction;
 using Common.ValueObjects;
