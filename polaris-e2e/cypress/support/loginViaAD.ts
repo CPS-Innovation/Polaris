0b4c720060d1ed14ab530b3c940e53ac7b322a61
renjithabby <renjithabby@gmail.com>
2023-08-10 14:08:35 +0100

    minor refactor

--- polaris-e2e/cypress/support/loginViaAD.ts
+++ polaris-e2e/cypress/support/loginViaAD.ts
@@ -48,6 +48,7 @@ export const loginViaAD = (username: string, password: string) => {
   cy.url().should("equal", "http://localhost:3000/polaris-ui");
 
   cy.origin("login.microsoftonline.com", { args: "" }, () => {
+    //NOTE : this wait is needed otherwise e2e test get stuck on the "Pick an Account screen of msal"
     cy.wait(1000);
   });
   return cy.visit("/polaris-ui").contains(username);
