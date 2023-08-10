07ae23d42d3288ecf794b86ec3f74df698c06402
renjithabby <renjithabby@gmail.com>
2023-08-10 14:04:25 +0100

    added new method to login using AAD authentication

--- polaris-e2e/cypress.config.ts
+++ polaris-e2e/cypress.config.ts
@@ -1,48 +1,48 @@
-import { defineConfig } from "cypress"
-import fs from "fs-extra"
-import path from "path"
+import { defineConfig } from "cypress";
+import fs from "fs-extra";
+import path from "path";
 
-const globalAny: any = global
+const globalAny: any = global;
 
 const getConfigurationByFile = (file: string) => {
-  const pathToConfigFile = path.resolve("config", `${file}.json`)
-  return fs.readJsonSync(pathToConfigFile)
-}
+  const pathToConfigFile = path.resolve("config", `${file}.json`);
+  return fs.readJsonSync(pathToConfigFile);
+};
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       if (!config.env.ENVIRONMENT) {
-        throw new Error("Please provide an ENVIRONMENT variable")
+        throw new Error("Please provide an ENVIRONMENT variable");
       }
-      require("cypress-timestamps/plugin")(on)
-      require("cypress-terminal-report/src/installLogsPrinter")(on)
+      require("cypress-timestamps/plugin")(on);
+      require("cypress-terminal-report/src/installLogsPrinter")(on);
 
       on("task", {
         storeTokenResponseInNode: (tokenResponse: any) => {
-          globalAny.tokenResponse = tokenResponse
-          return null
+          globalAny.tokenResponse = tokenResponse;
+          return null;
         },
         retrieveTokenResponseFromNode: () => {
-          return globalAny.tokenResponse || null
+          return globalAny.tokenResponse || null;
         },
-      })
+      });
 
-      const baseEnvFromFile = getConfigurationByFile("base")
+      const baseEnvFromFile = getConfigurationByFile("base");
       const environmentEnvFromFile = getConfigurationByFile(
         "env." + config.env.ENVIRONMENT
-      )
+      );
       const env = {
         ...baseEnvFromFile,
         ...environmentEnvFromFile,
-      }
+      };
 
-      config.baseUrl = env.BASE_URL
+      config.baseUrl = env.BASE_URL;
 
-      const resolvedEnv = { ...config.env, ...env }
-      console.log("Resolved env: ", resolvedEnv)
+      const resolvedEnv = { ...config.env, ...env };
+      console.log("Resolved env: ", resolvedEnv);
 
-      return { ...config, env: resolvedEnv }
+      return { ...config, env: resolvedEnv };
     },
     baseUrl: "http://example.org",
     video: true,
@@ -56,5 +56,6 @@ export default defineConfig({
     viewportWidth: 1500,
     defaultCommandTimeout: 60000,
     trashAssetsBeforeRuns: false,
+    experimentalModifyObstructiveThirdPartyCode: true,
   },
-})
+});
--- polaris-e2e/cypress/support/commands.ts
+++ polaris-e2e/cypress/support/commands.ts
@@ -1,47 +1,47 @@
-import "@testing-library/cypress/add-commands"
-import { injectTokens } from "./inject-tokens"
-import { CorrelationId, correlationIds } from "./correlation-ids"
-import { WAIT_UNTIL_OPTIONS } from "./options"
-import "cypress-wait-until"
+import "@testing-library/cypress/add-commands";
+import { loginViaAD } from "./loginViaAD";
+import { CorrelationId, correlationIds } from "./correlation-ids";
+import { WAIT_UNTIL_OPTIONS } from "./options";
+import "cypress-wait-until";
 
 type ADTokens = {
-  access_token: string
-  id_token: string
-  expires_in: string
-  ext_expires_in: string
-}
+  access_token: string;
+  id_token: string;
+  expires_in: string;
+  ext_expires_in: string;
+};
 
 declare global {
   namespace Cypress {
     interface Chainable<Subject> {
-      loginToAD(): Chainable<any>
-      loginToCms(): Chainable<any>
-      preemptivelyAttachCookies(): Chainable<any>
-      fullLogin(): Chainable<any>
-      clearCaseTracker(urn: string, caseId: string): Chainable<any>
-      getADTokens(): Chainable<ADTokens>
+      loginToAD(): Chainable<any>;
+      loginToCms(): Chainable<any>;
+      preemptivelyAttachCookies(): Chainable<any>;
+      fullLogin(): Chainable<any>;
+      clearCaseTracker(urn: string, caseId: string): Chainable<any>;
+      getADTokens(): Chainable<ADTokens>;
       getAuthHeaders(): Chainable<{
-        Authorization: string
-        credentials: "include"
-      }>
+        Authorization: string;
+        credentials: "include";
+      }>;
       setPolarisInstrumentationGuid(
         correlationId: CorrelationId
-      ): Chainable<AUTWindow>
-      selectPDFTextElement(matchString: string): void
+      ): Chainable<AUTWindow>;
+      selectPDFTextElement(matchString: string): void;
       // roll our own typing to help cast the body returned from the api call
-      checkDependencies(): Chainable<any>
-      api<T>(url: string, body?: RequestBody): Chainable<ApiResponseBody<T>>
+      checkDependencies(): Chainable<any>;
+      api<T>(url: string, body?: RequestBody): Chainable<ApiResponseBody<T>>;
       api<T>(
         method: HttpMethod,
         url: string,
         body?: RequestBody
-      ): Chainable<ApiResponseBody<T>>
-      api<T>(options: Partial<RequestOptions>): Chainable<ApiResponseBody<T>>
+      ): Chainable<ApiResponseBody<T>>;
+      api<T>(options: Partial<RequestOptions>): Chainable<ApiResponseBody<T>>;
     }
   }
 
   export interface ApiResponseBody<T> extends Cypress.Response<T> {
-    size?: number
+    size?: number;
   }
 }
 
@@ -63,10 +63,10 @@ const {
   CMS_LOGGED_IN_CONFIRMATION_LOCATOR,
   COOKIE_REDIRECT_URL,
   DEPENDENCIES,
-} = Cypress.env()
+} = Cypress.env();
 
 const AUTOMATION_LANDING_PAGE_PATH =
-  "/polaris-ui/?automation-test-first-visit=true"
+  "/polaris-ui/?automation-test-first-visit=true";
 
 Cypress.Commands.add(
   "getADTokens",
@@ -76,7 +76,7 @@ Cypress.Commands.add(
   () => {
     cy.task("retrieveTokenResponseFromNode").then((cachedTokens) => {
       if (cachedTokens) {
-        return cachedTokens
+        return cachedTokens;
       } else {
         return cy
           .log("setting tokens")
@@ -97,11 +97,11 @@ Cypress.Commands.add(
             cy
               .task("storeTokenResponseInNode", response.body)
               .task("retrieveTokenResponseFromNode")
-          )
+          );
       }
-    })
+    });
   }
-)
+);
 
 Cypress.Commands.add(
   "getAuthHeaders",
@@ -122,30 +122,10 @@ Cypress.Commands.add(
       cy.getADTokens().then((adTokens) => ({
         Authorization: `Bearer ${adTokens.access_token}`,
         credentials: "include",
-      }))
-    })
+      }));
+    });
   }
-)
-
-Cypress.Commands.add("loginToAD", () => {
-  // Intermittent failures have been observed on the first UI test of the day
-  //  whereby the very first UI interaction (this login process visit to the UI)
-  //  times out.  A working assumption is that the UI deployment is not warmed up
-  //  and so may take longer than the default 60 seconds to respond.  So rather than
-  //  increase the default timeout for all UI tests, we'll just increase the timeout
-  //  for this one type of visit (which will always be the first in any UI test run).
-  const timeoutMs = 2 * 60 * 1000
-
-  return cy
-    .visit(AUTOMATION_LANDING_PAGE_PATH, { timeout: timeoutMs })
-    .getADTokens()
-    .then((response) => {
-      injectTokens(response)
-    })
-    .reload()
-    .visit("/polaris-ui")
-    .contains(AD_USERNAME)
-})
+);
 
 Cypress.Commands.add("loginToCms", () => {
   // Pass in dependencies via args option
@@ -157,13 +137,13 @@ Cypress.Commands.add("loginToCms", () => {
     submitButtonLocator: CMS_SUBMIT_BUTTON_LOCATOR,
     loggedInConfirmationLocator: CMS_LOGGED_IN_CONFIRMATION_LOCATOR,
   } as {
-    username: string
-    password: string
-    usernameFieldLocator: string
-    passwordFieldLocator: string
-    submitButtonLocator: string
-    loggedInConfirmationLocator: string
-  }
+    username: string;
+    password: string;
+    usernameFieldLocator: string;
+    passwordFieldLocator: string;
+    submitButtonLocator: string;
+    loggedInConfirmationLocator: string;
+  };
 
   if (isFullUrl(CMS_LOGIN_PAGE_URL)) {
     cy.origin(
@@ -177,33 +157,33 @@ Cypress.Commands.add("loginToCms", () => {
         submitButtonLocator,
         loggedInConfirmationLocator,
       }) => {
-        cy.visit("/")
-        cy.get(usernameFieldLocator).type(username)
-        cy.get(passwordFieldLocator).type(password)
-        cy.get(submitButtonLocator).click()
-        cy.get(loggedInConfirmationLocator).should("exist")
+        cy.visit("/");
+        cy.get(usernameFieldLocator).type(username);
+        cy.get(passwordFieldLocator).type(password);
+        cy.get(submitButtonLocator).click();
+        cy.get(loggedInConfirmationLocator).should("exist");
       }
-    )
+    );
   } else {
-    cy.reload()
-    cy.visit(CMS_LOGIN_PAGE_URL)
-    cy.get(args.usernameFieldLocator).type(args.username)
-    cy.get(args.passwordFieldLocator).type(args.password)
-    cy.get(args.submitButtonLocator).click()
-    cy.get(args.loggedInConfirmationLocator).should("exist")
+    cy.reload();
+    cy.visit(CMS_LOGIN_PAGE_URL);
+    cy.get(args.usernameFieldLocator).type(args.username);
+    cy.get(args.passwordFieldLocator).type(args.password);
+    cy.get(args.submitButtonLocator).click();
+    cy.get(args.loggedInConfirmationLocator).should("exist");
   }
-})
+});
 
 Cypress.Commands.add("preemptivelyAttachCookies", () => {
   cy.visit(
     COOKIE_REDIRECT_URL +
       encodeURIComponent(Cypress.config().baseUrl + "/polaris-ui?auth-refresh")
-  )
-})
+  );
+});
 
 Cypress.Commands.add("fullLogin", () => {
-  cy.loginToAD().loginToCms().preemptivelyAttachCookies()
-})
+  cy.loginToAD().loginToCms().preemptivelyAttachCookies();
+});
 
 Cypress.Commands.add("clearCaseTracker", (urn, caseId) => {
   cy.getADTokens().then((adTokens) => {
@@ -230,28 +210,28 @@ Cypress.Commands.add("clearCaseTracker", (urn, caseId) => {
           .its("status")
           .then((status) => status === 404),
       WAIT_UNTIL_OPTIONS
-    )
-  })
-})
+    );
+  });
+});
 
 Cypress.Commands.add("checkDependencies", () => {
   for (var dependency of DEPENDENCIES) {
-    const url = dependency
-    const t0 = performance.now()
+    const url = dependency;
+    const t0 = performance.now();
     cy.request({ url }).then(({ status, body }) => {
-      var t1 = performance.now()
+      var t1 = performance.now();
       cy.log(
         `Previous call to ${url} returned ${status} in ${(
           (t1 - t0) /
           1000
         ).toFixed(2)}s`
-      )
-    })
+      );
+    });
   }
-})
+});
 
 declare global {
-  var __POLARIS_INSTRUMENTATION_GUID__: string
+  var __POLARIS_INSTRUMENTATION_GUID__: string;
 }
 
 Cypress.Commands.add(
@@ -260,13 +240,13 @@ Cypress.Commands.add(
     // note: on any direct navigation using cy.visit() this setting will be lost
     //  as the page is reloaded.
     cy.window().then((win) => {
-      win.__POLARIS_INSTRUMENTATION_GUID__ = correlationIds[correlationId]
-      console.log(win.__POLARIS_INSTRUMENTATION_GUID__)
+      win.__POLARIS_INSTRUMENTATION_GUID__ = correlationIds[correlationId];
+      console.log(win.__POLARIS_INSTRUMENTATION_GUID__);
     })
-)
+);
 
 Cypress.Commands.add("selectPDFTextElement", (matchString: string) => {
-  cy.wait(100)
+  cy.wait(100);
   cy.get(`.textLayer span:contains(${matchString})`)
     .filter(":not(:has(*))")
     .first()
@@ -274,16 +254,20 @@ Cypress.Commands.add("selectPDFTextElement", (matchString: string) => {
       cy.wrap(element)
         .trigger("mousedown")
         .then(() => {
-          const el = element[0]
-          const document = el.ownerDocument
-          const range = document.createRange()
-          range.selectNodeContents(el)
-          document.getSelection()?.removeAllRanges()
-          document.getSelection()?.addRange(range)
+          const el = element[0];
+          const document = el.ownerDocument;
+          const range = document.createRange();
+          range.selectNodeContents(el);
+          document.getSelection()?.removeAllRanges();
+          document.getSelection()?.addRange(range);
         })
-        .trigger("mouseup")
-      cy.document().trigger("selectionchange")
-    })
-})
+        .trigger("mouseup");
+      cy.document().trigger("selectionchange");
+    });
+});
 
-const isFullUrl = (url: string) => url.startsWith("http")
+const isFullUrl = (url: string) => url.startsWith("http");
+
+Cypress.Commands.add("loginToAD", () => {
+  return loginViaAD(AD_USERNAME, AD_PASSWORD);
+});
