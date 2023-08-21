5930c0783536c556cc7ec36293411ccfc3497d7d
Mark Jones <aceltuk@users.noreply.github.com>
2023-08-21 18:06:32 +0100

    Create the event hub namespace and register it with the vNet (#505)
    * Create the event hub namespace and register it with the vNet
    
    * Syntax change
    
    * Corrected syntax
    
    * Corrected lookup to existing subnet

---
+++ polaris-terraform/pipeline-terraform/event-hub.tf
@@ -0,0 +1,52 @@
+﻿resource "azurerm_eventhub_namespace" "polaris-eh-namespace" {
+  name = "eh-${local.resource_name}-namespace"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  sku = var.pipeline_event_hub_settings.sku
+  capacity = var.pipeline_event_hub_settings.capacity
+  public_network_access_enabled = false
+  
+  network_rulesets {
+    default_action                 = "Deny"
+    public_network_access_enabled  = false
+    trusted_service_access_enabled = true
+    virtual_network_rule {
+      subnet_id = data.azurerm_subnet.polaris_netherite_subnet.id
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Create Private Endpoint
+resource "azurerm_private_endpoint" "pipeline_eh_namespace_pe" {
+  name                = "${azurerm_eventhub_namespace.polaris-eh-namespace.name}-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_netherite_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = "polaris-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_event_hub_namespace.id]
+  }
+
+  private_service_connection {
+    name                           = "${azurerm_eventhub_namespace.polaris-eh-namespace.name}-psc"
+    private_connection_resource_id = azurerm_eventhub_namespace.polaris-eh-namespace.id
+    is_manual_connection           = false
+    subresource_names              = ["namespace"]
+  }
+}
+
+# Create DNS A Record
+resource "azurerm_private_dns_a_record" "pipeline_eh_namespace_dns_a" {
+  name                = azurerm_eventhub_namespace.polaris-eh-namespace.name
+  zone_name           = data.azurerm_private_dns_zone.dns_zone_event_hub_namespace.name
+  resource_group_name = "rg-${var.networking_resource_name_suffix}"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.pipeline_eh_namespace_pe.private_service_connection.0.private_ip_address]
+  tags                = local.common_tags
+}
