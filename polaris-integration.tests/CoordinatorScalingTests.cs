1a156567d371c81e2a98c3b97780548e09fcad9e
Alastair Pandelus <68290803+Alastair-Pandelus@users.noreply.github.com>
2023-09-05 10:17:33 +0100

    Feature/task 23597 add integration volume tests (#528)
    * mock DDEI
    
    * mock DDEI
    
    * fix Sonarcloud issue
    
    * existing port of Cypess Tests
    
    * integration tests
    
    * integration tests
    
    * remove unused code

---
+++ polaris-integration.tests/CoordinatorScalingTests.cs
@@ -0,0 +1,68 @@
+using polaris_integration.tests;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace polaris_integration.coordinator.tests
+{
+    // Test 1, 2, 3
+    public class AlphabeticalOrderer : ITestCaseOrderer
+    {
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(
+            IEnumerable<TTestCase> testCases) where TTestCase : ITestCase =>
+            testCases.OrderBy(testCase => testCase.TestMethod.Method.Name);
+    }
+
+    [TestCaseOrderer(ordererTypeName: "polaris_integration.coordinator.tests.AlphabeticalOrderer", ordererAssemblyName: "polaris-integration.tests")]
+    public class CoordinatorScalingTests : BaseCoordinatorScalingTest
+    {
+        const int _testCases = 10;
+
+        [Fact]
+        public async Task Test_1_Delete_Cases()
+        {
+            List<Task> tasks = new List<Task>();
+
+            for (var i = 1; i <= _testCases; i++)
+            {
+                var urn = $"TEST100-0{i.ToString("00")}";
+                var caseId = 100 + i;
+
+                tasks.Add(CaseDeleteAsync(urn, caseId));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        [Fact]
+        public async Task Test_2_Refresh_Cases()
+        {
+            List<Task> tasks = new List<Task>();
+
+            for (var i = 1; i <= _testCases; i++)
+            {
+                var urn = $"TEST100-0{i.ToString("00")}";
+                var caseId = 100 + i;
+
+                tasks.Add(CaseRefreshAsync(urn, caseId, Guid.NewGuid().ToString()) );
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        [Fact]
+        public async Task Test_3_WaitFor_Trackers()
+        {
+            List<Task> tasks = new List<Task>();
+
+            for (var i = 1; i <= _testCases; i++)
+            {
+                var urn = $"TEST100-0{i.ToString("00")}";
+                var caseId = 100 + i;
+
+                tasks.Add(WaitForCompletedTrackerAsync(urn, caseId, Guid.NewGuid().ToString()));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
