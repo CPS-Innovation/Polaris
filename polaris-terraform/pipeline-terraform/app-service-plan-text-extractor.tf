25ee6240ccf81549643ea0e81972dd08e851694f
aceltuk <mark.jones3@cps.gov.uk>
2023-09-05 20:29:53 +0100

    Removed orphaned service plans after adding a new coordinator EP1 plan and renaming the other two for consistency

--- polaris-terraform/pipeline-terraform/app-service-plan-text-extractor.tf
+++ polaris-terraform/pipeline-terraform/app-service-plan-text-extractor.tf
@@ -10,14 +10,3 @@ resource "azurerm_service_plan" "asp_polaris_ep_text_extractor" {
   tags                         = local.common_tags
   maximum_elastic_worker_count = var.pipeline_component_service_plans.text_extractor_maximum_instances
 }
-
-resource "azurerm_service_plan" "asp_polaris_text_extractor" {
-  #checkov:skip=CKV_AZURE_212:Ensure App Service has a minimum number of instances for fail over
-  name                         = "asp-text-extractor-${local.resource_name}"
-  location                     = azurerm_resource_group.rg.location
-  resource_group_name          = azurerm_resource_group.rg.name
-  os_type                      = "Linux"
-  sku_name                     = var.app_service_plan_sku
-  tags                         = local.common_tags
-  maximum_elastic_worker_count = 10
-}
