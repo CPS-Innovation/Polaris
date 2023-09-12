eb8ad942e4357ff038e81f1e140b0d88bab4c54d
Mark Jones <aceltuk@users.noreply.github.com>
2023-09-12 15:47:48 +0100

    Add new storage account definitions (#547)
    * Add new storage account definitions
    
    * Added Azure-managed identities to all storage accounts, in-line with SonarCloud recommendations
    
    * Add identity definition to UI storage account while I was "at it"

--- polaris-terraform/pipeline-terraform/storage-account.tf
+++ polaris-terraform/pipeline-terraform/storage-account.tf
@@ -43,6 +43,116 @@ resource "azurerm_storage_account" "sa" {
       retention_policy_days = 10
     }
   }
+  
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.common_tags
+}
+
+resource "azurerm_storage_account" "sa_pdf_generator" {
+  #checkov:skip=CKV_AZURE_206:Ensure that Storage Accounts use replication
+  #checkov:skip=CKV2_AZURE_38:Ensure soft-delete is enabled on Azure storage account
+  #checkov:skip=CKV2_AZURE_1:Ensure storage for critical data are encrypted with Customer Managed Key
+  #checkov:skip=CKV2_AZURE_21:Ensure Storage logging is enabled for Blob service for read requests
+  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  account_kind                    = "StorageV2"
+  account_replication_type        = "LRS"
+  account_tier                    = "Standard"
+  enable_https_traffic_only       = true
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
+
+  min_tls_version = "TLS1_2"
+
+  network_rules {
+    default_action = "Deny"
+  }
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.common_tags
+}
+
+resource "azurerm_storage_account" "sa_text_extractor" {
+  #checkov:skip=CKV_AZURE_206:Ensure that Storage Accounts use replication
+  #checkov:skip=CKV2_AZURE_38:Ensure soft-delete is enabled on Azure storage account
+  #checkov:skip=CKV2_AZURE_1:Ensure storage for critical data are encrypted with Customer Managed Key
+  #checkov:skip=CKV2_AZURE_21:Ensure Storage logging is enabled for Blob service for read requests
+  name                = "sacps${var.env != "prod" ? var.env : ""}textextractor"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  account_kind                    = "StorageV2"
+  account_replication_type        = "LRS"
+  account_tier                    = "Standard"
+  enable_https_traffic_only       = true
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
+
+  min_tls_version = "TLS1_2"
+
+  network_rules {
+    default_action = "Deny"
+  }
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 
   tags = local.common_tags
 }
@@ -59,6 +169,32 @@ resource "azurerm_storage_account_network_rules" "pipeline_sa_rules" {
     data.azurerm_subnet.polaris_pdfgenerator_subnet.id,
     data.azurerm_subnet.polaris_textextractor_subnet.id,
     data.azurerm_subnet.polaris_gateway_subnet.id,
+    data.azurerm_subnet.polaris_apps_subnet.id
+  ]
+}
+
+resource "azurerm_storage_account_network_rules" "pipeline_sa_pdf_generator_rules" {
+  storage_account_id = azurerm_storage_account.sa_pdf_generator.id
+
+  default_action = "Deny"
+  bypass         = ["Metrics", "Logging", "AzureServices"]
+  depends_on     = [azurerm_storage_account.sa_pdf_generator]
+  virtual_network_subnet_ids = [
+    data.azurerm_subnet.polaris_ci_subnet.id,
+    data.azurerm_subnet.polaris_pdfgenerator_subnet.id,
+    data.azurerm_subnet.polaris_apps_subnet.id
+  ]
+}
+
+resource "azurerm_storage_account_network_rules" "pipeline_sa_text_extractor_rules" {
+  storage_account_id = azurerm_storage_account.sa_text_extractor.id
+
+  default_action = "Deny"
+  bypass         = ["Metrics", "Logging", "AzureServices"]
+  depends_on     = [azurerm_storage_account.sa_text_extractor]
+  virtual_network_subnet_ids = [
+    data.azurerm_subnet.polaris_ci_subnet.id,
+    data.azurerm_subnet.polaris_textextractor_subnet.id,
     data.azurerm_subnet.polaris_apps_subnet.id
   ]
 }
@@ -202,6 +338,256 @@ resource "azurerm_private_dns_a_record" "pipeline_sa_queue_dns_a" {
   tags                = local.common_tags
 }
 
+# PDF Generator Storage Account Private Endpoint and DNS Config
+# Create Private Endpoint for Blobs
+resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_blob_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-blob-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = "polaris-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_blob_storage.id]
+  }
+
+  private_service_connection {
+    name                           = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-blob-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_pdf_generator.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+}
+
+# Create DNS A Record
+resource "azurerm_private_dns_a_record" "pipeline_sa_pdf_generator_blob_dns_a" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator"
+  zone_name           = data.azurerm_private_dns_zone.dns_zone_blob_storage.name
+  resource_group_name = "rg-${var.networking_resource_name_suffix}"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.pipeline_sa_pdf_generator_blob_pe.private_service_connection.0.private_ip_address]
+  tags                = local.common_tags
+}
+
+# Create Private Endpoint for Tables
+resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_table_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-table-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = "polaris-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_table_storage.id]
+  }
+
+  private_service_connection {
+    name                           = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-table-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_pdf_generator.id
+    is_manual_connection           = false
+    subresource_names              = ["table"]
+  }
+}
+
+# Create DNS A Record
+resource "azurerm_private_dns_a_record" "pipeline_sa_pdf_generator_table_dns_a" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator"
+  zone_name           = data.azurerm_private_dns_zone.dns_zone_table_storage.name
+  resource_group_name = "rg-${var.networking_resource_name_suffix}"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.pipeline_sa_pdf_generator_table_pe.private_service_connection.0.private_ip_address]
+  tags                = local.common_tags
+}
+
+# Create Private Endpoint for Files
+resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_file_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-file-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = "polaris-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_file_storage.id]
+  }
+
+  private_service_connection {
+    name                           = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-file-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_pdf_generator.id
+    is_manual_connection           = false
+    subresource_names              = ["file"]
+  }
+}
+
+# Create DNS A Record
+resource "azurerm_private_dns_a_record" "pipeline_sa_pdf_generator_file_dns_a" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator"
+  zone_name           = data.azurerm_private_dns_zone.dns_zone_file_storage.name
+  resource_group_name = "rg-${var.networking_resource_name_suffix}"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.pipeline_sa_pdf_generator_file_pe.private_service_connection.0.private_ip_address]
+  tags                = local.common_tags
+}
+
+# Create Private Endpoint for Queues
+resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_queue_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-queue-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = "polaris-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_queue_storage.id]
+  }
+
+  private_service_connection {
+    name                           = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-queue-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_pdf_generator.id
+    is_manual_connection           = false
+    subresource_names              = ["queue"]
+  }
+}
+
+# Create DNS A Record
+resource "azurerm_private_dns_a_record" "pipeline_sa_pdf_generator_queue_dns_a" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator"
+  zone_name           = data.azurerm_private_dns_zone.dns_zone_queue_storage.name
+  resource_group_name = "rg-${var.networking_resource_name_suffix}"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.pipeline_sa_pdf_generator_queue_pe.private_service_connection.0.private_ip_address]
+  tags                = local.common_tags
+}
+
+# Text Extractor Storage Account Private Endpoint and DNS Config
+# Create Private Endpoint for Blobs
+resource "azurerm_private_endpoint" "pipeline_sa_text_extractor_blob_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}textextractor-blob-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = "polaris-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_blob_storage.id]
+  }
+
+  private_service_connection {
+    name                           = "sacps${var.env != "prod" ? var.env : ""}textextractor-blob-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_text_extractor.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+}
+
+# Create DNS A Record
+resource "azurerm_private_dns_a_record" "pipeline_sa_text_extractor_blob_dns_a" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}textextractor"
+  zone_name           = data.azurerm_private_dns_zone.dns_zone_blob_storage.name
+  resource_group_name = "rg-${var.networking_resource_name_suffix}"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.pipeline_sa_text_extractor_blob_pe.private_service_connection.0.private_ip_address]
+  tags                = local.common_tags
+}
+
+# Create Private Endpoint for Tables
+resource "azurerm_private_endpoint" "pipeline_sa_text_extractor_table_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}textextractor-table-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = "polaris-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_table_storage.id]
+  }
+
+  private_service_connection {
+    name                           = "sacps${var.env != "prod" ? var.env : ""}textextractor-table-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_text_extractor.id
+    is_manual_connection           = false
+    subresource_names              = ["table"]
+  }
+}
+
+# Create DNS A Record
+resource "azurerm_private_dns_a_record" "pipeline_sa_text_extractor_table_dns_a" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}textextractor"
+  zone_name           = data.azurerm_private_dns_zone.dns_zone_table_storage.name
+  resource_group_name = "rg-${var.networking_resource_name_suffix}"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.pipeline_sa_text_extractor_table_pe.private_service_connection.0.private_ip_address]
+  tags                = local.common_tags
+}
+
+# Create Private Endpoint for Files
+resource "azurerm_private_endpoint" "pipeline_sa_text_extractor_file_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}textextractor-file-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = "polaris-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_file_storage.id]
+  }
+
+  private_service_connection {
+    name                           = "sacps${var.env != "prod" ? var.env : ""}textextractor-file-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_text_extractor.id
+    is_manual_connection           = false
+    subresource_names              = ["file"]
+  }
+}
+
+# Create DNS A Record
+resource "azurerm_private_dns_a_record" "pipeline_sa_text_extractor_file_dns_a" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}textextractor"
+  zone_name           = data.azurerm_private_dns_zone.dns_zone_file_storage.name
+  resource_group_name = "rg-${var.networking_resource_name_suffix}"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.pipeline_sa_text_extractor_file_pe.private_service_connection.0.private_ip_address]
+  tags                = local.common_tags
+}
+
+# Create Private Endpoint for Queues
+resource "azurerm_private_endpoint" "pipeline_sa_text_extractor_queue_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}textextractor-queue-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = "polaris-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_queue_storage.id]
+  }
+
+  private_service_connection {
+    name                           = "sacps${var.env != "prod" ? var.env : ""}textextractor-queue-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_text_extractor.id
+    is_manual_connection           = false
+    subresource_names              = ["queue"]
+  }
+}
+
+# Create DNS A Record
+resource "azurerm_private_dns_a_record" "pipeline_sa_text_extractor_queue_dns_a" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}textextractor"
+  zone_name           = data.azurerm_private_dns_zone.dns_zone_queue_storage.name
+  resource_group_name = "rg-${var.networking_resource_name_suffix}"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.pipeline_sa_text_extractor_queue_pe.private_service_connection.0.private_ip_address]
+  tags                = local.common_tags
+}
+
 resource "azapi_resource" "pipeline_sa_coordinator_file_share" {
   type      = "Microsoft.Storage/storageAccounts/fileServices/shares@2022-09-01"
   name      = "pipeline-coordinator-content-share"
@@ -211,11 +597,11 @@ resource "azapi_resource" "pipeline_sa_coordinator_file_share" {
 resource "azapi_resource" "pipeline_sa_pdf_generator_file_share" {
   type      = "Microsoft.Storage/storageAccounts/fileServices/shares@2022-09-01"
   name      = "pipeline-pdf-generator-content-share"
-  parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sa.name}/fileServices/default"
+  parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sa_pdf_generator.name}/fileServices/default"
 }
 
 resource "azapi_resource" "pipeline_sa_text_extractor_file_share" {
   type      = "Microsoft.Storage/storageAccounts/fileServices/shares@2022-09-01"
   name      = "pipeline-text-extractor-content-share"
-  parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sa.name}/fileServices/default"
+  parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sa_text_extractor.name}/fileServices/default"
 }
