33f8c6b73bb7a6e8209d79be8e20eca33ea3be9a
Renjith Abby <renjithabby@gmail.com>
2023-09-14 11:38:23 +0100

    Story/22748 : Main content area navigation (#548)
    * initial commit on main content area navigation
    
    * minor refactor
    
    * added cypress test
    
    * updated the logic of start from the last active focus
    
    * minor refactro
    
    * minor correction

--- polaris-ui/cypress/e2e/case-details/case-details-page.cy.ts
+++ polaris-ui/cypress/e2e/case-details/case-details-page.cy.ts
@@ -662,7 +662,7 @@ describe("case details page", () => {
       cy.realPress(["Shift", "Tab"]);
       verifyAriaDescriptionTextContent("WEST YORKSHIRE POLICE");
       cy.realPress(["Shift", "Tab"]);
-      cy.focused().should("have.id", "panel-0");
+      cy.focused().should("have.id", "active-tab-panel");
       cy.findByTestId("link-removeAll").click();
     });
 
@@ -802,6 +802,59 @@ describe("case details page", () => {
       keyPressAndVerifySelection("backward", "o");
       keyPressAndVerifySelection("backward", "1");
       keyPressAndVerifySelection("backward", "P");
+    });
+  });
+
+  describe("Switch main content areas using the Period '.' key press", () => {
+    it("Should be able switch between main content areas using the Period '.' Key Press", () => {
+      cy.visit("/case-details/12AB1111111/13401");
+      cy.findByTestId("btn-accordion-open-close-all").click();
+      cy.findByTestId("link-document-1").click();
+      cy.findByTestId("div-pdfviewer-0")
+        .should("exist")
+        .contains("REPORT TO CROWN PROSECUTOR FOR CHARGING DECISION,");
+      cy.realPress(".");
+      cy.focused().should("have.id", "side-panel");
+      cy.realPress(".");
+      cy.focused().should("have.id", "document-tabs");
+      cy.realPress(".");
+      cy.focused().should("have.id", "active-tab-panel");
+      cy.realPress(".");
+      cy.focused().should("have.id", "side-panel");
+    });
+
+    it("Should continue from the last active content if the focus has been changed to the inner element", () => {
+      cy.visit("/case-details/12AB1111111/13401");
+      cy.findByTestId("btn-accordion-open-close-all").click();
+      cy.findByTestId("link-document-2").click();
+      cy.findByTestId("link-document-1").click();
+      cy.findByTestId("div-pdfviewer-1")
+        .should("exist")
+        .contains("REPORT TO CROWN PROSECUTOR FOR CHARGING DECISION,");
+      cy.realPress(".");
+      cy.focused().should("have.id", "side-panel");
+      cy.realPress(".");
+      cy.focused().should("have.id", "document-tabs");
+      cy.findAllByTestId("btn-tab-0").click();
+      cy.realPress(".");
+      cy.focused().should("have.id", "document-tabs");
+      cy.realPress(".");
+      cy.focused().should("have.id", "active-tab-panel");
+      cy.realPress("Tab");
+      cy.focused().should("have.id", "btn-report-issue");
+      cy.realPress(".");
+      cy.focused().should("have.id", "active-tab-panel");
+      cy.realPress(".");
+      cy.focused().should("have.id", "side-panel");
+    });
+
+    it("Should keep the focus on side-panel, if there are no documents open  while pressing the Period '.' Key", () => {
+      cy.visit("/case-details/12AB1111111/13401");
+      cy.findByTestId("btn-accordion-open-close-all").click();
+      cy.realPress(".");
+      cy.focused().should("have.id", "side-panel");
+      cy.realPress(".");
+      cy.focused().should("have.id", "side-panel");
     });
   });
 });
