52ea7e163f6776503f08db1e03420f5f73a537e0
Stef Stachow <stef@stachow.co.uk>
2023-09-18 22:33:53 +0100

    wip

--- polaris-analytics/app/Program.cs
+++ polaris-analytics/app/Program.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Core;
+﻿using System.Text.RegularExpressions;
+using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.OperationalInsights;
@@ -10,6 +11,10 @@ namespace polaris_analytics
     const string ProdSubscriptionId = "/subscriptions/888d158f-ade3-4528-9c59-84bd2260d5ee";
     const string LogAnalyticsResourceGroupName = "rg-polaris-analytics";
     const string LogAnalyticsResourceName = "la-polaris";
+
+    const string EmailRegexPattern = @"[^@\s""']+@[^@\s]+\.[^@\s""']+";
+
+
 
     static void Main()
     {
@@ -30,7 +35,10 @@ namespace polaris_analytics
       {
         var fileName = Path.ChangeExtension(polarisSavedSearch.Data.DisplayName, ".kql");
         var filePath = Path.Join("../queries", fileName);
-        File.WriteAllText(filePath, polarisSavedSearch.Data.Query);
+
+        // remove email address
+        var redactedQuery = Regex.Replace(polarisSavedSearch.Data.Query, EmailRegexPattern, "REDACTED@REDACTED.EMAIL.ADDRESS");
+        File.WriteAllText(filePath, redactedQuery);
       }
     }
   }
