9f10ad31c0725f1977d5ceca73f355dcc9534364
aceltuk <mark.jones3@cps.gov.uk>
2023-09-18 11:08:53 +0100

    Initial draft of powershell script that attempts to send custom events to App Insights. Also includes a touch.txt change to ensure a simple build of the coordinator is pushed out.

---
+++ polaris-devops-pipelines/scripts/SendCustomEventToAppInsights.ps1
@@ -0,0 +1,48 @@
+﻿
+Param(
+    [String]$InstrumentationKey,
+    [String]$Name,
+    [String]$Message,
+    [bool]$Success = $True)
+
+#install Microsoft.ApplicationInsights package
+Install-Package Microsoft.ApplicationInsights -Version 2.0.1
+
+# setup telemetry client ("*ApplicationInsights.dll" DLL must be present)
+$telemetryClient  = New-Object -TypeName "Microsoft.ApplicationInsights.TelemetryClient"
+$telemetryClient.InstrumentationKey     = $InstrumentationKey
+
+try
+{
+    # initialize context information
+    $telemetryClient.Context.Session.Id     = "$($env:SYSTEM_TEAMPROJECTID)/$($env:BUILD_BUILDID)"
+    $telemetryClient.Context.User.Id        = "$($env:SYSTEM_TEAMPROJECTID)/$($env:BUILD_BUILDID)"
+    $telemetryClient.Context.Operation.Id   = $Name
+    $telemetryClient.Context.Operation.Name = $Name
+
+    # setup request telemetry information
+    #$request = New-Object -TypeName "Microsoft.ApplicationInsights.DataContracts.EventTelemetry"
+    #$request.Name       = $Name
+
+    $p = [pscustomobject]@{
+        Str = "Test Message"
+    } | ConvertTo-Json
+
+    $measurement = [pscustomobject]@{
+        totalTime = 0.33
+    } | ConvertTo-Json
+    $telemetryClient.TrackEvent($Name, $p, $measurement);
+
+    # track the request
+    #$telemetryClient.TrackEvent($request)
+}
+catch
+{
+    $telemtryException = New-Object -TypeName "Microsoft.ApplicationInsights.DataContracts.ExceptionTelemetry"
+    $telemtryException.Exception = $_.Exception
+    $client.TrackException($telemtryException)
+}
+finally
+{
+    $telemetryClient.Flush()
+}
