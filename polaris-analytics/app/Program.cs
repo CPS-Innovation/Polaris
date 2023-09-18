f08c10a09602887da17800d7b63d54b4ef853c80
Stef Stachow <stef@stachow.co.uk>
2023-09-18 14:17:38 +0100

    #23777 - add console app to export kusto queries to source control

---
+++ polaris-analytics/app/Program.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.OperationalInsights;
+
+namespace polaris_analytics
+{
+  internal class Program
+  {
+    const string ProdSubscriptionId = "/subscriptions/888d158f-ade3-4528-9c59-84bd2260d5ee";
+    const string LogAnalyticsResourceGroupName = "rg-polaris-analytics";
+    const string LogAnalyticsResourceName = "la-polaris";
+
+    static void Main()
+    {
+      var client = new ArmClient(new DefaultAzureCredential());
+
+      var subscriptionIdentifier = new ResourceIdentifier(ProdSubscriptionId);
+      var subscription = client.GetSubscriptionResource(subscriptionIdentifier);
+
+      var resourceGroups = subscription.GetResourceGroups();
+      var resourceGroup = resourceGroups.Get(LogAnalyticsResourceGroupName).Value;
+
+      var workspace = resourceGroup.GetOperationalInsightsWorkspace(LogAnalyticsResourceName).Value;
+      var polarisSavedSearches = workspace.GetOperationalInsightsSavedSearches()
+          .Where(savedSearch => savedSearch.Id.Name.Contains("polaris_", StringComparison.OrdinalIgnoreCase))
+          .ToList();
+
+      foreach (var polarisSavedSearch in polarisSavedSearches)
+      {
+        var fileName = Path.ChangeExtension(polarisSavedSearch.Data.DisplayName, ".kql");
+        var filePath = Path.Join("../queries", fileName);
+        File.WriteAllText(filePath, polarisSavedSearch.Data.Query);
+      }
+    }
+  }
+}
+
