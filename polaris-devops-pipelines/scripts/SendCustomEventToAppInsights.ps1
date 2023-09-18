8c785c1b8a4de7de053ba232755de5786ba0dc53
aceltuk <mark.jones3@cps.gov.uk>
2023-09-18 13:07:04 +0100

    Added some logging to see where it is hanging

--- polaris-devops-pipelines/scripts/SendCustomEventToAppInsights.ps1
+++ polaris-devops-pipelines/scripts/SendCustomEventToAppInsights.ps1
@@ -6,51 +6,46 @@ Param(
     [bool]$Success = $True)
 
 #insall NuGet package manager if not found
+Write-Output "Checking for existing NuGet package manager"
 $_nugetUrl = "https://api.nuget.org/v3/index.json"
 $packageSources = Get-PackageSource
 if(@($packageSources).Where{$_.location -eq $_nugetUrl}.count -eq 0)
 {
+    Write-Output "Installing NuGet package manager"
     Register-PackageSource -Trusted -Name MyNuGet -Location $_nugetUrl -ProviderName NuGet
 }
 
 #install Microsoft.ApplicationInsights package
+Write-Output "Installing Microsoft.ApplicationInsights"
 Install-Package -Force Microsoft.ApplicationInsights -RequiredVersion "2.21.0" -Scope CurrentUser
 
+Write-Output "Instantiating Telemetry Client"
 # setup telemetry client ("*ApplicationInsights.dll" DLL must be present)
 $telemetryClient  = New-Object -TypeName "Microsoft.ApplicationInsights.TelemetryClient"
 $telemetryClient.InstrumentationKey = $InstrumentationKey
 
+# initialize context information
+Write-Output "Initializing Telemetry Client with Context"
+$telemetryClient.Context.Session.Id = "$($env:SYSTEM_TEAMPROJECTID)/$($env:BUILD_BUILDID)"
+$telemetryClient.Context.User.Id = "$($env:SYSTEM_TEAMPROJECTID)/$($env:BUILD_BUILDID)"
+$telemetryClient.Context.Operation.Id = $Name
+$telemetryClient.Context.Operation.Name = $Name
+
 try
 {
-    # initialize context information
-    $telemetryClient.Context.Session.Id = "$($env:SYSTEM_TEAMPROJECTID)/$($env:BUILD_BUILDID)"
-    $telemetryClient.Context.User.Id = "$($env:SYSTEM_TEAMPROJECTID)/$($env:BUILD_BUILDID)"
-    $telemetryClient.Context.Operation.Id = $Name
-    $telemetryClient.Context.Operation.Name = $Name
-
-    # setup request telemetry information
-    #$request = New-Object -TypeName "Microsoft.ApplicationInsights.DataContracts.EventTelemetry"
-    #$request.Name       = $Name
-
-    #$p = [pscustomobject]@{
-    #    Str = $Message
-    #} | ConvertTo-Json
-
-    #$measurement = [pscustomobject]@{
-    #    totalTime = 0.33
-    #} | ConvertTo-Json
+    Write-Output "Calling 'TrackEvent' with '$Message'"
     $telemetryClient.TrackEvent($Message);
-
-    # track the request
-    #$telemetryClient.TrackEvent($request)
 }
 catch
 {
+    Write-Output "Exception Caught - Recording"
     $telemtryException = New-Object -TypeName "Microsoft.ApplicationInsights.DataContracts.ExceptionTelemetry"
     $telemtryException.Exception = $_.Exception
-    $client.TrackException($telemtryException)
+    $telemetryClient.TrackException($telemtryException)
 }
 finally
 {
+    Write-Output "Flushing to output stream"
     $telemetryClient.Flush()
+    Write-Output "Finished writing message to App Insights"
 }
