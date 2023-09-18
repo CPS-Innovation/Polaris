55d2c2e3a199e3feaf5e62a855098986b821bd34
aceltuk <mark.jones3@cps.gov.uk>
2023-09-18 13:12:03 +0100

    Changed to Write-Host

--- polaris-devops-pipelines/scripts/SendCustomEventToAppInsights.ps1
+++ polaris-devops-pipelines/scripts/SendCustomEventToAppInsights.ps1
@@ -6,26 +6,26 @@ Param(
     [bool]$Success = $True)
 
 #insall NuGet package manager if not found
-Write-Output "Checking for existing NuGet package manager"
+Write-Host "Checking for existing NuGet package manager"
 $_nugetUrl = "https://api.nuget.org/v3/index.json"
 $packageSources = Get-PackageSource
 if(@($packageSources).Where{$_.location -eq $_nugetUrl}.count -eq 0)
 {
-    Write-Output "Installing NuGet package manager"
+    Write-Host "Installing NuGet package manager"
     Register-PackageSource -Trusted -Name MyNuGet -Location $_nugetUrl -ProviderName NuGet
 }
 
 #install Microsoft.ApplicationInsights package
-Write-Output "Installing Microsoft.ApplicationInsights"
+Write-Host "Installing Microsoft.ApplicationInsights"
 Install-Package -Force Microsoft.ApplicationInsights -RequiredVersion "2.21.0" -Scope CurrentUser
 
-Write-Output "Instantiating Telemetry Client"
+Write-Host "Instantiating Telemetry Client"
 # setup telemetry client ("*ApplicationInsights.dll" DLL must be present)
 $telemetryClient  = New-Object -TypeName "Microsoft.ApplicationInsights.TelemetryClient"
 $telemetryClient.InstrumentationKey = $InstrumentationKey
 
 # initialize context information
-Write-Output "Initializing Telemetry Client with Context"
+Write-Host "Initializing Telemetry Client with Context"
 $telemetryClient.Context.Session.Id = "$($env:SYSTEM_TEAMPROJECTID)/$($env:BUILD_BUILDID)"
 $telemetryClient.Context.User.Id = "$($env:SYSTEM_TEAMPROJECTID)/$($env:BUILD_BUILDID)"
 $telemetryClient.Context.Operation.Id = $Name
@@ -33,19 +33,19 @@ $telemetryClient.Context.Operation.Name = $Name
 
 try
 {
-    Write-Output "Calling 'TrackEvent' with '$Message'"
+    Write-Host "Calling 'TrackEvent' with '$Message'"
     $telemetryClient.TrackEvent($Message);
 }
 catch
 {
-    Write-Output "Exception Caught - Recording"
+    Write-Host "Exception Caught - Recording"
     $telemtryException = New-Object -TypeName "Microsoft.ApplicationInsights.DataContracts.ExceptionTelemetry"
     $telemtryException.Exception = $_.Exception
     $telemetryClient.TrackException($telemtryException)
 }
 finally
 {
-    Write-Output "Flushing to output stream"
+    Write-Host "Flushing to output stream"
     $telemetryClient.Flush()
-    Write-Output "Finished writing message to App Insights"
+    Write-Host "Finished writing message to App Insights"
 }
