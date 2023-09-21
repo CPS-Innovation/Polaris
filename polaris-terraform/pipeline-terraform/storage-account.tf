655af4e3d0f762df6b92e7a1dc77540ec5418394
aceltuk <mark.jones3@cps.gov.uk>
2023-09-21 18:46:13 +0100

    Removed file and queue sub-resources for private endpoints as they are not supported in Premium storage accounts

--- polaris-terraform/pipeline-terraform/storage-account.tf
+++ polaris-terraform/pipeline-terraform/storage-account.tf
@@ -426,48 +426,6 @@ resource "azurerm_private_endpoint" "pipeline_sa_coordinator_table_pe" {
   }
 }
 
-# Create Private Endpoint for Files
-resource "azurerm_private_endpoint" "pipeline_sa_coordinator_file_pe" {
-  name                = "sacps${var.env != "prod" ? var.env : ""}coordinator-file-pe"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
-  tags                = local.common_tags
-
-  private_dns_zone_group {
-    name                 = "polaris-dns-zone-group"
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_file_storage.id]
-  }
-
-  private_service_connection {
-    name                           = "sacps${var.env != "prod" ? var.env : ""}coordinator-file-psc"
-    private_connection_resource_id = azurerm_storage_account.sa_coordinator.id
-    is_manual_connection           = false
-    subresource_names              = ["file"]
-  }
-}
-
-# Create Private Endpoint for Queues
-resource "azurerm_private_endpoint" "pipeline_sa_coordinator_queue_pe" {
-  name                = "sacps${var.env != "prod" ? var.env : ""}coordinator-queue-pe"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
-  tags                = local.common_tags
-
-  private_dns_zone_group {
-    name                 = "polaris-dns-zone-group"
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_queue_storage.id]
-  }
-
-  private_service_connection {
-    name                           = "sacps${var.env != "prod" ? var.env : ""}coordinator-queue-psc"
-    private_connection_resource_id = azurerm_storage_account.sa_coordinator.id
-    is_manual_connection           = false
-    subresource_names              = ["queue"]
-  }
-}
-
 # PDF Generator Storage Account Private Endpoint and DNS Config
 # Create Private Endpoint for Blobs
 resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_blob_pe" {
