d8ff26f82c914fc5734e7065d6212973d390c37a
Mark Jones <aceltuk@users.noreply.github.com>
2023-10-02 15:52:47 +0100

    Removed zone-balancing statement and added Checkov rule-check exclusion (#586)

--- polaris-terraform/ui-terraform/app-service-plan.tf
+++ polaris-terraform/ui-terraform/app-service-plan.tf
@@ -2,13 +2,13 @@
 
 resource "azurerm_service_plan" "asp_polaris" {
   #checkov:skip=CKV_AZURE_212:Ensure App Service has a minimum number of instances for fail over
-  name                   = "asp-${local.resource_name}"
-  location               = azurerm_resource_group.rg_polaris.location
-  resource_group_name    = azurerm_resource_group.rg_polaris.name
-  os_type                = "Linux"
-  sku_name               = var.app_service_plan_web_sku
-  zone_balancing_enabled = true
-  tags                   = local.common_tags
+  #checkov:skip=CKV_AZURE_225:Ensure the App Service Plan is zone redundant
+  name                = "asp-${local.resource_name}"
+  location            = azurerm_resource_group.rg_polaris.location
+  resource_group_name = azurerm_resource_group.rg_polaris.name
+  os_type             = "Linux"
+  sku_name            = var.app_service_plan_web_sku
+  tags                = local.common_tags
 }
 
 resource "azurerm_monitor_autoscale_setting" "amas_polaris" {
--- polaris-terraform/ui-terraform/proxy-service-plan.tf
+++ polaris-terraform/ui-terraform/proxy-service-plan.tf
@@ -2,13 +2,13 @@
 
 resource "azurerm_service_plan" "asp_polaris_proxy" {
   #checkov:skip=CKV_AZURE_212:Ensure App Service has a minimum number of instances for fail over
-  name                   = "asp-${local.resource_name}-proxy"
-  location               = azurerm_resource_group.rg_polaris.location
-  resource_group_name    = azurerm_resource_group.rg_polaris.name
-  os_type                = "Linux"
-  sku_name               = var.app_service_plan_proxy_sku
-  zone_balancing_enabled = true
-  tags                   = local.common_tags
+  #checkov:skip=CKV_AZURE_225:Ensure the App Service Plan is zone redundant
+  name                = "asp-${local.resource_name}-proxy"
+  location            = azurerm_resource_group.rg_polaris.location
+  resource_group_name = azurerm_resource_group.rg_polaris.name
+  os_type             = "Linux"
+  sku_name            = var.app_service_plan_proxy_sku
+  tags                = local.common_tags
 }
 
 resource "azurerm_monitor_autoscale_setting" "amas_polaris_proxy" {
