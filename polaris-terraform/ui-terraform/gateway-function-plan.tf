0f4a04ed84bd048f7f9834412b7bde41cca7c391
aceltuk <mark.jones3@cps.gov.uk>
2023-10-02 15:19:37 +0100

    Removed zone-balancing statement and added Checkov rule-check exclusion

--- polaris-terraform/ui-terraform/gateway-function-plan.tf
+++ polaris-terraform/ui-terraform/gateway-function-plan.tf
@@ -2,12 +2,12 @@
 
 resource "azurerm_service_plan" "asp_polaris_gateway" {
   #checkov:skip=CKV_AZURE_212:Ensure App Service has a minimum number of instances for fail over
+  #checkov:skip=CKV_AZURE_225:Ensure the App Service Plan is zone redundant
   name                         = "asp-${local.resource_name}-gateway"
   location                     = azurerm_resource_group.rg_polaris.location
   resource_group_name          = azurerm_resource_group.rg_polaris.name
   os_type                      = "Linux"
   sku_name                     = var.app_service_plan_gateway_sku
   tags                         = local.common_tags
-  zone_balancing_enabled       = true
   maximum_elastic_worker_count = 10
 }
