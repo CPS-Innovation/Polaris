b02bfcf41cd413ddb7e80fee9862dc40149d2aa0
aceltuk <mark.jones3@cps.gov.uk>
2023-10-03 15:40:39 +0100

    Another try to get .NET installed, removing legacy sources

--- polaris-devops-pipelines/scale-set/build-agent.sh
+++ polaris-devops-pipelines/scale-set/build-agent.sh
@@ -1,46 +1,52 @@
-﻿echo '==== Add Microsoft package source ===='
+﻿echo '==== Cleaning up inherited package sources first ===='
+sudo apt remove 'dotnet*'
+sudo apt remove 'aspnetcore*'
+sudo rm /etc/apt/sources.list.d/microsoft-prod.list
+sudo apt update
+
+echo '==== Add Microsoft package source ===='
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i ./packages-microsoft-prod.deb
+sudo dpkg -i ./packages-microsoft-prod.deb
 rm ./packages-microsoft-prod.deb
-sudo apt-get update
+sudo apt update
 
 echo '==== Update from package sources ===='
-sudo apt-get upgrade -y
+sudo apt upgrade -y
 
 echo '==== Install dependencies ===='
-DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata
-
-echo '==== PowerShell ===='
-sudo apt-get update && apt-get install -y powershell
-
-echo '==== Utils ===='
-sudo apt-get update -yq
-sudo apt-get install -y wget 
-sudo apt-get install -y apt-utils 
-sudo apt-get install -y apt-transport-https 
-sudo apt-get install -y git
-sudo apt-get install -y ca-certificates
-sudo apt-get install -y curl
-sudo apt-get install -y iputils-ping
-sudo apt-get install -y jq
-sudo apt-get install -y lsb-release
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y zip
-sudo apt-get install -y unzip 
-sudo apt-get clean
-
-echo '==== Azure CLI ===='
-curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt -y install tzdata
 
 echo '==== dotnet 3.1 ===='
 # dotnet 3.1 is used by azure devops itself
-sudo apt-get update && sudo apt-get install -y dotnet-sdk-3.1
+sudo apt update && sudo apt install -y dotnet-sdk-3.1
 
 echo '==== dotnet 6 ===='
-sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
+sudo apt update && sudo apt install -y dotnet-sdk-6.0
 
 echo '==== dotnet 7 ===='
-sudo apt-get update && sudo apt-get install -y dotnet-sdk-7.0
+sudo apt update && sudo apt install -y dotnet-sdk-7.0
+
+echo '==== PowerShell ===='
+sudo apt update && apt install -y powershell
+
+echo '==== Utils ===='
+sudo apt update -yq
+sudo apt install -y wget 
+sudo apt install -y apt-utils 
+sudo apt install -y apt-transport-https 
+sudo apt install -y git
+sudo apt install -y ca-certificates
+sudo apt install -y curl
+sudo apt install -y iputils-ping
+sudo apt install -y jq
+sudo apt install -y lsb-release
+sudo apt install -y software-properties-common
+sudo apt install -y zip
+sudo apt install -y unzip 
+sudo apt clean
+
+echo '==== Azure CLI ===='
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
 sudo apt install libc6
 sudo apt install libgcc1
@@ -54,22 +60,22 @@ sudo apt install libunwind8
 sudo apt install zlib1g
 
 echo '==== Node.js, related utils and Python3 ===='
-sudo apt-get update -yq
-sudo apt-get -yq install curl gnupg ca-certificates
+sudo apt update -yq
+sudo apt -yq install curl gnupg ca-certificates
 curl -L https://deb.nodesource.com/setup_18.x | bash
-sudo apt-get install -y npm 
-sudo apt-get install -y nodejs
-sudo apt-get install -y build-essential
-sudo apt-get install -y libgtk2.0-0
-sudo apt-get install -y libgtk-3-0
-sudo apt-get install -y libgbm-dev
-sudo apt-get install -y libnotify-dev
-sudo apt-get install -y libgconf-2-4
-sudo apt-get install -y libnss3
-sudo apt-get install -y libxss1
-sudo apt-get install -y libasound2
-sudo apt-get install -y libxtst6
-sudo apt-get install -y xauth
-sudo apt-get install -y xvfb
-sudo apt-get install -y python3-pip
-sudo apt-get clean
+sudo apt install -y npm 
+sudo apt install -y nodejs
+sudo apt install -y build-essential
+sudo apt install -y libgtk2.0-0
+sudo apt install -y libgtk-3-0
+sudo apt install -y libgbm-dev
+sudo apt install -y libnotify-dev
+sudo apt install -y libgconf-2-4
+sudo apt install -y libnss3
+sudo apt install -y libxss1
+sudo apt install -y libasound2
+sudo apt install -y libxtst6
+sudo apt install -y xauth
+sudo apt install -y xvfb
+sudo apt install -y python3-pip
+sudo apt clean
