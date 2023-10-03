2cda990077fce4b95b396f71ce28c7cf14c75425
aceltuk <mark.jones3@cps.gov.uk>
2023-10-03 14:49:25 +0100

    Tweaked build image

--- polaris-devops-pipelines/scale-set/build-agent.sh
+++ polaris-devops-pipelines/scale-set/build-agent.sh
@@ -2,37 +2,74 @@
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i ./packages-microsoft-prod.deb
 rm ./packages-microsoft-prod.deb
-apt-get update
+sudo apt-get update
 
 echo '==== Update from package sources ===='
-apt-get upgrade -y
+sudo apt-get upgrade -y
 
 echo '==== Install dependencies ===='
 DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata
-apt-get install -y wget apt-utils apt-transport-https software-properties-common git gnupg ca-certificates curl iputils-ping jq lsb-release zip unzip
-apt-get clean
 
 echo '==== PowerShell ===='
-apt-get update && apt-get install -y powershell
+sudo apt-get update && apt-get install -y powershell
+
+echo '==== Utils ===='
+sudo apt-get update -yq
+sudo apt-get install -y wget 
+sudo apt-get install -y apt-utils 
+sudo apt-get install -y apt-transport-https 
+sudo apt-get install -y git
+sudo apt-get install -y ca-certificates
+sudo apt-get install -y curl
+sudo apt-get install -y iputils-ping
+sudo apt-get install -y jq
+sudo apt-get install -y lsb-release
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y zip
+sudo apt-get install -y unzip 
+sudo apt-get clean
 
 echo '==== Azure CLI ===='
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
 echo '==== dotnet 3.1 ===='
 # dotnet 3.1 is used by azure devops itself
-apt-get install -y dotnet-sdk-3.1
+sudo apt-get update && sudo apt-get install -y dotnet-sdk-3.1
 
 echo '==== dotnet 6 ===='
-apt-get install -y dotnet-sdk-6.0
+sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
 
 echo '==== dotnet 7 ===='
-apt-get install -y dotnet-sdk-7.0
+sudo apt-get update && sudo apt-get install -y dotnet-sdk-7.0
 
-echo '==== Node.js and Python3 ===='
-apt-get update -yq
-curl -sL https://deb.nodesource.com/setup_18.x | bash
-apt-get install -y npm 
-apt-get install -y nodejs
-apt-get install -y build-essential
-apt-get install -y python3-pip
-apt-get clean
+sudo apt install libc6
+sudo apt install libgcc1
+sudo apt install libgcc-s1
+sudo apt install libgssapi-krb5-2
+sudo apt install libicu70
+sudo apt install liblttng-ust1
+sudo apt install libssl3
+sudo apt install libstdc++6
+sudo apt install libunwind8
+sudo apt install zlib1g
+
+echo '==== Node.js, related utils and Python3 ===='
+sudo apt-get update -yq
+sudo apt-get -yq install curl gnupg ca-certificates
+curl -L https://deb.nodesource.com/setup_18.x | bash
+sudo apt-get install -y npm 
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+sudo apt-get install -y libgtk2.0-0
+sudo apt-get install -y libgtk-3-0
+sudo apt-get install -y libgbm-dev
+sudo apt-get install -y libnotify-dev
+sudo apt-get install -y libgconf-2-4
+sudo apt-get install -y libnss3
+sudo apt-get install -y libxss1
+sudo apt-get install -y libasound2
+sudo apt-get install -y libxtst6
+sudo apt-get install -y xauth
+sudo apt-get install -y xvfb
+sudo apt-get install -y python3-pip
+sudo apt-get clean
