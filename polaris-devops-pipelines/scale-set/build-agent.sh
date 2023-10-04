932b7c3e21dbe20fbdc1d9e1db1e84fe98ed0d5a
aceltuk <mark.jones3@cps.gov.uk>
2023-10-04 12:54:41 +0100

    Added sudo to mono call

--- polaris-devops-pipelines/scale-set/build-agent.sh
+++ polaris-devops-pipelines/scale-set/build-agent.sh
@@ -84,5 +84,5 @@ echo '==== Installing NuGet via Mono and Microsoft.ApplicationInsights ===='
 sudo apt-get install -y mono-complete
 sudo wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -O /usr/local/bin/nuget
 sudo chmod +x /usr/local/bin/nuget.exe
-mono nuget.exe install Microsoft.ApplicationInsights
+sudo mono nuget.exe install Microsoft.ApplicationInsights
 sudo apt-get clean
