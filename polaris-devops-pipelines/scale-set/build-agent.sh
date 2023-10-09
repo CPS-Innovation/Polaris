2145f07b5bd492f24d2f0e5e4f612837ef18006e
aceltuk <mark.jones3@cps.gov.uk>
2023-10-09 14:15:06 +0100

    Removed docker and kubectl - not needed

--- polaris-devops-pipelines/scale-set/build-agent.sh
+++ polaris-devops-pipelines/scale-set/build-agent.sh
@@ -107,25 +107,6 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://
 sudo apt-get update -yq && sudo apt-get install -y terraform 
 sudo apt-get clean
 
-echo '==== Docker ===='
-# https://docs.docker.com/engine/install/ubuntu/
-apt-get remove docker docker-engine docker.io containerd runc
-apt-get install ca-certificates curl gnupg lsb-release
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -yq
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-echo '=== Kubectl ==='
-# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
-apt-get install -y ca-certificates curl
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -yq
-sudo apt-get install -y kubectl
-sudo apt-get clean
-
 echo "==== Giving AzDevOps user access to the '/home', '/usr/share', and '/opt' directories ===="
 sudo chmod -R 777 /home
 setfacl -Rdm "u:AzDevOps:rwX" /home
