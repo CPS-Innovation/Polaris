9d66ad67ed467a968eeb71cf3d17acd197f089c9
aceltuk <mark.jones3@cps.gov.uk>
2023-11-08 14:24:41 +0000

    Add sideloaded pipelines

--- polaris-terraform/pipeline-terraform/app-service-plan-coordinator.tf
+++ polaris-terraform/pipeline-terraform/app-service-plan-coordinator.tf
@@ -11,3 +11,15 @@ resource "azurerm_service_plan" "asp_polaris_pipeline_ep_coordinator" {
   tags                         = local.common_tags
   maximum_elastic_worker_count = var.pipeline_component_service_plans.coordinator_plan_maximum_burst
 }
+
+resource "azurerm_service_plan" "asp_polaris_pipeline_ep_coordinator_v2" {
+  #checkov:skip=CKV_AZURE_212:Ensure App Service has a minimum number of instances for fail over
+  #checkov:skip=CKV_AZURE_225:Ensure the App Service Plan is zone redundant
+  name                         = "asp-coordinator-v2-ep-${local.resource_name}"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  os_type                      = "Linux"
+  sku_name                     = var.pipeline_component_service_plans.coordinator_service_plan_sku
+  tags                         = local.common_tags
+  maximum_elastic_worker_count = var.pipeline_component_service_plans.coordinator_plan_maximum_burst
+}
