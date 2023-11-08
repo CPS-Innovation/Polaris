567c8502d7615b7de30debb0a49b10ae7930458e
aceltuk <mark.jones3@cps.gov.uk>
2023-11-08 14:27:02 +0000

    Restored original versions of pipeline components

---
+++ polaris-terraform/pipeline-terraform/functions-text-extractor.tf
@@ -0,0 +1,128 @@
+#################### Functions ####################
+
+resource "azurerm_linux_function_app" "fa_text_extractor" {
+  name                          = "fa-${local.resource_name}-text-extractor"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  service_plan_id               = azurerm_service_plan.asp_polaris_ep_text_extractor.id
+  storage_account_name          = azurerm_storage_account.sa_text_extractor.name
+  storage_account_access_key    = azurerm_storage_account.sa_text_extractor.primary_access_key
+  virtual_network_subnet_id     = data.azurerm_subnet.polaris_textextractor_subnet.id
+  tags                          = local.common_tags
+  functions_extension_version   = "~4"
+  https_only                    = true
+  public_network_access_enabled = false
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"                 = "dotnet"
+    "FUNCTIONS_EXTENSION_VERSION"              = "~4"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE"      = "false"
+    "WEBSITE_ENABLE_SYNC_UPDATE_SITE"          = "true"
+    "WEBSITE_CONTENTOVERVNET"                  = "1"
+    "WEBSITE_RUN_FROM_PACKAGE"                 = "1"
+    "WEBSITE_DNS_SERVER"                       = var.dns_server
+    "WEBSITE_DNS_ALT_SERVER"                   = "168.63.129.16"
+    "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = azurerm_storage_account.sa_text_extractor.primary_connection_string
+    "WEBSITE_CONTENTSHARE"                     = azapi_resource.pipeline_sa_text_extractor_file_share.name
+    "SCALE_CONTROLLER_LOGGING_ENABLED"         = var.pipeline_logging.text_extractor_scale_controller
+    "AzureWebJobsStorage"                      = azurerm_storage_account.sa_text_extractor.primary_connection_string
+    "ComputerVisionClientServiceKey"           = azurerm_cognitive_account.computer_vision_service.primary_access_key
+    "ComputerVisionClientServiceUrl"           = azurerm_cognitive_account.computer_vision_service.endpoint
+    "SearchClientAuthorizationKey"             = azurerm_search_service.ss.primary_key
+    "SearchClientEndpointUrl"                  = "https://${azurerm_search_service.ss.name}.search.windows.net"
+    "SearchClientIndexName"                    = jsondecode(file("search-index-definition.json")).name
+  }
+
+  site_config {
+    ftps_state                             = "FtpsOnly"
+    http2_enabled                          = true
+    runtime_scale_monitoring_enabled       = true
+    vnet_route_all_enabled                 = true
+    elastic_instance_minimum               = var.pipeline_component_service_plans.text_extractor_always_ready_instances
+    app_scale_limit                        = var.pipeline_component_service_plans.text_extractor_maximum_scale_out_limit
+    application_insights_connection_string = data.azurerm_application_insights.global_ai.connection_string
+    application_insights_key               = data.azurerm_application_insights.global_ai.instrumentation_key
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  auth_settings {
+    enabled                       = false
+    issuer                        = "https://sts.windows.net/${data.azurerm_client_config.current.tenant_id}/"
+    unauthenticated_client_action = "AllowAnonymous"
+  }
+}
+
+module "azurerm_app_reg_fa_text_extractor" {
+  source                  = "./modules/terraform-azurerm-azuread-app-registration"
+  display_name            = "fa-${local.resource_name}-text-extractor-appreg"
+  identifier_uris         = ["api://fa-${local.resource_name}-text-extractor"]
+  prevent_duplicate_names = true
+  #use this code for adding app_roles
+  /*app_role = [
+    {
+      allowed_member_types = ["Application"]
+      description          = "Can parse document texts using the ${local.resource_name} Polaris Text Extractor"
+      display_name         = "Parse document texts in ${local.resource_name}"
+      id                   = element(random_uuid.random_id[*].result, 3)
+      value                = "application.extracttext"
+    }
+  ]*/
+  #use this code for adding api permissions
+  required_resource_access = [{
+    # Microsoft Graph
+    resource_app_id = "00000003-0000-0000-c000-000000000000"
+    resource_access = [{
+      # User.Read
+      id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d"
+      type = "Scope"
+    }]
+  }]
+
+  tags = ["terraform"]
+}
+
+resource "azuread_application_password" "faap_fa_text_extractor_app_service" {
+  application_object_id = module.azurerm_app_reg_fa_text_extractor.object_id
+  end_date_relative     = "17520h"
+}
+
+module "azurerm_service_principal_fa_text_extractor" {
+  source                       = "./modules/terraform-azurerm-azuread_service_principal"
+  application_id               = module.azurerm_app_reg_fa_text_extractor.client_id
+  app_role_assignment_required = false
+  owners                       = [data.azurerm_client_config.current.object_id]
+}
+
+resource "azuread_service_principal_password" "sp_fa_text_extractor_pw" {
+  service_principal_id = module.azurerm_service_principal_fa_text_extractor.object_id
+}
+
+resource "azuread_service_principal_delegated_permission_grant" "polaris_text_extractor_grant_access_to_msgraph" {
+  service_principal_object_id          = module.azurerm_service_principal_fa_text_extractor.object_id
+  resource_service_principal_object_id = azuread_service_principal.msgraph.object_id
+  claim_values                         = ["User.Read"]
+}
+
+# Create Private Endpoint
+resource "azurerm_private_endpoint" "pipeline_text_extractor_pe" {
+  name                = "${azurerm_linux_function_app.fa_text_extractor.name}-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = data.azurerm_subnet.polaris_apps_subnet.id
+  tags                = local.common_tags
+
+  private_dns_zone_group {
+    name                 = data.azurerm_private_dns_zone.dns_zone_apps.name
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_zone_apps.id]
+  }
+
+  private_service_connection {
+    name                           = "${azurerm_linux_function_app.fa_text_extractor.name}-psc"
+    private_connection_resource_id = azurerm_linux_function_app.fa_text_extractor.id
+    is_manual_connection           = false
+    subresource_names              = ["sites"]
+  }
+}
