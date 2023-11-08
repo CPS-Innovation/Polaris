1417e3959dc8148667797628571fe11fa4964951
aceltuk <mark.jones3@cps.gov.uk>
2023-11-08 15:44:25 +0000

    Revert changes

--- polaris-terraform/pipeline-terraform/app-service-plan-text-extractor.tf
+++ polaris-terraform/pipeline-terraform/app-service-plan-text-extractor.tf
@@ -11,15 +11,3 @@ resource "azurerm_service_plan" "asp_polaris_ep_text_extractor" {
   tags                         = local.common_tags
   maximum_elastic_worker_count = var.pipeline_component_service_plans.text_extractor_plan_maximum_burst
 }
-
-resource "azurerm_service_plan" "asp_polaris_ep_text_extractor_v2" {
-  #checkov:skip=CKV_AZURE_212:Ensure App Service has a minimum number of instances for fail over
-  #checkov:skip=CKV_AZURE_225:Ensure the App Service Plan is zone redundant
-  name                         = "asp-text-extractor-v2-ep-${local.resource_name}"
-  location                     = azurerm_resource_group.rg.location
-  resource_group_name          = azurerm_resource_group.rg.name
-  os_type                      = "Linux"
-  sku_name                     = var.pipeline_component_service_plans.text_extractor_plan_sku
-  tags                         = local.common_tags
-  maximum_elastic_worker_count = var.pipeline_component_service_plans.text_extractor_plan_maximum_burst
-}
