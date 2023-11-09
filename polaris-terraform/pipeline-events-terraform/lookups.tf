ed4ce4842176c497d9c93b7dd26ef59bed49a8c4
aceltuk <mark.jones3@cps.gov.uk>
2023-11-09 15:14:45 +0000

    Remove unnecessary additional permissions - apps in the slot will not need them at present

--- polaris-terraform/pipeline-events-terraform/lookups.tf
+++ polaris-terraform/pipeline-events-terraform/lookups.tf
@@ -13,16 +13,6 @@ data "azurerm_linux_function_app" "fa_text_extractor" {
   resource_group_name = "rg-${local.pipeline_resource_name}"
 }
 
-data "azurerm_linux_function_app" "fa_text_extractor_staging1" {
-  name                = "fa-${local.polaris_resource_name}-text-extractor-staging1"
-  resource_group_name = "rg-${local.pipeline_resource_name}"
-}
-
-data "azurerm_linux_function_app" "fa_text_extractor_staging2" {
-  name                = "fa-${local.polaris_resource_name}-text-extractor-staging2"
-  resource_group_name = "rg-${local.pipeline_resource_name}"
-}
-
 data "azurerm_storage_container" "pipeline_storage_container" {
   name                 = "documents"
   storage_account_name = "sacps${var.env != "prod" ? var.env : ""}polarispipeline"
--- polaris-terraform/pipeline-events-terraform/storage-account.tf
+++ polaris-terraform/pipeline-events-terraform/storage-account.tf
@@ -5,18 +5,6 @@ resource "azurerm_role_assignment" "ra_blob_delegator_text_extractor" {
   principal_id         = data.azurerm_linux_function_app.fa_text_extractor.identity[0].principal_id
 }
 
-resource "azurerm_role_assignment" "ra_blob_delegator_text_extractor_staging1" {
-  scope                = data.azurerm_storage_account.pipeline_storage_account.id
-  role_definition_name = "Storage Blob Delegator"
-  principal_id         = data.azurerm_linux_function_app.fa_text_extractor_staging1.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "ra_blob_delegator_text_extractor_staging2" {
-  scope                = data.azurerm_storage_account.pipeline_storage_account.id
-  role_definition_name = "Storage Blob Delegator"
-  principal_id         = data.azurerm_linux_function_app.fa_text_extractor_staging2.identity[0].principal_id
-}
-
 resource "azurerm_role_assignment" "ra_blob_delegator_coordinator" {
   scope                = data.azurerm_storage_account.pipeline_storage_account.id
   role_definition_name = "Storage Blob Delegator"
@@ -33,18 +21,6 @@ resource "azurerm_role_assignment" "ra_blob_data_contributor_text_extractor" {
   scope                = data.azurerm_storage_container.pipeline_storage_container.resource_manager_id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = data.azurerm_linux_function_app.fa_text_extractor.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "ra_blob_data_contributor_text_extractor_staging1" {
-  scope                = data.azurerm_storage_container.pipeline_storage_container.resource_manager_id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azurerm_linux_function_app.fa_text_extractor_staging1.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "ra_blob_data_contributor_text_extractor_staging2" {
-  scope                = data.azurerm_storage_container.pipeline_storage_container.resource_manager_id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azurerm_linux_function_app.fa_text_extractor_staging2.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "ra_blob_data_reader_computer_vision_account" {
