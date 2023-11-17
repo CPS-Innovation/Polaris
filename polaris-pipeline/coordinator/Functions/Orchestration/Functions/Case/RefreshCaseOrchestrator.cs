9aa03973c975f4f53b935ad4a60dca3c7c5c4514
Stef <stef@stachow.co.uk>
2023-11-17 20:42:15 +0000

    Revert "Revert "Bugfix/24476 coorindator performance (#680)" (#683)"
    This reverts commit 2400f5d1f6c0d84dba4114d92d67457f7a34e620.

--- polaris-pipeline/coordinator/Functions/Orchestration/Functions/Case/RefreshCaseOrchestrator.cs
+++ polaris-pipeline/coordinator/Functions/Orchestration/Functions/Case/RefreshCaseOrchestrator.cs
@@ -18,6 +18,7 @@ using coordinator.Functions.ActivityFunctions.Case;
 using coordinator.Functions.DurableEntity.Entity.Contract;
 using coordinator.Functions.Orchestration.Functions.Document;
 using coordinator.TelemetryEvents;
+using coordinator.Validators;
 using Mapster;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
@@ -30,9 +31,9 @@ namespace coordinator.Functions.Orchestration.Functions.Case
     {
         private readonly ILogger<RefreshCaseOrchestrator> _log;
         private readonly IConfiguration _configuration;
+        private readonly ICmsDocumentsResponseValidator _cmsDocumentsResponseValidator;
         private readonly ITelemetryClient _telemetryClient;
         private readonly TimeSpan _timeout;
-
         const string loggingName = $"{nameof(RefreshCaseOrchestrator)} - {nameof(Run)}";
 
         public static string GetKey(string caseId)
@@ -40,10 +41,15 @@ namespace coordinator.Functions.Orchestration.Functions.Case
             return $"[{caseId}]";
         }
 
-        public RefreshCaseOrchestrator(ILogger<RefreshCaseOrchestrator> log, IConfiguration configuration, ITelemetryClient telemetryClient)
+        public RefreshCaseOrchestrator(
+            ILogger<RefreshCaseOrchestrator> log,
+            IConfiguration configuration,
+            ICmsDocumentsResponseValidator cmsDocumentsResponseValidator,
+            ITelemetryClient telemetryClient)
         {
             _log = log;
             _configuration = configuration;
+            _cmsDocumentsResponseValidator = cmsDocumentsResponseValidator;
             _telemetryClient = telemetryClient;
             _timeout = TimeSpan.FromSeconds(double.Parse(_configuration[ConfigKeys.CoordinatorKeys.CoordinatorOrchestratorTimeoutSecs]));
         }
@@ -60,10 +66,18 @@ namespace coordinator.Functions.Orchestration.Functions.Case
             var caseEntity = await CreateOrGetCaseDurableEntity(context, payload.CmsCaseId, true, payload.CorrelationId, log);
             caseEntity.SetCaseStatus((context.CurrentUtcDateTime, CaseRefreshStatus.Running, null));
 
+            RefreshedCaseEvent telemetryEvent = default;
             try
             {
+                telemetryEvent = new RefreshedCaseEvent(
+                    correlationId: payload.CorrelationId,
+                    caseId: payload.CmsCaseId,
+                    versionId: await caseEntity.GetVersion(),
+                    startTime: await caseEntity.GetStartTime()
+                );
+
                 log.LogMethodFlow(payload.CorrelationId, loggingName, $"Run main orchestration for case {payload.CmsCaseId}");
-                var orchestratorTask = RunCaseOrchestrator(context, caseEntity, payload);
+                var orchestratorTask = RunCaseOrchestrator(context, caseEntity, payload, telemetryEvent);
 
                 using var cts = new CancellationTokenSource();
                 var deadline = context.CurrentUtcDateTime.Add(_timeout);
@@ -74,6 +88,7 @@ namespace coordinator.Functions.Orchestration.Functions.Case
                 {
                     // success case
                     cts.Cancel();
+                    _telemetryClient.TrackEvent(telemetryEvent);
                     return await orchestratorTask;
                 }
 
@@ -84,6 +99,7 @@ namespace coordinator.Functions.Orchestration.Functions.Case
                 caseEntity.SetCaseStatus((context.CurrentUtcDateTime, CaseRefreshStatus.Failed, exception.Message));
 
                 log.LogMethodError(payload.CorrelationId, loggingName, $"Error when running {nameof(RefreshCaseOrchestrator)} orchestration with id '{context.InstanceId}'", exception);
+                _telemetryClient.TrackEventFailure(telemetryEvent);
                 throw;
             }
             finally
@@ -92,52 +108,35 @@ namespace coordinator.Functions.Orchestration.Functions.Case
             }
         }
 
-        private async Task<TrackerDto> RunCaseOrchestrator(IDurableOrchestrationContext context, ICaseDurableEntity caseEntity, CaseOrchestrationPayload payload)
+        private async Task<TrackerDto> RunCaseOrchestrator(IDurableOrchestrationContext context, ICaseDurableEntity caseEntity, CaseOrchestrationPayload payload, RefreshedCaseEvent telemetryEvent)
         {
             const string loggingName = nameof(RunCaseOrchestrator);
-            RefreshedCaseEvent telemetryEvent = default;
-            try
-            {
-                telemetryEvent = new RefreshedCaseEvent(
-                    correlationId: payload.CorrelationId,
-                    caseId: payload.CmsCaseId,
-                    versionId: await caseEntity.GetVersion(),
-                    startTime: await caseEntity.GetStartTime()
-                );
-                var log = context.CreateReplaySafeLogger(_log);
 
-                log.LogMethodEntry(payload.CorrelationId, loggingName, payload.ToJson());
-                log.LogMethodFlow(payload.CorrelationId, loggingName, $"Resetting case entity for {context.InstanceId}");
-                caseEntity.Reset(context.InstanceId);
-                caseEntity.SetCaseStatus((context.CurrentUtcDateTime, CaseRefreshStatus.Running, null));
+            var log = context.CreateReplaySafeLogger(_log);
 
-                var documents = await GetDocuments(context, loggingName, log, payload);
-                telemetryEvent.CmsDocsCount = documents.CmsDocuments.Length;
-                caseEntity.SetCaseStatus((context.CurrentUtcDateTime, CaseRefreshStatus.DocumentsRetrieved, null));
+            log.LogMethodEntry(payload.CorrelationId, loggingName, payload.ToJson());
+            log.LogMethodFlow(payload.CorrelationId, loggingName, $"Resetting case entity for {context.InstanceId}");
+            caseEntity.Reset(context.InstanceId);
+            caseEntity.SetCaseStatus((context.CurrentUtcDateTime, CaseRefreshStatus.Running, null));
 
-                var (documentTasks, cmsDocsProcessedCount, pcdRequestsProcessedCount) = await GetDocumentTasks(context, caseEntity, payload, documents, log);
-                telemetryEvent.CmsDocsProcessedCount = cmsDocsProcessedCount;
-                telemetryEvent.PcdRequestsProcessedCount = pcdRequestsProcessedCount;
-                await Task.WhenAll(documentTasks.Select(BufferCall));
+            var documents = await GetDocuments(context, loggingName, log, payload);
+            telemetryEvent.CmsDocsCount = documents.CmsDocuments.Length;
+            caseEntity.SetCaseStatus((context.CurrentUtcDateTime, CaseRefreshStatus.DocumentsRetrieved, null));
 
-                if (await caseEntity.AllDocumentsFailed())
-                    throw new CaseOrchestrationException("CMS Documents, PCD Requests or Defendants and Charges failed to process during orchestration.");
+            var (documentTasks, cmsDocsProcessedCount, pcdRequestsProcessedCount) = await GetDocumentTasks(context, caseEntity, payload, documents, log);
+            telemetryEvent.CmsDocsProcessedCount = cmsDocsProcessedCount;
+            telemetryEvent.PcdRequestsProcessedCount = pcdRequestsProcessedCount;
+            await Task.WhenAll(documentTasks.Select(BufferCall));
 
-                caseEntity.SetCaseStatus((context.CurrentUtcDateTime, CaseRefreshStatus.Completed, null));
+            if (await caseEntity.AllDocumentsFailed())
+                throw new CaseOrchestrationException("CMS Documents, PCD Requests or Defendants and Charges failed to process during orchestration.");
 
-                log.LogMethodExit(payload.CorrelationId, loggingName, "Returning tracker");
-                telemetryEvent.EndTime = context.CurrentUtcDateTime;
+            caseEntity.SetCaseStatus((context.CurrentUtcDateTime, CaseRefreshStatus.Completed, null));
 
-                var trackerDto = caseEntity.Adapt<TrackerDto>();
-                _telemetryClient.TrackEvent(telemetryEvent);
-                return trackerDto;
+            log.LogMethodExit(payload.CorrelationId, loggingName, "Returning tracker");
+            telemetryEvent.EndTime = context.CurrentUtcDateTime;
 
-            }
-            catch (Exception)
-            {
-                _telemetryClient.TrackEventFailure(telemetryEvent);
-                throw;
-            }
+            return caseEntity.Adapt<TrackerDto>();
         }
 
         private async static Task<(List<Task>, int, int)> GetDocumentTasks
@@ -247,7 +246,10 @@ namespace coordinator.Functions.Orchestration.Functions.Case
 
             var getCaseEntitiesActivityPayload = new GetCaseDocumentsActivityPayload(payload.CmsCaseUrn, payload.CmsCaseId, payload.CmsAuthValues, payload.CorrelationId);
             var documents = await context.CallActivityAsync<(CmsDocumentDto[] CmsDocuments, PcdRequestDto[] PcdRequests, DefendantsAndChargesListDto DefendantsAndCharges)>(nameof(GetCaseDocuments), getCaseEntitiesActivityPayload);
-
+            if (!_cmsDocumentsResponseValidator.Validate(documents.CmsDocuments))
+            {
+                throw new CaseOrchestrationException("Invalid cms documents response: duplicate document ids detected.");
+            }
             return documents;
         }
     }
