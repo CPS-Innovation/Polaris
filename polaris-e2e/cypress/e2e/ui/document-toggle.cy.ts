93cec8fc14aa52b680d6c503098dd96e6368af75
Stef Stachow <stef@stachow.co.uk>
2023-11-27 15:26:37 +0000

    Reinstate e2e tests

--- polaris-e2e/cypress/e2e/ui/document-toggle.cy.ts
+++ polaris-e2e/cypress/e2e/ui/document-toggle.cy.ts
@@ -24,31 +24,32 @@ describe("Document toggle", () => {
     // open case details page
     cy.findByTestId("btn-accordion-open-close-all").click()
 
-    // cy.findByText(TARGET_CAN_REDACT_DOCUMENT_NAME).click()
-    // cy.selectPDFTextElement("12345")
-    // cy.findByTestId("btn-redact")
-    // cy.findByTestId("tab-remove").click()
+    cy.findByText(TARGET_CAN_REDACT_DOCUMENT_NAME).click()
+    cy.selectPDFTextElement("12345")
+    cy.findByTestId("btn-redact")
+    cy.findByTestId("tab-remove").click()
 
-    // cy.findByText(TARGET_NOT_OCR_PROCESSED_DOCUMENT_NAME).click()
-    // cy.selectPDFTextElement("12345")
-    // cy.findByTestId("redaction-warning")
-    // cy.findByTestId("tab-remove").click()
+    cy.findByText(TARGET_NOT_OCR_PROCESSED_DOCUMENT_NAME).click()
+    cy.selectPDFTextElement("12345")
+    cy.findByTestId("redaction-warning")
+    cy.findByTestId("tab-remove").click()
 
-    // cy.findByText(TARGET_NOT_CORRECT_DOC_TYPE_DOCUMENT_NAME).click()
-    // cy.selectPDFTextElement("12345")
-    // cy.findByTestId("redaction-warning")
-    // cy.findByTestId("tab-remove").click()
-
-    // cy.findByText(TARGET_ALREADY_CHECKED_OUT_DOCUMENT_NAME).click()
-    // cy.selectPDFTextElement("12345")
-    // cy.findByTestId("btn-redact").click()
-    // cy.findByTestId("btn-error-modal-ok")
-    // cy.contains("Mock OtherUser")
+    cy.findByText(TARGET_NOT_CORRECT_DOC_TYPE_DOCUMENT_NAME).click()
+    cy.selectPDFTextElement("12345")
+    cy.findByTestId("redaction-warning")
+    cy.findByTestId("tab-remove").click()
 
     cy.findByText(TARGET_DIRECTION_OUT_DOCUMENT_NAME).click()
     cy.selectPDFTextElement("12345")
     cy.findByTestId("redaction-warning")
     cy.findByTestId("tab-remove").click()
+
+    // helpful if this goes last as we don't have to tidy up the UI
+    cy.findByText(TARGET_ALREADY_CHECKED_OUT_DOCUMENT_NAME).click()
+    cy.selectPDFTextElement("12345")
+    cy.findByTestId("btn-redact").click()
+    cy.findByTestId("btn-error-modal-ok")
+    cy.contains("Mock OtherUser")
   })
 })
 
