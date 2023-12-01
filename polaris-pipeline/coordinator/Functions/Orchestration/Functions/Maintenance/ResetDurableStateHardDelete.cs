67811d8ce4c2b01bae1b435af70154497ce3c930
Stef Stachow <stef@stachow.co.uk>
2023-12-01 16:27:55 +0000

    #24676 - add ResetDurableStateHardDelete

--- polaris-pipeline/coordinator/Functions/Orchestration/Functions/Maintenance/ResetDurableStateHardDelete.cs
+++ polaris-pipeline/coordinator/Functions/Orchestration/Functions/Maintenance/ResetDurableStateHardDelete.cs
@@ -4,26 +4,21 @@ using DurableTask.AzureStorage;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Logging;
-
 
 // This code taken from Azure functions durable extension discussion [0]. As per comment [1]
 //  "this is very much a non-graceful purge of all orchestration data."
 //
 // [0]: https://github.com/Azure/azure-functions-durable-extension/discussions/2029
 // [1]: https://github.com/Azure/azure-functions-durable-extension/discussions/2029#discussioncomment-1760004
-
 public class ResetDurableStateHardDelete
 {
     readonly INameResolver _nameResolver;
-    private readonly ILogger<ResetDurableStateHardDelete> _logger;
 
     // INameResolver is a service of the Functions host that can
     // be used to look up app settings.
-    public ResetDurableStateHardDelete(INameResolver nameResolver, ILogger<ResetDurableStateHardDelete> logger)
+    public ResetDurableStateHardDelete(INameResolver nameResolver)
     {
         _nameResolver = nameResolver;
-        _logger = logger;
     }
 
     [FunctionName(nameof(ResetDurableStateHardDelete))]
