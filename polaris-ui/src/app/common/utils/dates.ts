761ee7439e5b7339d669f503c01fbe016ae57ec0
Rhys Bridges <bridgesrhys@gmail.com>
2023-12-19 16:10:09 +0000

    Fix: time formatting using date-fns-tz (#752)
    * fix: prettier issue
    
    * fix: use datefns tz to pin to uk time
    
    * fix: use datefns tz to pin to uk time

--- polaris-ui/src/app/common/utils/dates.ts
+++ polaris-ui/src/app/common/utils/dates.ts
@@ -1,4 +1,7 @@
 import { format, parseISO, differenceInYears, isValid } from "date-fns";
+import { formatInTimeZone } from "date-fns-tz";
+
+import enGB from "date-fns/locale/en-GB";
 
 export const CommonDateTimeFormats = {
   ShortDate: "dd/MM/yyyy",
@@ -10,10 +13,19 @@ export const CommonDateTimeFormats = {
 export const formatDate = (isoDateString: string, dateTimeFormat: string) =>
   isoDateString && format(parseISO(isoDateString), dateTimeFormat);
 
+const formatToUkDateTime = (isoDateString: string) =>
+  isoDateString &&
+  formatInTimeZone(
+    parseISO(isoDateString),
+    "Europe/London",
+    CommonDateTimeFormats.ShortTime,
+    { locale: enGB }
+  );
+
 export const formatTime = (isoDateString: string) => {
   try {
     return isoDateString.includes("T") && isValid(parseISO(isoDateString))
-      ? formatDate(isoDateString, CommonDateTimeFormats.ShortTime)
+      ? formatToUkDateTime(isoDateString)
       : null;
   } catch {
     // fallback to null if parseISO fails to parse the datetime
