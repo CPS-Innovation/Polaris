3e7ccd5acc89d37a91179e09f01ef6abd40870c5
Rhys Bridges <bridgesrhys@gmail.com>
2023-12-19 13:01:45 +0000

    story: 24223 show doc time if available (#745)
    * story: 24223 show doc time if available
    
    * story: 24223 change to utc times

--- polaris-ui/src/app/common/utils/dates.ts
+++ polaris-ui/src/app/common/utils/dates.ts
@@ -1,13 +1,25 @@
-import { format, parseISO, differenceInYears } from "date-fns";
+import { format, parseISO, differenceInYears, isValid } from "date-fns";
 
 export const CommonDateTimeFormats = {
   ShortDate: "dd/MM/yyyy",
   ShortDateTextMonth: "dd MMM yyyy",
   ShortDateFullTextMonth: "dd LLLL yyyy",
+  ShortTime: "HH:mm",
 };
 
 export const formatDate = (isoDateString: string, dateTimeFormat: string) =>
   isoDateString && format(parseISO(isoDateString), dateTimeFormat);
+
+export const formatTime = (isoDateString: string) => {
+  try {
+    return isoDateString.includes("T") && isValid(parseISO(isoDateString))
+      ? formatDate(isoDateString, CommonDateTimeFormats.ShortTime)
+      : null;
+  } catch {
+    // fallback to null if parseISO fails to parse the datetime
+    return null;
+  }
+};
 
 export const getAgeFromIsoDate = (isoDateString: string) =>
   isoDateString && differenceInYears(new Date(), parseISO(isoDateString));
