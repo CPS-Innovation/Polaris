e49b1c49416067f491d5e2a6c08ef11a4ca702cb
Stef <stef@stachow.co.uk>
2023-12-19 17:23:14 +0000

    story: 24223 show doc time if available (#745) (#748)
    * story: 24223 show doc time if available (#745)
    
    * story: 24223 show doc time if available
    
    * story: 24223 change to utc times
    
    * Story/24223 (#749)
    
    * story: 24223 show doc time if available
    
    * story: 24223 change to utc times
    
    * fix: prettier issue
    
    * fix: prettier issue (#751)
    
    * Fix: time formatting using date-fns-tz (#752)
    
    * fix: prettier issue
    
    * fix: use datefns tz to pin to uk time
    
    * fix: use datefns tz to pin to uk time
    
    * fix: cypress serve.json fix (#754)
    
    ---------
    
    Co-authored-by: Rhys Bridges <bridgesrhys@gmail.com>

--- polaris-ui/src/app/common/utils/dates.ts
+++ polaris-ui/src/app/common/utils/dates.ts
@@ -1,13 +1,37 @@
-import { format, parseISO, differenceInYears } from "date-fns";
+import { format, parseISO, differenceInYears, isValid } from "date-fns";
+import { formatInTimeZone } from "date-fns-tz";
+
+import enGB from "date-fns/locale/en-GB";
 
 export const CommonDateTimeFormats = {
   ShortDate: "dd/MM/yyyy",
   ShortDateTextMonth: "dd MMM yyyy",
   ShortDateFullTextMonth: "dd LLLL yyyy",
+  ShortTime: "HH:mm",
 };
 
 export const formatDate = (isoDateString: string, dateTimeFormat: string) =>
   isoDateString && format(parseISO(isoDateString), dateTimeFormat);
+
+const formatToUkDateTime = (isoDateString: string) =>
+  isoDateString &&
+  formatInTimeZone(
+    parseISO(isoDateString),
+    "Europe/London",
+    CommonDateTimeFormats.ShortTime,
+    { locale: enGB }
+  );
+
+export const formatTime = (isoDateString: string) => {
+  try {
+    return isoDateString.includes("T") && isValid(parseISO(isoDateString))
+      ? formatToUkDateTime(isoDateString)
+      : null;
+  } catch {
+    // fallback to null if parseISO fails to parse the datetime
+    return null;
+  }
+};
 
 export const getAgeFromIsoDate = (isoDateString: string) =>
   isoDateString && differenceInYears(new Date(), parseISO(isoDateString));
