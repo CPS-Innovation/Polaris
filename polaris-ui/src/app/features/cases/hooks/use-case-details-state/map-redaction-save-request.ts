2c5382a67a5db4e5ddb252ba8743ca23a95a379b
Stef Stachow <stef@stachow.co.uk>
2023-12-27 21:11:04 +0000

    #24950 - santise redaction requests

--- polaris-ui/src/app/features/cases/hooks/use-case-details-state/map-redaction-save-request.ts
+++ polaris-ui/src/app/features/cases/hooks/use-case-details-state/map-redaction-save-request.ts
@@ -1,51 +1,80 @@
+import { round } from "lodash";
 import { IPdfHighlight } from "../../domain/IPdfHighlight";
 import { RedactionSavePage } from "../../domain/gateway/RedactionSavePage";
 import { RedactionSaveRequest } from "../../domain/gateway/RedactionSaveRequest";
+
+const NUMBER_DECIMAL_PLACES = 2;
 
 export const mapRedactionSaveRequest = (
   documentId: RedactionSaveRequest["documentId"],
   redactionHighlights: IPdfHighlight[]
 ) => {
-  const redactionSaveRequest = redactionHighlights.reduce(
-    (acc, curr) => {
-      const { position, highlightType } = curr;
+  const redactions = [] as RedactionSavePage[];
 
-      let redactionPage = acc.redactions.find(
-        (item) => item.pageIndex === position.pageNumber
-      );
+  for (const redactionHighlight of redactionHighlights) {
+    const { position, highlightType } = redactionHighlight;
+    const { pageNumber: pageIndex } = position;
+    const { height, width } = position.boundingRect;
 
-      const { height, width } = position.boundingRect;
-      if (!redactionPage) {
-        const { pageNumber: pageIndex } = position;
+    const redactionPageExists = redactions.some(
+      (item) => item.pageIndex === position.pageNumber
+    );
 
-        redactionPage = {
-          pageIndex: pageIndex,
-          height,
-          width,
-          redactionCoordinates: [],
-        };
-        acc.redactions.push(redactionPage);
-      }
+    if (!redactionPageExists) {
+      redactions.push({
+        pageIndex,
+        height: roundToFixedDecimalPlaces(height),
+        width: roundToFixedDecimalPlaces(width),
+        redactionCoordinates: [],
+      });
+    }
 
-      const rects =
-        highlightType === "area" ? [position.boundingRect] : position.rects;
+    const redactionPage = redactions.find(
+      (item) => item.pageIndex === position.pageNumber
+    )!;
 
-      redactionPage.redactionCoordinates.push(
-        ...rects.map((item) => ({
-          x1: item.x1,
-          y1: height - item.y1,
-          x2: item.x2,
-          y2: height - item.y2,
-        }))
-      );
+    const rects =
+      highlightType === "area" ? [position.boundingRect] : position.rects;
 
-      return acc;
-    },
-    {
-      documentId,
-      redactions: [] as RedactionSavePage[],
-    } as RedactionSaveRequest
-  );
+    redactionPage.redactionCoordinates.push(
+      ...rects.map((item) => ({
+        x1: item.x1,
+        y1: roundToFixedDecimalPlaces(getSafeCoordinate(height, item.y1)),
+        x2: item.x2,
+        y2: roundToFixedDecimalPlaces(getSafeCoordinate(height, item.y2)),
+      }))
+    );
+  }
 
-  return redactionSaveRequest;
+  return {
+    documentId,
+    redactions,
+  };
 };
+
+const getSafeCoordinate = (height: number, y: number) => {
+  // It is possible for the user to draw an "area" redaction that begins at
+  //  the foot of a page and then continues on to the next page. The UI will
+  //  only indicate the part of the redaction that is on the start page.
+  //  However the coordinates of the end point of the redaction are returned,
+  //  meaning ultimately we pass invalid coordinates to the back end, which do not
+  //  match the visible redaction as presented to the user. So we correct here.
+  if (y > height) {
+    return 0;
+  }
+  // It is possible for the user to draw an "area" redaction that begins at
+  //  the top of a page and then upwards continues on to the preceding page. The UI will
+  //  only indicate the part of the redaction that is on the start page.
+  //  However the coordinates of the end point of the redaction are returned,
+  //  meaning ultimately we pass invalid coordinates to the back end, which do not
+  //  match the visible redaction as presented to the user. So we correct here.
+  if (y < 0) {
+    return height;
+  }
+  // The coordinate system of the UI has (0,0) at the top-left corner of a page.  The
+  //  back end has (0,0) at the bottom-left, so we transpose here.
+  return height - y;
+};
+
+const roundToFixedDecimalPlaces = (num: number) =>
+  round(num, NUMBER_DECIMAL_PLACES);
