992b06f0d94153c31e097a132bee65e3573ddc09
Stef Stachow <stef@stachow.co.uk>
2023-12-28 09:41:11 +0000

    Fix DI

--- polaris-pipeline/Common/Factories/PipelineClientRequestFactory.cs
+++ polaris-pipeline/Common/Factories/PipelineClientRequestFactory.cs
@@ -1,7 +1,5 @@
 ﻿using Common.Constants;
 using Common.Factories.Contracts;
-using Common.Logging;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Http;
 
@@ -9,23 +7,15 @@ namespace Common.Factories
 {
     public class PipelineClientRequestFactory : IPipelineClientRequestFactory
     {
-        private readonly ILogger<PipelineClientRequestFactory> _logger;
-
-        public PipelineClientRequestFactory(ILogger<PipelineClientRequestFactory> logger)
-        {
-            _logger = logger;
-        }
-
         public HttpRequestMessage Create(HttpMethod httpMethod, string requestUri, Guid correlationId, string cmsAuthValues = null)
         {
-            _logger.LogMethodEntry(correlationId, nameof(Create), requestUri);
-
             var request = new HttpRequestMessage(httpMethod, requestUri);
             request.Headers.Add(HttpHeaderKeys.CorrelationId, correlationId.ToString());
             if (cmsAuthValues != null)
+            {
                 request.Headers.Add(HttpHeaderKeys.CmsAuthValues, cmsAuthValues);
+            }
 
-            _logger.LogMethodExit(correlationId, nameof(Create), string.Empty);
             return request;
         }
     }
