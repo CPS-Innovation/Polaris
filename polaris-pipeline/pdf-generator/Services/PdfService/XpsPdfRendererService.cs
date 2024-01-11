3a9205852848995124aa7e58c116a41e935e7a5d
Neil Foubister <151544338+NeilFoubister@users.noreply.github.com>
2024-01-11 16:55:03 +0000

    25068: 9 additional file types can now be converted to PDF

--- polaris-pipeline/pdf-generator/Services/PdfService/XpsPdfRendererService.cs
+++ polaris-pipeline/pdf-generator/Services/PdfService/XpsPdfRendererService.cs
@@ -1,22 +1,22 @@
-﻿using System;
+using System;
 using System.IO;
 using Aspose.Pdf;
 using pdf_generator.Factories.Contracts;
 
 namespace pdf_generator.Services.PdfService;
 
-public class PdfRendererService : IPdfService
+public class XpsPdfRendererService : IPdfService
 {
     private readonly IAsposeItemFactory _asposeItemFactory;
 
-    public PdfRendererService(IAsposeItemFactory asposeItemFactory)
+    public XpsPdfRendererService(IAsposeItemFactory asposeItemFactory)
     {
         _asposeItemFactory = asposeItemFactory ?? throw new ArgumentNullException(nameof(asposeItemFactory));
     }
 
     public void ReadToPdfStream(Stream inputStream, Stream pdfStream, Guid correlationId)
     {
-        var doc = _asposeItemFactory.CreateRenderedPdfDocument(inputStream, correlationId);
+        var doc = _asposeItemFactory.CreateRenderedXpsPdfDocument(inputStream, correlationId);
         doc.Save(pdfStream, SaveFormat.Pdf);
         pdfStream.Seek(0, SeekOrigin.Begin);
     }
