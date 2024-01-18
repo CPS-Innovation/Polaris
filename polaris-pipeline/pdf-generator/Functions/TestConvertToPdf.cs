a1d597e8970edff6f34b10c9c3077ea0f40f9a09
Mark Jones <aceltuk@users.noreply.github.com>
2024-01-18 16:18:39 +0000

    Fix bug where incoming input stream is handled different under aspnetcore (#804)
    * Fix bug where incoming input stream is handled different under aspnetcore than in-proc function operation. Added facility to copy the received stream to a memory stream before handing it over to Aspose. The Aspose libraries dispose the incoming stream. Also tidied up some references and fixed the EmailPdfService, which was not using the mail message it had just created when generating the MHTML document for conversion.
    
    * Added missing references (for code inside the #If Windows statements). And corrected the "Verify" signature for some of the Moq calls.

---
+++ polaris-pipeline/pdf-generator/Functions/TestConvertToPdf.cs
@@ -0,0 +1,74 @@
+﻿/*using System;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.AspNetCore.Http;
+using pdf_generator.Services.PdfService;
+using Common.Domain.Exceptions;
+using Common.Extensions;
+using Common.Logging;
+
+namespace pdf_generator.Functions
+{
+    public class TestConvertToPdf
+    {
+        private readonly IPdfOrchestratorService _pdfOrchestratorService;
+        private readonly ILogger<TestConvertToPdf> _logger;
+        private const string LoggingName = nameof(TestConvertToPdf);
+
+        public TestConvertToPdf(
+             IPdfOrchestratorService pdfOrchestratorService,
+             ILogger<TestConvertToPdf> logger)
+        {
+            _pdfOrchestratorService = pdfOrchestratorService;
+            _logger = logger;
+        }
+
+        [Function(nameof(TestConvertToPdf))]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "test-convert-to-pdf")] HttpRequest request, 
+            FunctionContext executionContext)
+        {
+            var correlationId = Guid.NewGuid();
+            try
+            {
+                #region Validate-Inputs
+                
+                var fileType = request.Headers.GetFileType();
+                
+                const string documentId = "test-document";
+                
+                #endregion
+                
+                request.EnableBuffering();
+                if (request.ContentLength == null || !request.Body.CanSeek)
+                    throw new BadRequestException("An empty document stream was received from the TestClient", nameof(request));
+                
+                var originalBytes = request.ContentLength;
+                _logger.LogMethodFlow(correlationId, LoggingName, $"Original bytes received: {originalBytes}");
+                
+                request.Body.Seek(0, SeekOrigin.Begin);
+                    
+                var pdfStream = _pdfOrchestratorService.ReadToPdfStream(request.Body, fileType, documentId, Guid.NewGuid());
+                var bytes = pdfStream.Length;
+                _logger.LogMethodFlow(correlationId, LoggingName, $"Converted bytes: {bytes}");
+                    
+                pdfStream.Position = 0;
+                return new FileStreamResult(pdfStream, "application/pdf")
+                {
+                    FileDownloadName = $"{nameof(TestConvertToPdf)}.pdf",
+                };
+            }
+            catch (Exception exception)
+            {
+                _logger.LogMethodError(correlationId, LoggingName, exception.Message, exception);
+ 
+                return new ObjectResult(exception.ToString())
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
+            }
+        }
+    }
+}*/
