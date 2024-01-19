cd00c51ac5aacbda575f0580c1a8e5b86030e70b
Rhys Bridges <bridgesrhys@gmail.com>
2024-01-19 11:17:22 +0000

    #25242 - change clashing urns in tests

--- polaris-e2e/cypress/e2e/api/subsequent-refresh-locks.cy.ts
+++ polaris-e2e/cypress/e2e/api/subsequent-refresh-locks.cy.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 import { ApiRoutes, makeApiRoutes } from "./helpers/make-routes"
 
-const { REFRESH_TARGET_URN, REFRESH_TARGET_CASE_ID } = Cypress.env()
+const { REFRESH_LOCK_URN, REFRESH_LOCK_CASE_ID } = Cypress.env();
 
 let routes: ApiRoutes
 
@@ -13,25 +13,21 @@ describe("Subsequent Refresh Locks", { tags: '@ci' }, () => {
   })
 
   it("the pipeline runs as a singleton", () => {
-    cy.clearCaseTracker(REFRESH_TARGET_URN, REFRESH_TARGET_CASE_ID)
+    cy.clearCaseTracker(REFRESH_LOCK_URN, REFRESH_LOCK_CASE_ID)
       .api(
-        routes.TRACKER_START(
-          REFRESH_TARGET_URN,
-          REFRESH_TARGET_CASE_ID,
-          "PHASE_1"
-        )
+        routes.TRACKER_START(REFRESH_LOCK_URN, REFRESH_LOCK_CASE_ID, "PHASE_1")
       )
       .its("status")
       .then((status) => status === 202)
       .api({
         ...routes.TRACKER_START(
-          REFRESH_TARGET_URN,
-          REFRESH_TARGET_CASE_ID,
+          REFRESH_LOCK_URN,
+          REFRESH_LOCK_CASE_ID,
           "PHASE_1"
         ),
         failOnStatusCode: false,
       })
       .its("status")
-      .then((status) => status === 403)
+      .then((status) => status === 403);
   })
 })
