f4eac7e8474eb9e50ebfcf1e8376cdba20102b79
Stef Stachow <stef@stachow.co.uk>
2024-01-22 15:55:53 +0000

    #25223 - ui config subst. on server can be run more than once now

--- polaris-ui/public/subsititute-config.js
+++ polaris-ui/public/subsititute-config.js
@@ -1,7 +1,11 @@
 // this file is required for setting config values in deployed code see #12977
 const fs = require("fs");
 
+const cleanFolder = "polaris-ui/static/js-pre-substitution";
 const folder = "polaris-ui/static/js";
+
+fs.rmSync(folder, { recursive: true, force: true });
+fs.cpSync(cleanFolder, folder, { recursive: true });
 
 const REACT_APP_CONFIGS = Object.entries(process.env).filter(([key]) =>
   key.startsWith("REACT_APP_")
