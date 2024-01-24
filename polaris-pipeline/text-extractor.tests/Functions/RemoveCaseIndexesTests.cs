2b1c91c835b054484767054e2dea305cb1d10fb6
Neil Foubister <151544338+NeilFoubister@users.noreply.github.com>
2024-01-24 08:56:09 +0000

    Feature/25102 create new text extractor–endpoint for removing case indexes (#823)
    * #25102: New RemoveCaseIndexes endpoint added along with unit tests.
    
    * #25102: RemoveCaseIndexesAsync added to TextExtractorClient and OrchestrationProvider updated to use the new method.

---
+++ polaris-pipeline/text-extractor.tests/Functions/RemoveCaseIndexesTests.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using Common.Domain.Exceptions;
+using Common.Dto.Request;
+using Common.Handlers.Contracts;
+using Common.Services.CaseSearchService;
+using Common.Services.CaseSearchService.Contracts;
+using Common.Telemetry.Wrappers.Contracts;
+using Common.Wrappers.Contracts;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using text_extractor.Functions;
+using Xunit;
+
+namespace text_extractor.tests.Functions
+{
+    public class RemoveCaseIndexesTests
+    {
+        private readonly Fixture _fixture;
+        private readonly string _serializedExtractTextRequest;
+        private readonly HttpRequestMessage _httpRequestMessage;
+        private readonly RemoveCaseIndexesRequestDto _removeCaseIndexesRequest;
+        private HttpResponseMessage _errorHttpResponseMessage;
+        private readonly Mock<ISearchIndexService> _mockSearchIndexService;
+        private readonly Mock<ILogger<RemoveCaseIndexes>> _mockLogger;
+        private readonly Mock<IJsonConvertWrapper> _mockJsonConvertWrapper;
+        private readonly Mock<ITelemetryAugmentationWrapper> _mockTelemetryAugmentationWrapper;
+        private readonly Mock<IExceptionHandler> _mockExceptionHandler;
+        private readonly Guid _correlationId;
+        private readonly RemoveCaseIndexes _removeCaseIndexes;
+
+        public RemoveCaseIndexesTests()
+        {
+            _fixture = new Fixture();
+            _serializedExtractTextRequest = _fixture.Create<string>();
+            _httpRequestMessage = new HttpRequestMessage()
+            {
+                Content = new StringContent(_serializedExtractTextRequest)
+            };
+            _removeCaseIndexesRequest = _fixture.Create<RemoveCaseIndexesRequestDto>();
+            _mockSearchIndexService = new Mock<ISearchIndexService>();
+            _mockJsonConvertWrapper = new Mock<IJsonConvertWrapper>();
+            _mockTelemetryAugmentationWrapper = new Mock<ITelemetryAugmentationWrapper>();
+            _mockExceptionHandler = new Mock<IExceptionHandler>();
+            _correlationId = _fixture.Create<Guid>();
+            _mockLogger = new Mock<ILogger<RemoveCaseIndexes>>();
+
+            _mockSearchIndexService
+                .Setup(service => service.RemoveCaseIndexEntriesAsync(It.IsAny<long>()))
+                .ReturnsAsync(new IndexDocumentsDeletedResult
+                {
+                    DocumentCount = 1,
+                    SuccessCount = 1,
+                    FailureCount = 0
+                });
+
+            _removeCaseIndexes = new RemoveCaseIndexes(
+                                _mockLogger.Object,
+                                _mockSearchIndexService.Object,
+                                _mockTelemetryAugmentationWrapper.Object,
+                                _mockExceptionHandler.Object,
+                                _mockJsonConvertWrapper.Object);
+        }
+
+        [Fact]
+        public void Run_ShouldReturnAnExceptionWhenInitializingAndLoggerIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new RemoveCaseIndexes(null, null, null, null, null));
+        }
+
+        [Fact]
+        public void Run_ShouldReturnAnExceptionWhenInitializingAndSearchIndexServiceIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new RemoveCaseIndexes(_mockLogger.Object, null, null, null, null));
+        }
+
+        [Fact]
+        public void Run_ShouldReturnAnExceptionWhenInitializingAndTelemetryAugmentationWrapperIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new RemoveCaseIndexes(_mockLogger.Object, _mockSearchIndexService.Object, null, null, null));
+        }
+
+        [Fact]
+        public void Run_ShouldReturnAnExceptionWhenInitializingAndExceptionHandlerrIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new RemoveCaseIndexes(_mockLogger.Object, _mockSearchIndexService.Object, _mockTelemetryAugmentationWrapper.Object, null, null));
+        }
+
+        [Fact]
+        public void Run_ShouldReturnAnExceptionWhenInitializingAndJsonConvertWrapperIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new RemoveCaseIndexes(_mockLogger.Object, _mockSearchIndexService.Object, _mockTelemetryAugmentationWrapper.Object, _mockExceptionHandler.Object, null));
+        }
+
+        [Fact]
+        public async Task Run_ReturnsExceptionWhenCorrelationIdIsMissing()
+        {
+            _errorHttpResponseMessage = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            _mockExceptionHandler.Setup(handler => handler.HandleException(It.IsAny<Exception>(), It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<ILogger<RemoveCaseIndexes>>()))
+                .Returns(_errorHttpResponseMessage);
+            _httpRequestMessage.Content = new StringContent(" ");
+
+            var response = await _removeCaseIndexes.Run(_httpRequestMessage);
+
+            response.Should().Be(_errorHttpResponseMessage);
+        }
+
+        [Fact]
+        public async Task Run_ReturnsBadRequestWhenUsingAnInvalidCorrelationId()
+        {
+            _errorHttpResponseMessage = new HttpResponseMessage(HttpStatusCode.BadRequest);
+            _mockExceptionHandler.Setup(handler => handler.HandleException(It.IsAny<BadRequestException>(), It.IsAny<Guid>(), It.IsAny<string>(), _mockLogger.Object))
+                .Returns(_errorHttpResponseMessage);
+            _httpRequestMessage.Headers.Add("Correlation-Id", string.Empty);
+
+            var response = await _removeCaseIndexes.Run(_httpRequestMessage);
+
+            response.Should().Be(_errorHttpResponseMessage);
+        }
+
+        [Fact]
+        public async Task Run_ReturnsBadRequestWhenUsingAnEmptyCorrelationId()
+        {
+            _errorHttpResponseMessage = new HttpResponseMessage(HttpStatusCode.BadRequest);
+            _mockExceptionHandler.Setup(handler => handler.HandleException(It.IsAny<BadRequestException>(), It.IsAny<Guid>(), It.IsAny<string>(), _mockLogger.Object))
+                .Returns(_errorHttpResponseMessage);
+            _httpRequestMessage.Headers.Add("Correlation-Id", Guid.Empty.ToString());
+
+            var response = await _removeCaseIndexes.Run(_httpRequestMessage);
+
+            response.Should().Be(_errorHttpResponseMessage);
+        }
+
+        [Fact]
+        public async Task Run_ReturnsOk()
+        {
+            _httpRequestMessage.Headers.Add("Correlation-Id", _correlationId.ToString());
+            _httpRequestMessage.Content = new StringContent("", Encoding.UTF8, "application/json");
+            _mockJsonConvertWrapper.Setup(wrapper => wrapper.DeserializeObject<RemoveCaseIndexesRequestDto>(It.IsAny<string>()))
+                .Returns(new RemoveCaseIndexesRequestDto());
+            _mockJsonConvertWrapper.Setup(wrapper => wrapper.SerializeObject(It.IsAny<IndexDocumentsDeletedResult>()))
+                .Returns(string.Empty);
+
+            var response = await _removeCaseIndexes.Run(_httpRequestMessage);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Run_ReturnsResponseWhenExceptionOccurs()
+        {
+            _errorHttpResponseMessage = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            var exception = new Exception();
+            _mockExceptionHandler.Setup(handler => handler.HandleException(It.IsAny<Exception>(), It.IsAny<Guid>(), It.IsAny<string>(), _mockLogger.Object))
+                .Returns(_errorHttpResponseMessage);
+
+            var response = await _removeCaseIndexes.Run(_httpRequestMessage);
+
+            response.Should().Be(_errorHttpResponseMessage);
+        }
+    }
+}
---
+++ polaris-pipeline/text-extractor/Functions/RemoveCaseIndexes.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Common.Configuration;
+using Common.Domain.Exceptions;
+using Common.Dto.Request;
+using Common.Extensions;
+using Common.Handlers.Contracts;
+using Common.Logging;
+using Common.Services.CaseSearchService.Contracts;
+using Common.Telemetry.Wrappers.Contracts;
+using Common.Wrappers.Contracts;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace text_extractor.Functions
+{
+    public class RemoveCaseIndexes
+    {
+        private readonly ILogger<RemoveCaseIndexes> _log;
+        private readonly ISearchIndexService _searchIndexService;
+        private readonly ITelemetryAugmentationWrapper _telemetryAugmentationWrapper;
+        private readonly IExceptionHandler _exceptionHandler;
+        private readonly IJsonConvertWrapper _jsonConvertWrapper;
+        private const string loggingName = nameof(RemoveCaseIndexes);
+
+        public RemoveCaseIndexes(ILogger<RemoveCaseIndexes> logger, ISearchIndexService searchIndexService, ITelemetryAugmentationWrapper telemetryAugmentationWrapper, IExceptionHandler exceptionHandler, IJsonConvertWrapper jsonConvertWrapper)
+        {
+            _log = logger ?? throw new ArgumentNullException(nameof(logger));
+            _searchIndexService = searchIndexService ?? throw new ArgumentNullException(nameof(searchIndexService));
+            _telemetryAugmentationWrapper = telemetryAugmentationWrapper ?? throw new ArgumentNullException(nameof(telemetryAugmentationWrapper));
+            _exceptionHandler = exceptionHandler ?? throw new ArgumentNullException(nameof(exceptionHandler));
+            _jsonConvertWrapper = jsonConvertWrapper ?? throw new ArgumentNullException(nameof(jsonConvertWrapper));
+        }
+
+        [FunctionName(nameof(RemoveCaseIndexes))]
+        public async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = RestApi.RemoveCaseIndexes)] HttpRequestMessage request)
+        {
+            Guid correlationId = default;
+
+            try
+            {
+                correlationId = request.Headers.GetCorrelationId();
+                _telemetryAugmentationWrapper.RegisterCorrelationId(correlationId);
+
+                if (request.Content == null)
+                {
+                    throw new BadRequestException("Request body has no content", nameof(request));
+                }
+
+                var content = await request.Content.ReadAsStringAsync();
+                var requestDto = _jsonConvertWrapper.DeserializeObject<RemoveCaseIndexesRequestDto>(content);
+
+                _log.LogMethodFlow(correlationId, loggingName, $"Begin removing case indexes for case ID {requestDto.CaseId}");
+
+                var result = await _searchIndexService.RemoveCaseIndexEntriesAsync(requestDto.CaseId);
+
+                _log.LogMethodFlow(correlationId, loggingName, $"Case indexes removed for case ID {requestDto.CaseId}");
+
+                return new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(_jsonConvertWrapper.SerializeObject(result))
+                };
+            }
+            catch (Exception exception)
+            {
+                return _exceptionHandler.HandleException(exception, correlationId, loggingName, _log);
+            }
+            finally
+            {
+                _log.LogMethodExit(correlationId, loggingName, string.Empty);
+            }
+        }
+    }
+}
