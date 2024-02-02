ff5fd90c649e9a0e519301bac0e32f3da19b5ce0
aceltuk <mark.jones3@cps.gov.uk>
2024-02-02 16:05:19 +0000

    Removed test pdf converter function - failing sonar cloud duplication analysis

--- polaris-terraform/pipeline-terraform/functions-pdf-generator.tf
+++ polaris-terraform/pipeline-terraform/functions-pdf-generator.tf
@@ -16,7 +16,6 @@ resource "azurerm_windows_function_app" "fa_pdf_generator" {
   
 
   app_settings = {
-    "AzureWebJobs.TestConvertToPdf.Disabled"          = "1"
     "AzureWebJobsStorage"                             = azurerm_storage_account.sa_pdf_generator.primary_connection_string
     "BlobServiceContainerName"                        = "documents"
     "BlobServiceUrl"                                  = "https://sacps${var.env != "prod" ? var.env : ""}polarispipeline.blob.core.windows.net/"
