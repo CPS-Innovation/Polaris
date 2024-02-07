8d6a0054c1aa0bf9f801dec6f9e12dc7d0299bc6
Rhys Bridges <rhys@hampton.io>
2024-02-07 09:03:53 +0000

    #25621 - attachment failure e2e test wip

---
+++ polaris-e2e/cypress/e2e/api/attachments-failure.cy.ts
@@ -0,0 +1,69 @@
+/// <reference types="cypress" />
+import "cypress-wait-until"
+import { PipelineResults } from "../../../gateway/PipelineResults"
+import { ApiRoutes, makeApiRoutes } from "./helpers/make-routes"
+import { WAIT_UNTIL_OPTIONS } from "../../support/options"
+
+const { 
+  ATTACHMENT_FAILURE_CASE_URN, 
+  ATTACHMENT_FAILURE_CASE_ID,
+  ATTACHMENT_FAILURE_PASSWORD_PROTECTED_DOCUMENT_NAME,
+  ATTACHMENT_FAILURE_OK_DOCUMENT_NAME
+} =
+  Cypress.env()
+
+let routes: ApiRoutes
+
+describe("Attachments failure", { tags: '@ci' }, () => {
+  beforeEach(() => {
+    cy.getAuthHeaders().then((headers) => {
+      routes = makeApiRoutes(headers)
+    })
+  })
+
+  it("can observe failed documents that are UnableToConvertToPdf in the tracker responses ", () => {
+    cy.clearCaseTracker(ATTACHMENT_FAILURE_CASE_URN, ATTACHMENT_FAILURE_CASE_ID)
+      .api(routes.TRACKER_START(ATTACHMENT_FAILURE_CASE_URN, ATTACHMENT_FAILURE_CASE_ID))
+      .waitUntil(
+        () =>
+          cy
+            .api({
+              ...routes.GET_TRACKER(ATTACHMENT_FAILURE_CASE_URN, ATTACHMENT_FAILURE_CASE_ID),
+              failOnStatusCode: false,
+            })
+            .its("status")
+            .then((status) => status !== 404),
+        WAIT_UNTIL_OPTIONS
+      )
+      .waitUntil(
+        () =>
+          cy
+            .api<PipelineResults>(
+              routes.GET_TRACKER(ATTACHMENT_FAILURE_CASE_URN, ATTACHMENT_FAILURE_CASE_ID)
+            )
+            .its("body")
+            .then(({ documents }) => !!documents.length),
+        WAIT_UNTIL_OPTIONS
+      )
+      .api<PipelineResults>(
+        routes.GET_TRACKER(ATTACHMENT_FAILURE_CASE_URN, ATTACHMENT_FAILURE_CASE_ID)
+      )
+      .its("body")
+      .then(({ documents }) => {
+        expect(
+          documents.some(
+            (document) =>
+              document.cmsOriginalFileName === ATTACHMENT_FAILURE_PASSWORD_PROTECTED_DOCUMENT_NAME &&
+              document.status === "UnableToConvertToPdf"
+          )
+        ).to.be.true
+        expect(
+          documents.some(
+            (document) =>
+              document.cmsOriginalFileName === ATTACHMENT_FAILURE_OK_DOCUMENT_NAME &&
+              document.status === "Indexed"
+          )
+        ).to.be.true
+      })
+  })
+})
