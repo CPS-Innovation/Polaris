0f0c3467d59ad9e1690e57a6227791918919c4de
Rhys Bridges <bridgesrhys@gmail.com>
2024-02-07 15:06:01 +0000

    Feature/25621 attachment failure test (#880)
    * #25621 - attachment failure e2e test wip
    
    * #25621 - attachment failure e2e test wip
    
    * #25620 - regen mock for new attachment failure case

---
+++ polaris-e2e/cypress/e2e/api/conversion-failure.cy.ts
@@ -0,0 +1,77 @@
+/// <reference types="cypress" />
+import "cypress-wait-until"
+import { PipelineResults } from "../../../gateway/PipelineResults"
+import { ApiRoutes, makeApiRoutes } from "./helpers/make-routes"
+import { WAIT_UNTIL_OPTIONS } from "../../support/options"
+
+const { 
+  CONVERSION_FAILURE_CASE_URN, 
+  CONVERSION_FAILURE_CASE_ID,
+  CONVERSION_FAILURE_PASSWORD_PROTECTED_DOCUMENT_NAME,
+  CONVERSION_FAILURE_INVALID_EXTENSION_NAME,
+  CONVERSION_FAILURE_OK_DOCUMENT_NAME
+} =
+  Cypress.env()
+
+let routes: ApiRoutes
+
+describe("Conversion failure", { tags: '@ci' }, () => {
+  beforeEach(() => {
+    cy.getAuthHeaders().then((headers) => {
+      routes = makeApiRoutes(headers)
+    })
+  })
+
+  it("can observe failed documents that are UnableToConvertToPdf in the tracker responses ", () => {
+    cy.clearCaseTracker(CONVERSION_FAILURE_CASE_URN, CONVERSION_FAILURE_CASE_ID)
+      .api(routes.TRACKER_START(CONVERSION_FAILURE_CASE_URN, CONVERSION_FAILURE_CASE_ID))
+      .waitUntil(
+        () =>
+          cy
+            .api({
+              ...routes.GET_TRACKER(CONVERSION_FAILURE_CASE_URN, CONVERSION_FAILURE_CASE_ID),
+              failOnStatusCode: false,
+            })
+            .its("status")
+            .then((status) => status !== 404),
+        WAIT_UNTIL_OPTIONS
+      )
+      .waitUntil(
+        () =>
+          cy
+            .api<PipelineResults>(
+              routes.GET_TRACKER(CONVERSION_FAILURE_CASE_URN, CONVERSION_FAILURE_CASE_ID)
+            )
+            .its("body")
+            .then(({ documents }) => !!documents.length),
+        WAIT_UNTIL_OPTIONS
+      )
+      .api<PipelineResults>(
+        routes.GET_TRACKER(CONVERSION_FAILURE_CASE_URN, CONVERSION_FAILURE_CASE_ID)
+      )
+      .its("body")
+      .then(({ documents }) => {
+        expect(
+          documents.some(
+            (document) =>
+              document.cmsOriginalFileName === CONVERSION_FAILURE_PASSWORD_PROTECTED_DOCUMENT_NAME &&
+              document.status === "UnableToConvertToPdf"
+          )
+        ).to.be.true
+        expect(
+          documents.some(
+            (document) =>
+              document.cmsOriginalFileName === CONVERSION_FAILURE_INVALID_EXTENSION_NAME &&
+              document.status === "UnableToConvertToPdf"
+          )
+        ).to.be.true
+        expect(
+          documents.some(
+            (document) =>
+              document.cmsOriginalFileName === CONVERSION_FAILURE_OK_DOCUMENT_NAME &&
+              document.status === "Indexed"
+          )
+        ).to.be.true
+      })
+  })
+})
