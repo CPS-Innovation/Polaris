5840e8c066330baec1e1efd2acc24d98f0b1c230
Rhys Bridges <rhys@hampton.io>
2024-02-21 13:28:08 +0000

    #25836 - catch aspose index out of range exception and return input stream

--- polaris-pipeline/pdf-generator/Domain/Document/PdfConversionResult.cs
+++ polaris-pipeline/pdf-generator/Domain/Document/PdfConversionResult.cs
@@ -10,13 +10,13 @@ public class PdfConversionResult
         DocumentId = documentId;
         ConversionHandler = conversionHandler;
     }
-    
+
     public string DocumentId { get; set; }
-    
+
     public Stream ConvertedDocument { get; set; }
 
     public PdfConverterType ConversionHandler { get; set; }
-    
+
     public PdfConversionStatus ConversionStatus { get; set; }
 
     public string Feedback { get; set; }
@@ -27,10 +27,22 @@ public class PdfConversionResult
         ConversionStatus = PdfConversionStatus.DocumentConverted;
     }
 
+    public void RecordConversionQualifiedSuccess(Stream convertedDocument)
+    {
+        ConvertedDocument = convertedDocument;
+        ConversionStatus = PdfConversionStatus.DocumentConverted;
+        Feedback = $"Aspose not handling otherwise heatlhy document with id '{DocumentId}'";
+    }
+
     public void RecordConversionFailure(PdfConversionStatus status, string message)
     {
         ConversionStatus = status;
         Feedback = $"Unable to convert document with id '{DocumentId}' to pdf. Exception: {message}.";
+    }
+
+    public bool HasFailureReason()
+    {
+        return !string.IsNullOrEmpty(Feedback);
     }
 
     public string GetFailureReason()
