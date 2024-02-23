f6f39d77a762cd51051e9131cf8036cc05cbd97f
renjithabby <renjithabby@gmail.com>
2024-02-23 12:35:34 +0000

    updated the styles for read documents

--- polaris-ui/src/app/features/cases/presentation/case-details/accordion/AccordionDocument.tsx
+++ polaris-ui/src/app/features/cases/presentation/case-details/accordion/AccordionDocument.tsx
@@ -45,84 +45,89 @@ export const AccordionDocument: React.FC<Props> = ({
         caseDocument.docRead ? classes.docRead : ""
       }`}
     >
-      <div className={`${classes["accordion-document-item-wrapper"]}`}>
-        {canViewDocument ? (
-          <LinkButton
-            onClick={() => {
-              trackEvent("Open Document From Case Details", {
-                documentId: caseDocument.documentId,
-              });
-              handleOpenPdf({ documentId: caseDocument.documentId });
-            }}
-            className={`${classes["accordion-document-link-button"]}`}
-            dataTestId={`link-document-${caseDocument.documentId}`}
-            ariaLabel={`Open Document ${caseDocument.presentationFileName}`}
-          >
-            {caseDocument.presentationFileName}
-          </LinkButton>
-        ) : (
-          <span
-            className={`${classes["accordion-document-link-name"]}`}
-            data-testid={`name-text-document-${caseDocument.documentId}`}
-          >
-            {caseDocument.presentationFileName}
-          </span>
-        )}
-        <div className={`${classes["accordion-document-date"]}`}>
-          <span className={`${classes["visuallyHidden"]}`}> Date Added</span>
-          <DateIcon className={classes.dateIcon} />
-          {caseDocument.cmsFileCreatedDate &&
-            formatDate(
-              caseDocument.cmsFileCreatedDate,
-              CommonDateTimeFormats.ShortDateTextMonth
+      <div className={classes.listItemWrapper}>
+        <div className={`${classes["accordion-document-item-wrapper"]}`}>
+          {canViewDocument ? (
+            <LinkButton
+              onClick={() => {
+                trackEvent("Open Document From Case Details", {
+                  documentId: caseDocument.documentId,
+                });
+                handleOpenPdf({ documentId: caseDocument.documentId });
+              }}
+              className={`${classes["accordion-document-link-button"]}`}
+              dataTestId={`link-document-${caseDocument.documentId}`}
+              ariaLabel={`Open Document ${caseDocument.presentationFileName}`}
+            >
+              {caseDocument.presentationFileName}
+            </LinkButton>
+          ) : (
+            <span
+              className={`${classes["accordion-document-link-name"]}`}
+              data-testid={`name-text-document-${caseDocument.documentId}`}
+            >
+              {caseDocument.presentationFileName}
+            </span>
+          )}
+          <div className={`${classes["accordion-document-date"]}`}>
+            <span className={`${classes["visuallyHidden"]}`}> Date Added</span>
+            <DateIcon className={classes.dateIcon} />
+            {caseDocument.cmsFileCreatedDate &&
+              formatDate(
+                caseDocument.cmsFileCreatedDate,
+                CommonDateTimeFormats.ShortDateTextMonth
+              )}
+            {formattedFileCreatedTime && (
+              <>
+                <span className={`${classes["visuallyHidden"]}`}>
+                  Time added
+                </span>
+                <TimeIcon className={classes.timeIcon} />
+                {caseDocument.cmsFileCreatedDate && formattedFileCreatedTime}
+              </>
             )}
-          {formattedFileCreatedTime && (
-            <>
-              <span className={`${classes["visuallyHidden"]}`}>Time added</span>
-              <TimeIcon className={classes.timeIcon} />
-              {caseDocument.cmsFileCreatedDate && formattedFileCreatedTime}
-            </>
+          </div>
+
+          {!!caseDocument.attachments.length && (
+            <div className={classes.attachmentWrapper}>
+              <AttachmentIcon className={classes.attachmentIcon} />
+              <span data-testid={`attachment-text-${caseDocument.documentId}`}>
+                {getAttachmentText()}
+              </span>
+            </div>
           )}
         </div>
+        <div className={classes.witnessIndicators}>
+          {caseDocument.witnessIndicators.length > 0 &&
+            caseDocument.witnessIndicators
+              .sort(
+                (a, b) =>
+                  witnessIndicatorPrecedenceOrder.indexOf(a) -
+                  witnessIndicatorPrecedenceOrder.indexOf(b)
+              )
+              .map((indicator) => (
+                <strong
+                  className={`govuk-tag govuk-tag--grey ${classes.tooltip}`}
+                  key={indicator}
+                  data-testid={`indicator-${caseDocument.documentId}-${indicator}`}
+                >
+                  {indicator}{" "}
+                  <span className={classes.tooltiptext}>
+                    {witnessIndicatorNames[indicator]}
+                  </span>
+                </strong>
+              ))}
+        </div>
 
-        {!!caseDocument.attachments.length && (
-          <div className={classes.attachmentWrapper}>
-            <AttachmentIcon className={classes.attachmentIcon} />
-            <span data-testid={`attachment-text-${caseDocument.documentId}`}>
-              {getAttachmentText()}
-            </span>
-          </div>
+        {!canViewDocument && (
+          <span
+            className={`${classes["accordion-document-read-warning"]}`}
+            data-testid={`view-warning-document-${caseDocument.documentId}`}
+          >
+            Document only available on CMS
+          </span>
         )}
       </div>
-      <div className={classes.witnessIndicators}>
-        {caseDocument.witnessIndicators.length > 0 &&
-          caseDocument.witnessIndicators
-            .sort(
-              (a, b) =>
-                witnessIndicatorPrecedenceOrder.indexOf(a) -
-                witnessIndicatorPrecedenceOrder.indexOf(b)
-            )
-            .map((indicator) => (
-              <strong
-                className={`govuk-tag govuk-tag--grey ${classes.tooltip}`}
-                key={indicator}
-                data-testid={`indicator-${caseDocument.documentId}-${indicator}`}
-              >
-                {indicator}{" "}
-                <span className={classes.tooltiptext}>
-                  {witnessIndicatorNames[indicator]}
-                </span>
-              </strong>
-            ))}
-      </div>
-      {!canViewDocument && (
-        <span
-          className={`${classes["accordion-document-read-warning"]}`}
-          data-testid={`view-warning-document-${caseDocument.documentId}`}
-        >
-          Document only available on CMS
-        </span>
-      )}
     </li>
   );
 };
