f198235a798f7377cec7b486296aabacdea6695f
Rhys Bridges <rhys@hampton.io>
2024-02-28 16:16:28 +0000

    #25875 - added redaction integration test with image comparison

---
+++ polaris-pipeline/pdf-generator-integration.tests/Helpers/PdfConversionHelper.cs
@@ -0,0 +1,28 @@
+using Aspose.Pdf;
+using Aspose.Pdf.Devices;
+
+public class PdfConversionHelper
+{
+  public static async Task<List<MemoryStream>> ConvertAndSavePdfToImages(Document pdfFile)
+  {
+    var tasks = new List<Task<MemoryStream>>();
+
+    pdfFile.Pages.ToList().ForEach(page =>
+    {
+      Resolution resolution = new Resolution(300);
+      PngDevice pngDevice = new PngDevice(resolution);
+
+      var ms = new MemoryStream();
+      tasks.Add(Task.Run(() =>
+      {
+        pngDevice.Process(page, ms);
+        ms.Position = 0; // Reset the stream position to the beginning
+        return ms;
+      }));
+    });
+
+    var streamResults = await Task.WhenAll(tasks);
+    return streamResults.ToList();
+  }
+
+}
