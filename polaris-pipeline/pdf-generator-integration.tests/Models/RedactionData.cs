aff08f2844616abda214f3d689cb557026580342
Rhys Bridges <rhys@hampton.io>
2024-03-04 12:35:59 +0000

    #26064 - broken ocr layer integration test with mocked document

--- polaris-pipeline/pdf-generator-integration.tests/Models/RedactionData.cs
+++
@@ -1,7 +0,0 @@
-using Common.Dto.Request.Redaction;
-
-public class RedactionData
-{
-  public required string DocumentId { get; set; }
-  public required List<RedactionDefinitionDto> Redactions { get; set; }
-}
--- polaris-pipeline/pdf-generator.integration.tests/RedactionAssuranceTests.cs
+++ polaris-pipeline/pdf-generator.integration.tests/RedactionAssuranceTests.cs
@@ -13,129 +13,113 @@ using Common.Dto.Request.Redaction;
 using Common.Dto.Request;
 using Aspose.Pdf;
 
-namespace pdf_generator_integration.tests;
-
-public class RedactionAssuranceTests
+namespace pdf_generator_integration.tests
 {
-  private Fixture _fixture;
-  private readonly Guid _correlationId;
-  private readonly string _caseId;
-  private readonly string _documentId;
-  private readonly AsposeRedactionProvider _asposeRedactionProvider;
-
-  public RedactionAssuranceTests()
+  public class RedactionAssuranceTests
   {
-    _fixture = new Fixture();
-    _correlationId = _fixture.Create<Guid>();
-    _caseId = _fixture.Create<string>();
-    _documentId = _fixture.Create<string>();
+    private Fixture _fixture;
+    private readonly Guid _correlationId;
+    private readonly string _caseId;
+    private readonly string _documentId;
+    private readonly AsposeRedactionProvider _asposeRedactionProvider;
 
-    const string licenceFileName = "Aspose.Total.NET.lic";
-    new Aspose.Cells.License().SetLicense(licenceFileName);
-    new Aspose.Diagram.License().SetLicense(licenceFileName);
-    new Aspose.Email.License().SetLicense(licenceFileName);
-    new Aspose.Imaging.License().SetLicense(licenceFileName);
-    new Aspose.Pdf.License().SetLicense(licenceFileName);
-    new Aspose.Slides.License().SetLicense(licenceFileName);
-    new Aspose.Words.License().SetLicense(licenceFileName);
-
-    var redactionImplementation = new ImageConversionImplementation(
-      Options.Create(new ImageConversionOptions
-      {
-        Resolution = 150,
-        QualityPercent = 50
-      }));
-
-    var coordinateCalculator = new CoordinateCalculator(new Mock<ILogger<CoordinateCalculator>>().Object);
-
-    _asposeRedactionProvider = new AsposeRedactionProvider(
-      redactionImplementation,
-      coordinateCalculator,
-      new Mock<ITelemetryClient>().Object
-    );
-  }
-
-  [Fact]
-  public async Task RedactionAssurance_ImagesNotLost()
-  {
-    // Arrange
-    using var inputStream = GetType().Assembly.GetManifestResourceStream("pdf_generator.integration.tests.TestResources.document.pdf") ?? throw new Exception("pdf_generator_integration.tests.TestResources.document.pdf not found");
-    using var originalDocument = new Document(inputStream);
-
-    var redactionData = LoadRedactionDataFromJson(originalDocument.FileName, "pdf_generator.integration.tests.TestResources.document_redactions.json");
-
-    // Act
-    using var outputStream = await _asposeRedactionProvider.Redact(inputStream, _caseId, _documentId, redactionData, _correlationId);
-    var redactedDocument = new Document(outputStream);
-
-    var redactedImageStreams = await PdfConversionHelper.ConvertAndSavePdfToImages(redactedDocument);
-
-    // Assert
-    using var assertionImageStreamOne = GetType().Assembly.GetManifestResourceStream("pdf_generator.integration.tests.TestResources.redacted_page_1.png") ?? throw new Exception("pdf_generator_integration.tests.TestResources.document_page_one.png not found");
-    using var assertionImageStreamTwo = GetType().Assembly.GetManifestResourceStream("pdf_generator.integration.tests.TestResources.redacted_page_2.png") ?? throw new Exception("pdf_generator_integration.tests.TestResources.document_page_two.png not found");
-
-    var pageOneDiff = ImageSharpCompare.CalcDiff(redactedImageStreams[0], assertionImageStreamOne, ResizeOption.Resize);
-    pageOneDiff.AbsoluteError.Should().BeLessThan(1);
-
-    var pageTwoDiff = ImageSharpCompare.CalcDiff(redactedImageStreams[1], assertionImageStreamTwo, ResizeOption.Resize);
-    pageTwoDiff.AbsoluteError.Should().BeLessThan(1);
-  }
-
-  [Fact]
-  public async Task RedactionAssurance_Overlapping_Redaction()
-  {
-    // Arrange
-    using var inputStream = GetType().Assembly.GetManifestResourceStream("pdf_generator.integration.tests.TestResources.overlapping_redaction.pdf") ?? throw new Exception("pdf_generator_integration.tests.TestResources.overlapping_redaction.pdf not found");
-    using var originalDocument = new Document(inputStream);
-
-    var redactionData = LoadRedactionDataFromJson(originalDocument.FileName, "pdf_generator.integration.tests.TestResources.overlapping_redaction_redactions.json");
-
-    // Act
-    using var outputStream = await _asposeRedactionProvider.Redact(inputStream, _caseId, _documentId, redactionData, _correlationId);
-    var redactedDocument = new Document(outputStream);
-
-    var redactedImageStreams = await PdfConversionHelper.ConvertAndSavePdfToImages(redactedDocument);
-
-    // Assert
-    using var assertionImageStream = GetType().Assembly.GetManifestResourceStream("pdf_generator.integration.tests.TestResources.overlapping_redaction_page_1.png") ?? throw new Exception("pdf_generator_integration.tests.TestResources.overlapping_redaction_page_1.png not found");
-
-    var pageOneDiff = ImageSharpCompare.CalcDiff(redactedImageStreams[0], assertionImageStream, ResizeOption.Resize);
-    pageOneDiff.AbsoluteError.Should().BeLessThan(1);
-  }
-
-  private RedactPdfRequestDto LoadRedactionDataFromJson(string fileName, string jsonPath)
-  {
-    using var redactedStream = GetType().Assembly.GetManifestResourceStream(jsonPath) ?? throw new Exception("pdf_generator_integration.tests.TestResources.document_redactions.json not found");
-    using var streamReader = new StreamReader(redactedStream);
-    var jsonText = streamReader.ReadToEnd();
-    var redactionData = JsonSerializer.Deserialize<RedactionData>(jsonText, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? throw new Exception("Failed to deserialize redaction data");
-
-    var redactionDefinitions = new List<RedactionDefinitionDto>();
-
-    foreach (var redaction in redactionData.Redactions)
+    public RedactionAssuranceTests()
     {
-      var redactionDefinition = new RedactionDefinitionDto
-      {
-        PageIndex = redaction.PageIndex,
-        Width = redaction.Width,
-        Height = redaction.Height,
-        RedactionCoordinates = redaction.RedactionCoordinates.Select(rc => new RedactionCoordinatesDto
-        {
-          X1 = rc.X1,
-          Y1 = rc.Y1,
-          X2 = rc.X2,
-          Y2 = rc.Y2
-        }).ToList()
-      };
+      _fixture = new Fixture();
+      _correlationId = _fixture.Create<Guid>();
+      _caseId = _fixture.Create<string>();
+      _documentId = _fixture.Create<string>();
 
-      redactionDefinitions.Add(redactionDefinition);
+      const string licenceFileName = "Aspose.Total.NET.lic";
+      new Aspose.Cells.License().SetLicense(licenceFileName);
+      new Aspose.Diagram.License().SetLicense(licenceFileName);
+      new Aspose.Email.License().SetLicense(licenceFileName);
+      new Aspose.Imaging.License().SetLicense(licenceFileName);
+      new Aspose.Pdf.License().SetLicense(licenceFileName);
+      new Aspose.Slides.License().SetLicense(licenceFileName);
+      new Aspose.Words.License().SetLicense(licenceFileName);
+
+      var redactionImplementation = new ImageConversionImplementation(
+        Options.Create(new ImageConversionOptions
+        {
+          Resolution = 150,
+          QualityPercent = 50
+        }));
+
+      var coordinateCalculator = new CoordinateCalculator(new Mock<ILogger<CoordinateCalculator>>().Object);
+
+      _asposeRedactionProvider = new AsposeRedactionProvider(
+        redactionImplementation,
+        coordinateCalculator,
+        new Mock<ITelemetryClient>().Object
+      );
     }
 
-    return new RedactPdfRequestDto
+    [Theory]
+    [InlineData("pdf_generator.integration.tests.TestResources.document.pdf", "pdf_generator.integration.tests.TestResources.document_redactions.json", "pdf_generator.integration.tests.TestResources.redacted_page_1.png", "pdf_generator.integration.tests.TestResources.redacted_page_2.png")]
+    [InlineData("pdf_generator.integration.tests.TestResources.overlapping_redaction.pdf", "pdf_generator.integration.tests.TestResources.overlapping_redaction_redactions.json", "pdf_generator.integration.tests.TestResources.overlapping_redaction_page_1.png", null)]
+    [InlineData("pdf_generator.integration.tests.TestResources.broken_ocr.pdf", "pdf_generator.integration.tests.TestResources.broken_ocr_redactions.json", "pdf_generator.integration.tests.TestResources.broken_ocr_page_1.png", null)]
+    public async Task RedactionAssurance(string pdfResourcePath, string redactionJsonPath, string assertionImageOnePath, string assertionImageTwoPath)
     {
-      FileName = fileName,
-      VersionId = 1,
-      RedactionDefinitions = redactionDefinitions
-    };
+      // Arrange
+      using var inputStream = GetType().Assembly.GetManifestResourceStream(pdfResourcePath) ?? throw new Exception($"{pdfResourcePath} not found");
+      using var originalDocument = new Document(inputStream);
+
+      var redactionData = LoadRedactionDataFromJson(originalDocument.FileName, redactionJsonPath);
+
+      // Act
+      using var outputStream = await _asposeRedactionProvider.Redact(inputStream, _caseId, _documentId, redactionData, _correlationId);
+      var redactedDocument = new Document(outputStream);
+
+      var redactedImageStreams = await PdfConversionHelper.ConvertAndSavePdfToImages(redactedDocument);
+
+      // Assert
+      using var assertionImageStreamOne = GetType().Assembly.GetManifestResourceStream(assertionImageOnePath) ?? throw new Exception($"{assertionImageOnePath} not found");
+      var pageOneDiff = ImageSharpCompare.CalcDiff(redactedImageStreams[0], assertionImageStreamOne, ResizeOption.Resize);
+      pageOneDiff.AbsoluteError.Should().BeLessThan(1);
+
+      if (!string.IsNullOrEmpty(assertionImageTwoPath))
+      {
+        using var assertionImageStreamTwo = GetType().Assembly.GetManifestResourceStream(assertionImageTwoPath) ?? throw new Exception($"{assertionImageTwoPath} not found");
+        var pageTwoDiff = ImageSharpCompare.CalcDiff(redactedImageStreams[1], assertionImageStreamTwo, ResizeOption.Resize);
+        pageTwoDiff.AbsoluteError.Should().BeLessThan(1);
+      }
+    }
+
+    private RedactPdfRequestDto LoadRedactionDataFromJson(string fileName, string jsonPath)
+    {
+      using var redactedStream = GetType().Assembly.GetManifestResourceStream(jsonPath) ?? throw new Exception($"{jsonPath} not found");
+      using var streamReader = new StreamReader(redactedStream);
+      var jsonText = streamReader.ReadToEnd();
+      var redactionData = JsonSerializer.Deserialize<RedactionData>(jsonText, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? throw new Exception("Failed to deserialize redaction data");
+
+      var redactionDefinitions = new List<RedactionDefinitionDto>();
+
+      foreach (var redaction in redactionData.Redactions)
+      {
+        var redactionDefinition = new RedactionDefinitionDto
+        {
+          PageIndex = redaction.PageIndex,
+          Width = redaction.Width,
+          Height = redaction.Height,
+          RedactionCoordinates = redaction.RedactionCoordinates.Select(rc => new RedactionCoordinatesDto
+          {
+            X1 = rc.X1,
+            Y1 = rc.Y1,
+            X2 = rc.X2,
+            Y2 = rc.Y2
+          }).ToList()
+        };
+
+        redactionDefinitions.Add(redactionDefinition);
+      }
+
+      return new RedactPdfRequestDto
+      {
+        FileName = fileName,
+        VersionId = 1,
+        RedactionDefinitions = redactionDefinitions
+      };
+    }
   }
 }
