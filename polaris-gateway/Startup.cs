039af7b1afb00b179f113371bca3e4ec47daa10b
Stef Stachow <stef@stachow.co.uk>
2024-03-13 13:51:27 +0000

    #26098 - remove Ddei client from gateway

--- polaris-gateway/Startup.cs
+++ polaris-gateway/Startup.cs
@@ -8,7 +8,6 @@ using PolarisGateway.Mappers;
 using PolarisGateway.Validators;
 using System.Diagnostics.CodeAnalysis;
 using System.Net.Http.Headers;
-using Ddei.Services.Extensions;
 using Microsoft.IdentityModel.Protocols;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Common.Telemetry;
@@ -66,7 +65,6 @@ namespace PolarisGateway
 
             services.AddSingleton<IRedactPdfRequestMapper, RedactPdfRequestMapper>();
 
-            services.AddDdeiClient(Configuration);
             services.AddSingleton<ITelemetryAugmentationWrapper, TelemetryAugmentationWrapper>();
             services.AddSingleton<ITelemetryClient, TelemetryClient>();
             services.AddSingleton<IUnhandledExceptionHandler, UnhandledExceptionHandler>();
