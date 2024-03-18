8a886492ae5b3d7eb92df6d48ecdb22eee606661
aceltuk <mark.jones3@cps.gov.uk>
2024-03-18 22:02:06 +0000

    Add test app to terraform - this will be reverted following testing on QA

--- polaris-terraform/pipeline-terraform/storage-account.tf
+++ polaris-terraform/pipeline-terraform/storage-account.tf
@@ -861,3 +861,9 @@ resource "azapi_resource" "pipeline_sa_text_extractor_file_share_staging1" {
   name      = "pipeline-text-extractor-content-share-1"
   parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sa_text_extractor.name}/fileServices/default"
 }
+
+resource "azapi_resource" "pipeline_sa_test_extractor_file_share" {
+  type      = "Microsoft.Storage/storageAccounts/fileServices/shares@2022-09-01"
+  name      = "pipeline-test-extractor-content-share"
+  parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sa_text_extractor.name}/fileServices/default"
+}
