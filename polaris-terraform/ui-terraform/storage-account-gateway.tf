767eb9df63ebc8e23fe3b69c97a4bdbd5caf8781
Mark Jones <aceltuk@users.noreply.github.com>
2024-03-26 09:43:36 +0000

    Feature/26379 add storage accounts for each slot (#1067)
    * WIP
    
    * Removing function app key usage (except for DDEI calls) from Polaris. Altered devops pipelines accordingly.
    
    * Formatting changes for terraform
    
    * Revert "Formatting changes for terraform"
    
    This reverts commit 4fb1242ccb0c7a290a9c30519da3d275b5ff85f7.
    
    * Removed coordinator code
    
    * Removed addition of Al's obsolete integration solution
    
    * Re-added dependencies to env.local.json in e2etests
    
    * Just some Resharper recommendations
    
    * Removed PolarisPipelineCoordinatorFunctionAppKey from lifecycle terraform settings as it no longer exists
    
    * Changes based on SonarCloud feedback
    
    * WIP #2
    
    * More corrections to complete the change

--- polaris-terraform/ui-terraform/storage-account-gateway.tf
+++ polaris-terraform/ui-terraform/storage-account-gateway.tf
@@ -1,12 +1,12 @@
-resource "azurerm_storage_account" "sa_pdf_generator" {
+﻿resource "azurerm_storage_account" "sa_gateway" {
   #checkov:skip=CKV_AZURE_206:Ensure that Storage Accounts use replication
   #checkov:skip=CKV2_AZURE_38:Ensure soft-delete is enabled on Azure storage account
   #checkov:skip=CKV2_AZURE_1:Ensure storage for critical data are encrypted with Customer Managed Key
   #checkov:skip=CKV2_AZURE_21:Ensure Storage logging is enabled for Blob service for read requests
   #checkov:skip=CKV2_AZURE_40:Ensure storage account is not configured with Shared Key authorization
-  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  name                = "sacps${var.env != "prod" ? var.env : ""}gateway"
+  resource_group_name = azurerm_resource_group.rg_polaris.name
+  location            = azurerm_resource_group.rg_polaris.location
 
   account_kind                    = "StorageV2"
   account_replication_type        = "LRS"
@@ -23,7 +23,9 @@ resource "azurerm_storage_account" "sa_pdf_generator" {
     bypass         = ["Metrics", "Logging", "AzureServices"]
     virtual_network_subnet_ids = [
       data.azurerm_subnet.polaris_ci_subnet.id,
-      data.azurerm_subnet.polaris_pdfgenerator_subnet.id,
+      data.azurerm_subnet.polaris_ui_subnet.id,
+      data.azurerm_subnet.polaris_gateway_subnet.id,
+      data.azurerm_subnet.polaris_proxy_subnet.id,
       data.azurerm_subnet.polaris_apps_subnet.id,
       data.azurerm_subnet.polaris_apps2_subnet.id
     ]
@@ -66,11 +68,11 @@ resource "azurerm_storage_account" "sa_pdf_generator" {
 
 # PDF Generator Storage Account Private Endpoint and DNS Config
 # Create Private Endpoint for Blobs
-resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_blob_pe" {
-  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-blob-pe"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+resource "azurerm_private_endpoint" "pipeline_sa_gateway_blob_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}gateway-blob-pe"
+  resource_group_name = azurerm_resource_group.rg_polaris.name
+  location            = azurerm_resource_group.rg_polaris.location
+  subnet_id           = data.azurerm_subnet.polaris_apps2_subnet.id
   tags                = local.common_tags
 
   private_dns_zone_group {
@@ -79,19 +81,19 @@ resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_blob_pe" {
   }
 
   private_service_connection {
-    name                           = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-blob-psc"
-    private_connection_resource_id = azurerm_storage_account.sa_pdf_generator.id
+    name                           = "sacps${var.env != "prod" ? var.env : ""}gateway-blob-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_gateway.id
     is_manual_connection           = false
     subresource_names              = ["blob"]
   }
 }
 
 # Create Private Endpoint for Tables
-resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_table_pe" {
-  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-table-pe"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+resource "azurerm_private_endpoint" "pipeline_sa_gateway_table_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}gateway-table-pe"
+  resource_group_name = azurerm_resource_group.rg_polaris.name
+  location            = azurerm_resource_group.rg_polaris.location
+  subnet_id           = data.azurerm_subnet.polaris_apps2_subnet.id
   tags                = local.common_tags
 
   private_dns_zone_group {
@@ -100,19 +102,19 @@ resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_table_pe" {
   }
 
   private_service_connection {
-    name                           = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-table-psc"
-    private_connection_resource_id = azurerm_storage_account.sa_pdf_generator.id
+    name                           = "sacps${var.env != "prod" ? var.env : ""}gateway-table-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_gateway.id
     is_manual_connection           = false
     subresource_names              = ["table"]
   }
 }
 
 # Create Private Endpoint for Files
-resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_file_pe" {
-  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-file-pe"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+resource "azurerm_private_endpoint" "pipeline_sa_gateway_file_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}gateway-file-pe"
+  resource_group_name = azurerm_resource_group.rg_polaris.name
+  location            = azurerm_resource_group.rg_polaris.location
+  subnet_id           = data.azurerm_subnet.polaris_apps2_subnet.id
   tags                = local.common_tags
 
   private_dns_zone_group {
@@ -121,19 +123,19 @@ resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_file_pe" {
   }
 
   private_service_connection {
-    name                           = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-file-psc"
-    private_connection_resource_id = azurerm_storage_account.sa_pdf_generator.id
+    name                           = "sacps${var.env != "prod" ? var.env : ""}gateway-file-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_gateway.id
     is_manual_connection           = false
     subresource_names              = ["file"]
   }
 }
 
 # Create Private Endpoint for Queues
-resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_queue_pe" {
-  name                = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-queue-pe"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  subnet_id           = data.azurerm_subnet.polaris_sa2_subnet.id
+resource "azurerm_private_endpoint" "pipeline_sa_gateway_queue_pe" {
+  name                = "sacps${var.env != "prod" ? var.env : ""}gateway-queue-pe"
+  resource_group_name = azurerm_resource_group.rg_polaris.name
+  location            = azurerm_resource_group.rg_polaris.location
+  subnet_id           = data.azurerm_subnet.polaris_apps2_subnet.id
   tags                = local.common_tags
 
   private_dns_zone_group {
@@ -142,15 +144,15 @@ resource "azurerm_private_endpoint" "pipeline_sa_pdf_generator_queue_pe" {
   }
 
   private_service_connection {
-    name                           = "sacps${var.env != "prod" ? var.env : ""}pdfgenerator-queue-psc"
-    private_connection_resource_id = azurerm_storage_account.sa_pdf_generator.id
+    name                           = "sacps${var.env != "prod" ? var.env : ""}gateway-queue-psc"
+    private_connection_resource_id = azurerm_storage_account.sa_gateway.id
     is_manual_connection           = false
     subresource_names              = ["queue"]
   }
 }
 
-resource "azapi_resource" "pipeline_sa_pdf_generator_file_share" {
+resource "azapi_resource" "polaris_sa_gateway_file_share" {
   type      = "Microsoft.Storage/storageAccounts/fileServices/shares@2022-09-01"
-  name      = "pipeline-pdf-generator-content-share"
-  parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sa_pdf_generator.name}/fileServices/default"
+  name      = "polaris-gateway-content-share"
+  parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg_polaris.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sa_gateway.name}/fileServices/default"
 }
