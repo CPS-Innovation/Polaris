e857568b2b822f526532d58adae9ab51dd445af8
aceltuk <mark.jones3@cps.gov.uk>
2024-04-12 13:12:28 +0100

    Catering for the root table in UAT being differently named than in the other envs!!

--- polaris-terraform/networking-terraform/lookups.tf
+++ polaris-terraform/networking-terraform/lookups.tf
@@ -4,6 +4,6 @@ data "azurerm_virtual_network" "vnet_networking" {
 }
 
 data "azurerm_route_table" "env_route_table" {
-  name                = "uks-rt-vnet-innovation-${var.environment.root_alias}"
+  name                = var.environment.root_alias == "uat" ? "uks-rt-innovation${var.environment.root_alias}" : "uks-rt-vnet-innovation-${var.environment.root_alias}"
   resource_group_name = "rg-${var.resource_name_prefix}"
 }
