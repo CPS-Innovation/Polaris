c700ae2431aafb5b7f76d0e2efa3560b8b2fe327
Renjith Abby <renjithabby@gmail.com>
2024-04-17 20:09:50 +0100

    Feature/26709:  a11y issue with CharacterCount compoenent (#1131)
    * added aria-live to text area seperatly
    
    * minor refactor
    
    * created teh CharacterCount component using TextArea, to control the aria-live notifications
    
    * minor refactor

--- polaris-ui/src/app/common/presentation/components/CharacterCount.tsx
+++ polaris-ui/src/app/common/presentation/components/CharacterCount.tsx
@@ -1,26 +1,61 @@
-import React from "react";
-import * as GDS from "govuk-react-jsx";
-
+import React, { useEffect, useState, useCallback } from "react";
+import { TextArea } from "./TextArea";
+import classes from "./CharacterCount.module.scss";
 type CharacterCountProps = {
+  name: string;
   id: string;
   label: {
     children: React.ReactNode;
   };
+  value: string;
   hint?: {
     children: React.ReactNode;
   };
+  disabled?: boolean;
+  maxCharacters: number;
   errorMessage?: {
     children: React.ReactNode;
   };
-  value: string;
-  maxlength: number;
-  name: string;
-  disabled?: boolean;
-  onChange?: (event: any) => void;
+  onChange: (value: React.ChangeEvent<HTMLTextAreaElement>) => void;
 };
-export const CharacterCount = React.forwardRef<
-  HTMLSelectElement | null,
-  CharacterCountProps
->((props, ref) => {
-  return <GDS.CharacterCount {...props} ref={ref} />;
-});
+export const CharacterCount: React.FC<CharacterCountProps> = (props) => {
+  const { maxCharacters, value } = props;
+  const [characterCount, setCharacterCount] = useState(maxCharacters);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCharacterCount(maxCharacters - value.length);
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  const getCharacterRemainingText = useCallback((count: number) => {
+    const absCount = Math.abs(count);
+    if (count >= 0) {
+      return count === 1
+        ? `You have ${absCount} character remaining`
+        : `You have ${absCount} characters remaining`;
+    }
+    return count === -1
+      ? `You have ${absCount} character too many`
+      : `You have ${absCount} characters too many`;
+  }, []);
+  return (
+    <div className={classes.characterCount}>
+      <TextArea {...props} />
+      <div
+        aria-hidden="true"
+        id={`${props.id}-info`}
+        className={`${
+          maxCharacters - value.length < 0 ? "govuk-error-message" : ""
+        }`}
+      >
+        {getCharacterRemainingText(maxCharacters - value.length)}
+      </div>
+      <div aria-live="polite" className={classes.visuallyHidden}>
+        {getCharacterRemainingText(characterCount)}
+      </div>
+    </div>
+  );
+};
