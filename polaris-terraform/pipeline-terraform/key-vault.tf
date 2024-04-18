4f982768431d98f6089ac27f8dbd4771db3fd1bf
Mark Jones <aceltuk@users.noreply.github.com>
2024-04-18 16:10:57 +0100

    Feature/26620 add sandbox deployment (#1133)
    * WIP #1
    
    * Edited UAT release pipeline (originally a copy of QA) to be simpler, minus tag management and change determination. Also added UAT config for the e2e test suite, but this contains a client ID that will only be known post first-deployment and so this will be edited once Terraform is first run and the first-run pipeline cancelled pre-e2e-test-stage.
    
    * Corrected dependency of Run_e2e_Tests stage
    
    * Removed redundant terraform key-vault permissions from pipeline terraform
    
    * Removed more redundant terraform config
    
    * Removed redundant admin pipelines
    
    * Removed deprecated block - causes error in terraform now where retention cannot be set for a new diagnostic setting
    
    * Correct the clientID held by the env.uat.json config with e2e tests now that the SPA has been deployed and registered.
    
    * Add e2e pipeline definition, by branch, where branch is supplied to it via the task runner (task in parent pipeline)
    
    * Rework UAT pipeline to run a new version of the e2e tests

--- polaris-terraform/pipeline-terraform/key-vault.tf
+++ polaris-terraform/pipeline-terraform/key-vault.tf
@@ -105,19 +105,6 @@ resource "azurerm_key_vault_access_policy" "kvap_terraform_sp" {
   ]
 }
 
-resource "azurerm_key_vault_access_policy" "terraform_kvap_terraform_sp" {
-  key_vault_id = data.azurerm_key_vault.terraform_key_vault.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azuread_service_principal.terraform_service_principal.object_id
-
-  secret_permissions = [
-    "Get",
-    "Set",
-    "Delete",
-    "Purge"
-  ]
-}
-
 resource "azurerm_role_assignment" "kv_role_fa_coordinator_crypto_user" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Crypto User"
@@ -152,18 +139,4 @@ resource "azurerm_role_assignment" "kv_role_fa_text_extractor_secrets_user" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_linux_function_app.fa_text_extractor.identity[0].principal_id
-}
-
-resource "azurerm_key_vault_secret" "kvs_pipeline_terraform_storage_connection_string" {
-  name            = "cpsdocumentstorage-connection-string"
-  value           = azurerm_storage_account.sa.primary_connection_string
-  key_vault_id    = data.azurerm_key_vault.terraform_key_vault.id
-  expiration_date = timeadd(timestamp(), "8760h")
-  content_type    = "password"
-
-  depends_on = [
-    azurerm_role_assignment.terraform_kv_role_terraform_sp,
-    azurerm_key_vault_access_policy.terraform_kvap_terraform_sp,
-    azurerm_storage_account.sa
-  ]
 }
