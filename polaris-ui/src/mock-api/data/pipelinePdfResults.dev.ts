a900ee2baed901132fda7cae78056443f5cea3fb
renjithabby <renjithabby@gmail.com>
2024-05-07 15:43:36 +0100

    added the searchPII pdf

--- polaris-ui/src/mock-api/data/pipelinePdfResults.dev.ts
+++ polaris-ui/src/mock-api/data/pipelinePdfResults.dev.ts
@@ -314,6 +314,31 @@ const pipelinePdfResult: PipelineResults = {
       hasFailedAttachments: false,
       hasNotes: false,
     },
+    {
+      documentId: "12",
+      cmsDocumentId: "12",
+      pdfBlobName: "SearchPII",
+      status: "Indexed",
+      cmsOriginalFileName: "SearchPII",
+      presentationTitle: "SearchPII",
+      cmsOriginalFileExtension: ".pdf",
+      cmsFileCreatedDate: "2020-06-02",
+      polarisDocumentVersionId: 1,
+      categoryListOrder: null,
+      cmsDocType: {
+        documentTypeId: 1019,
+        documentType: "MG12",
+        documentCategory: "MGForm",
+      },
+      presentationFlags: {
+        read: "Ok",
+        write: "Ok",
+      },
+      polarisParentDocumentId: null,
+      witnessId: null,
+      hasFailedAttachments: false,
+      hasNotes: false,
+    },
   ],
 };
 
