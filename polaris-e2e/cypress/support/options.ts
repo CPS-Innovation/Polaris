a0aac15056171972d12391d5817ae9d81c8ff219
Stef <stef@stachow.co.uk>
2024-05-08 22:30:44 +0100

    Feature/27054 recover from nuget upgrade incident (#1188)
    * #27054 - add test that breaks in DurableTask library 2.13.*
    
    * #27054 - e2e test tightening up; ensure pre-prod envs have CV autoscale on because it is degraded at the moment

--- polaris-e2e/cypress/support/options.ts
+++ polaris-e2e/cypress/support/options.ts
@@ -1,9 +1,5 @@
-export const WAIT_UNTIL_OPTIONS = { interval: 10 * 1000, timeout: 90 * 1000 }
+export const WAIT_UNTIL_OPTIONS = { interval: 5 * 1000, timeout: 90 * 1000 }
 export const RAPID_RETRY_WAIT_UNTIL_OPTIONS = {
-  interval: 0.5 * 1000,
-  timeout: 90 * 1000,
-}
-export const SHORT_TIMEOUT_WAIT_UNTIL_OPTIONS = {
   interval: 2 * 1000,
-  timeout: 15 * 1000,
+  timeout: 90 * 1000,
 }
