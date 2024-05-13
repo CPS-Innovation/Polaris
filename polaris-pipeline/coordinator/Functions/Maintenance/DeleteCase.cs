f0d36d1e2f47d2057e2cf6fa097e1f142bd8846e
Stef Stachow <stef@stachow.co.uk>
2024-05-13 15:21:18 +0100

    #27138 - delete case blobs now includes ocr jsons; require delete endpoint to called with function key

--- polaris-pipeline/coordinator/Functions/Maintenance/DeleteCase.cs
+++ polaris-pipeline/coordinator/Functions/Maintenance/DeleteCase.cs
@@ -1,53 +1,73 @@
-﻿using System;
-using System.Threading.Tasks;
-using Common.Configuration;
-using Common.Logging;
-using coordinator.Durable.Providers;
-using coordinator.Services.CleardownService;
-using Microsoft.AspNetCore.Http;
+﻿using Common.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using coordinator.Durable.Providers;
+using coordinator.Services.CleardownService;
+using Common.Extensions;
+using Microsoft.AspNetCore.Http;
+using coordinator.Helpers;
 
-namespace coordinator.Functions.Maintenance;
-
-public class DeleteCase
+namespace coordinator.Functions.Maintenance
 {
-    private readonly ILogger<DeleteCase> _logger;
-    private readonly IOrchestrationProvider _orchestrationProvider;
-    private readonly ICleardownService _cleardownService;
-
-    public DeleteCase(ILogger<DeleteCase> logger, IOrchestrationProvider orchestrationProvider, ICleardownService cleardownService)
+    public class DeleteCase
     {
-        _logger = logger;
-        _orchestrationProvider = orchestrationProvider;
-        _cleardownService = cleardownService;
-    }
+        private readonly ILogger<DeleteCase> _logger;
+        private readonly IOrchestrationProvider _orchestrationProvider;
+        private readonly ICleardownService _cleardownService;
 
-    [FunctionName(nameof(DeleteCase))]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task RunAsync(
-        [HttpTrigger(AuthorizationLevel.Function, "delete", Route = RestApi.Case)][DurableClient] IDurableOrchestrationClient client,
-        HttpRequest req,
-        string caseUrn,
-        int caseId)
-    {
-        var correlationId = Guid.NewGuid();
-        try
+        public DeleteCase(
+            ILogger<DeleteCase> logger,
+            IOrchestrationProvider orchestrationProvider,
+            ICleardownService cleardownService)
         {
-            // pass an explicit string for the caseUrn for logging purposes as we don't have access to the caseUrn here
-            await _cleardownService.DeleteCaseAsync(client,
-             caseUrn,
-             caseId,
-             correlationId,
-             waitForIndexToSettle: false);
+            _logger = logger;
+            _orchestrationProvider = orchestrationProvider;
+            _cleardownService = cleardownService;
         }
-        catch (Exception ex)
+
+        [FunctionName(nameof(DeleteCase))]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Run
+            (
+                [HttpTrigger(AuthorizationLevel.Function, "delete", Route = RestApi.Case)] HttpRequest req,
+                string caseUrn,
+                int caseId,
+                [DurableClient] IDurableOrchestrationClient orchestrationClient
+            )
         {
-            _logger.LogMethodError(correlationId, nameof(DeleteCase), ex.Message, ex);
+            Guid currentCorrelationId = default;
+
+            try
+            {
+                try
+                {
+                    currentCorrelationId = req.Headers.GetCorrelationId();
+                }
+                catch (Exception)
+                {
+                    // this is a maintenance function, so we don't want to fail the request if the correlationId is missing
+                    currentCorrelationId = Guid.NewGuid();
+                }
+
+                await _cleardownService.DeleteCaseAsync(orchestrationClient,
+                     caseUrn,
+                     caseId,
+                     currentCorrelationId,
+                     waitForIndexToSettle: true);
+
+                return new AcceptedResult();
+
+            }
+            catch (Exception ex)
+            {
+                return UnhandledExceptionHelper.HandleUnhandledException(_logger, nameof(DeleteCase), currentCorrelationId, ex);
+            }
         }
     }
 }
