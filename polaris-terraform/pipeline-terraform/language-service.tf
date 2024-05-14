71c23c8baaaff0fa27d810a57619cd0c4b479096
Neil Foubister <151544338+NeilFoubister@users.noreply.github.com>
2024-05-14 11:34:19 +0100

    #26193: Correction to the terraform cognitive services 'kind'

--- polaris-terraform/pipeline-terraform/language-service.tf
+++ polaris-terraform/pipeline-terraform/language-service.tf
@@ -3,7 +3,7 @@ resource "azurerm_cognitive_account" "language_service" {
   name                = "lang-${local.resource_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  kind                = "Language"
+  kind                = "TextAnalytics"
 
   sku_name                           = "S1"
   custom_subdomain_name              = "polaris${var.env}"
