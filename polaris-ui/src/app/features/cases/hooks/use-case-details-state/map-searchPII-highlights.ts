11b512be54b3e163fe079299a92bddd95554354d
renjithabby <renjithabby@gmail.com>
2024-06-11 11:07:14 +0100

    added  search pii integration test

--- polaris-ui/src/app/features/cases/hooks/use-case-details-state/map-searchPII-highlights.ts
+++ polaris-ui/src/app/features/cases/hooks/use-case-details-state/map-searchPII-highlights.ts
@@ -121,10 +121,11 @@ export const getFilteredByValidUkPhoneNumber = (
   results: ISearchPIIHighlight[]
 ) => {
   const UK_PHONE_NUMBER_REGEX =
-    /^(\+44\s?7\d{8,9}|\+44\s?([1-3]|[8-9])\d{8,9}|07\d{8,9}|0([1-3]|[8-9])\d{8,9}|\(?0([1-3]|[8-9])\d{2}\)?[\s-]?\d{3}[\s-]?\d{3,4}|\(?(0([1-3]|[8-9]))\d{3}\)?[\s-]?\d{3}[\s-]?\d{2,3})$/gm;
+    /^(\+447\d{8,9}|\+44([1-3]|[8-9])\d{8,9}|07\d{8,9}|0([1-3]|[8-9])\d{8,9}|\(?0([1-3]|[8-9])\d{2}\)?[-]?\d{3}[-]?\d{3,4}|\(?(0([1-3]|[8-9]))\d{3}\)?[-]?\d{3}[-]?\d{2,3})$/gm;
   return results.filter((highlight) => {
     if (highlight.piiCategory === "PhoneNumber") {
-      return !!highlight.textContent.match(UK_PHONE_NUMBER_REGEX);
+      const trimmedNumber = highlight.textContent.replace(/\s+/g, "");
+      return !!trimmedNumber.match(UK_PHONE_NUMBER_REGEX);
     }
     return true;
   });
