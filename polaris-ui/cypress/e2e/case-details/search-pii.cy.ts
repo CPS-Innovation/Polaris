f02ef4b0daff70ae059838aab99fdb143cd0da48
renjithabby <renjithabby@gmail.com>
2024-06-11 12:20:02 +0100

    minor update

--- polaris-ui/cypress/e2e/case-details/search-pii.cy.ts
+++ polaris-ui/cypress/e2e/case-details/search-pii.cy.ts
@@ -158,9 +158,9 @@ describe("Search PII", () => {
               { x1: 1.94, y1: 8.34, x2: 3.09, y2: 8.13 },
               { x1: 3.16, y1: 5.95, x2: 4.3, y2: 5.74 },
               { x1: 4.24, y1: 5.51, x2: 6.5, y2: 5.3 },
-              { x1: 2, y1: 5.28, x2: 2.96, y2: 5.11 },
               { x1: 6.57, y1: 5.28, x2: 7.24, y2: 5.07 },
               { x1: 0.98, y1: 5.06, x2: 3.99, y2: 4.86 },
+              { x1: 2, y1: 5.28, x2: 2.96, y2: 5.11 },
               { x1: 6.28, y1: 4.84, x2: 7.28, y2: 4.66 },
               { x1: 1.15, y1: 4.61, x2: 2.2, y2: 4.44 },
               { x1: 6.48, y1: 4, x2: 7.08, y2: 3.83 },
@@ -192,7 +192,75 @@ describe("Search PII", () => {
             ],
           },
         ],
+        pii: {
+          categories: [
+            {
+              polarisCategory: "Named individual",
+              providerCategory: "Person",
+              countSuggestions: 8,
+              countAccepted: 8,
+              countAmended: 0,
+            },
+            {
+              polarisCategory: "Email address",
+              providerCategory: "Email",
+              countSuggestions: 1,
+              countAccepted: 1,
+              countAmended: 0,
+            },
+            {
+              polarisCategory: "Address",
+              providerCategory: "Address",
+              countSuggestions: 2,
+              countAccepted: 2,
+              countAmended: 0,
+            },
+            {
+              polarisCategory: "Location",
+              providerCategory: "IPAddress",
+              countSuggestions: 1,
+              countAccepted: 1,
+              countAmended: 0,
+            },
+            {
+              polarisCategory: "Phone number",
+              providerCategory: "PhoneNumber",
+              countSuggestions: 5,
+              countAccepted: 5,
+              countAmended: 0,
+            },
+            {
+              polarisCategory: "NI Number",
+              providerCategory: "UKNationalInsuranceNumber",
+              countSuggestions: 2,
+              countAccepted: 2,
+              countAmended: 0,
+            },
+            {
+              polarisCategory: "Other",
+              providerCategory: "CreditCardNumber",
+              countSuggestions: 2,
+              countAccepted: 2,
+              countAmended: 0,
+            },
+            {
+              polarisCategory: "Other",
+              providerCategory: "EUDriversLicenseNumber",
+              countSuggestions: 1,
+              countAccepted: 1,
+              countAmended: 0,
+            },
+            {
+              polarisCategory: "Other",
+              providerCategory: "EUPassportNumber",
+              countSuggestions: 1,
+              countAccepted: 1,
+              countAmended: 0,
+            },
+          ],
+        },
       };
+
       const saveRequestObject = { body: "" };
       cy.trackRequestBody(
         saveRequestObject,
@@ -215,10 +283,6 @@ describe("Search PII", () => {
       cy.findByTestId("redaction-count-text-0").contains(
         "There are 23 redactions"
       );
-
-      // cy.findByTestId("b7720b68-cb4e-44bb-87b4-a767ba3d2fb0")
-      //   .findByRole("button")
-      //   .click();
       cy.findByTestId("btn-save-redaction-0").click();
 
       cy.findByTestId("div-modal").should("exist");
@@ -259,8 +323,8 @@ describe("Search PII", () => {
   });
 
   describe("Feature Flag Off", () => {
-    it("Should not show, turn on redaction suggestions menu item", () => {
-      cy.visit("/case-details/12AB1111111/13401");
+    it("Should not show, turn on redaction suggestions menu item if the feature flag is off", () => {
+      cy.visit("/case-details/12AB1111111/13401?searchPII=false");
       cy.findByTestId("btn-accordion-open-close-all").click();
       cy.findByTestId("link-document-12").click();
       cy.findByTestId("div-pdfviewer-0")
@@ -269,7 +333,7 @@ describe("Search PII", () => {
 
       cy.findByTestId("document-actions-dropdown-0").click();
       cy.findByTestId("dropdown-panel")
-        .contains("Turn off potential redactions")
+        .contains("Turn on potential redactions")
         .should("not.exist");
     });
   });
