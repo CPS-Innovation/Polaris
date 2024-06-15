e8fd47706e99bca7b38a1c233aec8c25be3b8068
Stef <stef@stachow.co.uk>
2024-06-15 17:38:18 +0100

    Feature/27615 exit long running polling (#1278)
    * #27615 - wip
    
    * #27615 - tidyups
    
    * 27615 - rename file
    
    * #27615 - tidyups
    
    * #27615 - tidyups
    
    * #27615 - tidyups

--- polaris-pipeline/coordinator/Durable/Orchestration/PollingHelper.cs
+++ polaris-pipeline/coordinator/Durable/Orchestration/PollingHelper.cs
@@ -1,4 +1,3 @@
-using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,43 +9,85 @@ namespace coordinator.Durable.Orchestration;
 public class PollingHelper
 {
     // By default, we mean do not retry
-    private static readonly RetryOptions _defaultRetryOptions = new(firstRetryInterval: TimeSpan.FromSeconds(1), maxNumberOfAttempts: 1);
-    public static async Task<(List<T>, T)> PollActivityUntilComplete<T>(IDurableOrchestrationContext context, PollingArgs pollingArgs, RetryOptions retryOptions = null)
+    private static readonly RetryOptions _defaultActivityRetryOptions = new(firstRetryInterval: TimeSpan.FromSeconds(1), maxNumberOfAttempts: 1);
+
+    public static async Task<PollingResult<T>> PollActivityUntilComplete<T>(IDurableOrchestrationContext context, PollingArgs pollingArgs)
     {
+        // Pause before making the call because we want one interval before making the first call
+        var firstCheckTime = context.CurrentUtcDateTime.AddMilliseconds(pollingArgs.PrePollingDelayMs);
+        await context.CreateTimer(firstCheckTime, CancellationToken.None);
+
         var results = new List<T>();
         while (true)
         {
-            var nextCheck = context.CurrentUtcDateTime.AddMilliseconds(pollingArgs.PollingIntervalMs);
-            await context.CreateTimer(nextCheck, CancellationToken.None);
-
+            // Note the two types of retry in this method:
+            //  1. The activity itself is retried according to the RetryOptions passed in the PollingArgs (transient HTTP errors)
+            //  2. The entire polling loop is retried until a positive flag is returned from the activity
             var (isCompleted, result) = await context.CallActivityWithRetryAsync<(bool, T)>(
                 pollingArgs.ActivityName,
-                retryOptions ?? _defaultRetryOptions,
+                pollingArgs.ActivityRetryOptions ?? _defaultActivityRetryOptions,
                 pollingArgs.ActivityInput);
 
             results.Add(result);
 
             if (isCompleted)
             {
-                return (results, result);
+                return new PollingResult<T>
+                {
+                    IsCompleted = true,
+                    Results = results,
+                    FinalResult = result
+                };
             }
+
+            if (results.Count >= pollingArgs.MaxPollingAttempts)
+            {
+                return new PollingResult<T>
+                {
+                    IsCompleted = false,
+                    Results = results,
+                    FinalResult = result
+                };
+            }
+
+            var nextCheck = context.CurrentUtcDateTime.AddMilliseconds(pollingArgs.PollingIntervalMs);
+            await context.CreateTimer(nextCheck, CancellationToken.None);
         }
     }
 
-    public static PollingArgs CreatePollingArgs(string activityName, int pollingIntervalMs, object activityInput)
+    public static PollingArgs CreatePollingArgs(
+        string activityName,
+        object activityInput,
+        int prePollingDelayMs,
+        int pollingIntervalMs,
+        int maxPollingAttempts,
+        RetryOptions activityRetryOptions = null)
     {
         return new PollingArgs
         {
             ActivityName = activityName,
             ActivityInput = activityInput,
-            PollingIntervalMs = pollingIntervalMs
+            PrePollingDelayMs = prePollingDelayMs,
+            PollingIntervalMs = pollingIntervalMs,
+            MaxPollingAttempts = maxPollingAttempts,
+            ActivityRetryOptions = activityRetryOptions,
         };
     }
+}
+
+public class PollingResult<T>
+{
+    public bool IsCompleted { get; set; }
+    public List<T> Results { get; set; }
+    public T FinalResult { get; set; }
 }
 
 public class PollingArgs
 {
     public string ActivityName { get; set; }
     public object ActivityInput { get; set; }
+    public int PrePollingDelayMs { get; set; }
     public int PollingIntervalMs { get; set; }
+    public int MaxPollingAttempts { get; set; }
+    public RetryOptions ActivityRetryOptions { get; set; }
 }
