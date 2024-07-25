fb43d63c30201d1e77259ec8632dde13b71d94c4
aceltuk <mark.jones3@cps.gov.uk>
2024-07-25 17:18:54 +0100

    WIP #1

--- polaris-terraform/ui-terraform/nginx.js
+++ polaris-terraform/ui-terraform/nginx.js
@@ -19,4 +19,127 @@ function taskListAuthRedirect(r) {
   )
 }
 
-export default { polarisAuthRedirect, taskListAuthRedirect }
+function fetchDestinationKey(r) {
+  const CORSHAM_FRAGMENT = "CPSAC";
+  const FARNBOROUGH_FRAGMENT = "CPSAF";
+  const classicOrModernFlag = r.uri.indexOf("/graphql/") > -1 ? "MODERN" : "CLASSIC";
+  const cookies = decodeURIComponent(r.headersIn.Cookie).split(/;\s*/);
+  const isCorshamCookiePresent = retrieveLoadBalancerTarget(cookies, CORSHAM_FRAGMENT);
+  const isFarnboroughCookiePresent = retrieveLoadBalancerTarget(cookies, FARNBOROUGH_FRAGMENT);
+  const clientIpAddress = r.headersIn["x-forwarded-for"];
+  const hostNameFlag = retrieveHostNameFlag(cookies); // returns e.g. CIN3
+  const corshamOrFarnboroughFlag = determineLoadBalancerTargetChoice(r.headersIn["Cms-Auth-Values"], isCorshamCookiePresent, isFarnboroughCookiePresent, clientIpAddress);
+
+  // construct a env variable key 
+  return `${classicOrModernFlag}_${corshamOrFarnboroughFlag}_${hostNameFlag}`; // e.g. "CLASSIC_CORS_CIN3"
+}
+
+function fetchDestinationIpAddress(r) {
+  // now look for the env setting for the key
+  const foundEnvSettings = process.env[r.variables["lookupKey"]];
+
+  let destinationIpAddress;
+
+  // if the key is present it would be something like "10.2.177.3;cin2.cps.gov.uk"
+  if (!!foundEnvSettings){
+    // we have a value for this key
+    destinationIpAddress = foundEnvSettings.split(";")[0];
+  } else {
+    //no value has been returned, we need to fall back to a default
+    const defaultEnvSettings = process.env["${classicOrModernFlag}_DEFAULT"];
+    destinationIpAddress = defaultEnvSettings.split(";")[0];
+  }
+
+  return destinationIpAddress;
+}
+
+function fetchDestinationHostName(r) {
+  // now look for the env setting for the key
+  const foundEnvSettings = process.env[r.variables["lookupKey"]];
+
+  let destinationHostName;
+
+  // if the key is present it would be something like "10.2.177.3;cin2.cps.gov.uk"
+  if (!!foundEnvSettings){
+    // we have a value for this key
+    destinationHostName = foundEnvSettings.split(";")[1];
+  } else {
+    //no value has been returned, we need to fall back to a default
+    const defaultEnvSettings = process.env["${classicOrModernFlag}_DEFAULT"];
+    destinationHostName = defaultEnvSettings.split(";")[1];
+  }
+
+  return destinationHostName;
+}
+
+function retrieveLoadBalancerTarget(cookies, target) {
+  const cookieSearch = cookies.filter(c => c.toUpperCase().indexOf(target) > -1);
+
+  let result = false;
+  if (cookieSearch[0] !== undefined) {
+    result = true;
+  }
+  return result;
+}
+
+function retrieveHostNameFlag(cookies) {
+  const cookieSearch = cookies.filter(c => c.toUpperCase().indexOf("BIGIPSERVER") > -1);
+
+  let result = "DEFAULT";
+  if (cookieSearch[0] !== undefined) {
+    const str = cookieSearch[0];
+    const startPos = str.toUpperCase().indexOf(".CPS.GOV.UK");
+    result = getHostName(str, startPos-1);
+  }
+  return result;
+}
+
+function getHostName(str, startPoint) {
+  let hostName = "";
+  for (let i = startPoint; i >= 0; i--) {
+    if (str[i] === "-") {
+      break;
+    }
+    hostName += str[i];
+  }
+  return hostName.toUpperCase().split("").reverse().join("");
+}
+
+function determineLoadBalancerTargetChoice(cmsAuthValues, isCorshamCookiePresent, isFarnboroughCookiePresent, clientIpAddress) {
+  const CORSHAM_FLAG = "CORS";
+  const FARNBOROUGH_FLAG = "FARN";
+
+  if (cmsAuthValues != null && cmsAuthValues !== '') {
+    const cmsAuthObj = JSON.parse(cmsAuthValues);
+    if (typeof(cmsAuthObj) != 'undefined' && cmsAuthObj != null)
+    {
+      const preferredLoadBalancerTarget = cmsAuthObj.PreferredLoadBalancerTarget;
+      if (preferredLoadBalancerTarget !== "") {
+        return preferredLoadBalancerTarget === "0" ? CORSHAM_FLAG : FARNBOROUGH_FLAG; 
+      }
+      
+      const ipAddress = clientIpAddress === "" ? cmsAuthObj.UserIpAddress : clientIpAddress;
+      const isCorshamLeaningIpAddress = isCorshamLeaningIpAddress(ipAddress);
+
+      if (isFarnboroughCookiePresent && isCorshamCookiePresent) {
+        return isCorshamLeaningIpAddress ? CORSHAM_FLAG : FARNBOROUGH_FLAG;
+      }
+
+      if (isFarnboroughCookiePresent) {
+        return FARNBOROUGH_FLAG;
+      }
+
+      if (isCorshamCookiePresent) {
+        return CORSHAM_FLAG;
+      }
+    }
+  }
+
+  return CORSHAM_FLAG;
+}
+
+function isCorshamLeaningIpAddress(userIpAddress) {
+  return (typeof(userIpAddress) != 'undefined' && userIpAddress !== "" && userIpAddress.startsWith("10.12."));
+}
+
+export default { polarisAuthRedirect, taskListAuthRedirect, fetchDestinationKey, fetchDestinationIpAddress, fetchDestinationHostName }
