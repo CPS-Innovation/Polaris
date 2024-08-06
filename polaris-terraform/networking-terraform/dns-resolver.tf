8bbb56e255d2aa9fa018375d92c017c0b36b0f4d
aceltuk <mark.jones3@cps.gov.uk>
2024-08-06 13:26:00 +0100

    Correct name of dns resolver definition to prevent attempted recreation

--- polaris-terraform/networking-terraform/dns-resolver.tf
+++ polaris-terraform/networking-terraform/dns-resolver.tf
@@ -1,5 +1,5 @@
 resource "azurerm_private_dns_resolver" "polaris_dns_resolver" {
-  name                = "example"
+  name                = "polaris-dns-resolver"
   resource_group_name = data.azurerm_resource_group.rg_networking.name
   location            = data.azurerm_resource_group.rg_networking.location
   virtual_network_id  = data.azurerm_virtual_network.vnet_networking.id
