5301d5fc5de17e6dd7ea6ee7b618469b2b9a7c6d
Jamie Brown <jamie.brown@cps.gov.uk>
2024-09-02 10:42:06 +0100

    Add P button

--- polaris-terraform/ui-terraform/cmsenv.js
+++ polaris-terraform/ui-terraform/cmsenv.js
@@ -70,6 +70,19 @@ function replaceCmsDomainsAjaxViewer(r, data, flags)
     __replaceCmsDomainsGeneric(r, data, flags, r.variables.websiteHostname);
 }
 
+function cmsMenuBarFilters(r, data, flags)
+{
+    data = __addAppLaunchButtonsToMenuBar(r, data, flags);
+    replaceCmsDomains(r, data, flags);
+}
+
+function __addAppLaunchButtonsToMenuBar(r, data, flags)
+{
+    data = data.replace(new RegExp('objMainWindow\.top\.frameData\.objMasterWindow\.top\.frameServerJS\.POLARIS_URL', 'g'), '"/polaris"');
+    data = data.replace(new RegExp('MENU_BAR_POLARIS_LOGO', 'g'), '"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAEnQAABJ0Ad5mH3gAAAJKSURBVEhLtZXPS9tgGMe/6Q9r01Y7pyiWlWFRmTpXNybOy2AIO6gTBwpWYV4Ezz3sH1AP/gPuphdF1It49bLD8FAnMoa0MBXGJrYeiiFNbU2b+OTHac2yRNYPNHngffJ++zzfJ28YmUAVcej3qmFawfr6d8iyBIYBampcKJeBqaluiGIZbrdTzzLHVIBlP+LmpqjGLpcPXq8DR0dxrK5+xcDAQ4yNvVDXzDBtUTAYoGu9EqGhoQ7NzUESOMfgYAjb2yns75/h5CSt5v4Nyx4wjIxi0UFVsNjZSSIWe47Fxc9oa2tEMnmlZ1Vi2qLW1gVcXooUMQgEnJidjSKXKyGd5mktiJ6eehwe/sTGxgftAQMsV8DzBczPv6L+v8fISCdt/Fv999FohPz4pGdVYmtMeV4zfG7uJdrbH2Bv7weamnxYWnqL21saMQPu9R44nQxKJRkcJ9KUichkBEiScadtCfj9HvW+ufmNNs1hZqYb2WweKytfUFvrUtf+xKbJz5DPSzg9zaKjoxGRiB+pVAZrazHtAQMsV8CybuzunmF0tBPhcB36+0M4OEhjefkdEolfelYllgWUOj0eiXqex/j4E9r8HJOTT3FxwZHYIz2rEssCDB1IHCfQWD7G8fEVhobCmJ7uRV9fSM8wxrIHXi8Qj7+mt/cNBKEIn08z/F9YrkAZx4mJLjW2urmCrTEtFEp6ZB1bAsp3wS6mAtfXAl15/Zej09T4ODDD1OStrQQZqn3RJEnC8HAvWlr8+qo1TAX+B7Y8uA9VFgDuAGLN1z00rPbxAAAAAElFTkSuQmCC"');
+    return data;
+}
+
 function __replaceCmsDomainsGeneric(r, data, flags, host)
 {
     // If a 302 has been issued then there's no point in processing in the response body
@@ -120,5 +133,6 @@ function __getCmsEnv(r)
 export default {
     proxyDestinationCorsham, proxyDestinationCorshamInternal, proxyDestinationModernCorsham, proxyDestinationModernCorshamInternal,
     proxyDestinationFarnborough, proxyDestinationFarnboroughInternal, proxyDestinationModernFarnborough, proxyDestinationModernFarnboroughInternal,
-    upstreamCmsDomainName, upstreamCmsModernDomainName, replaceCmsDomains, replaceCmsDomainsAjaxViewer, upstreamCmsServicesDomainName
+    upstreamCmsDomainName, upstreamCmsModernDomainName, replaceCmsDomains, replaceCmsDomainsAjaxViewer, upstreamCmsServicesDomainName,
+    cmsMenuBarFilters
 }
