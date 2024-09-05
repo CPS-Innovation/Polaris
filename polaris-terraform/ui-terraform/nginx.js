574db1253bd8346755066852b58b5021db455c2c
Stef Stachow <stef@stachow.co.uk>
2024-09-05 14:30:58 +0100

    Fix njs logic

--- polaris-terraform/ui-terraform/nginx.js
+++ polaris-terraform/ui-terraform/nginx.js
@@ -1,26 +1,33 @@
-function _legacyHandoverFormatShim(r) {
+import qs from "querystring"
+
+function _argsShim(args) {
+  if (args["r"]) {
+    return args
+  }
   // If we have no r param then we assume we are processing a legacy handover from the /polaris endpoint.
   // The CMS P button has no concept of the r param and assumes this endpoint forwards on to CWA domain.
   // So lets coerce the legacy format to the standard format by creating an r param if one does not exist.
   // Note 1: the expected incoming params in the legacy case are q and referer.
   // Note 2: we use a relative URL rather than a fully-qualified URL as the proxy runs under multiple names
   //  e.g. https://polaris-cmsproxy.azurewebsites.net/ and https://polaris.cps.gov.uk/
-  return {
-    variables: { ...r.variables },
-    args: {
-      ...r.args,
-      r:
-        r.args["r"] ??
-        `/auth-refresh-inbound?q=${r.args["q"]}&referer=${r.args["referer"]}`,
-    },
-  }
+
+  const serializedArgs = qs.stringify(args)
+  const clonedArgsToMutate = qs.parse(serializedArgs)
+  delete clonedArgsToMutate["cookie"]
+  // do not serialize cookie into our manufactured r param because cookie will be attached as the __cc param later on
+  const queryStringWithoutCookie = qs.stringify(clonedArgsToMutate)
+
+  const clonedArgs = qs.parse(serializedArgs)
+  clonedArgs["r"] = `/auth-refresh-inbound?${queryStringWithoutCookie}`
+  return clonedArgs
 }
 
-async function appAuthRedirect(r) {
-  r = _legacyHandoverFormatShim(r)
+function appAuthRedirect(r) {
+  const args = _argsShim(r.args)
 
-  const redirectUrl = r.args["r"]
-  const isWhitelisted = (r.variables.whitelistedUrls ?? "")
+  const whitelistedUrls = process.env.AUTH_HANDOVER_WHITELIST ?? ""
+  const redirectUrl = args["r"]
+  const isWhitelisted = whitelistedUrls
     .split(",")
     .some((url) => redirectUrl.startsWith(url))
 
@@ -29,29 +36,28 @@ async function appAuthRedirect(r) {
       302,
       `${redirectUrl}${
         redirectUrl.includes("?") ? "&" : "?"
-      }__cc=${encodeURIComponent(r.args["cookie"] ?? "")}`
+      }__cc=${encodeURIComponent(args["cookie"] ?? "")}`
     )
   } else {
-    r.return(403)
+    r.return(
+      403,
+      `HTTP Status 403: this deployment of the /init endpoint will only accept requests with r query parameters that start with one of the following strings: ${whitelistedUrls}`
+    )
   }
 }
 
 // This is a simulation of the https://cms.cps.gov.uk/polaris endpoint.
 //  Primarily useful when users are using CMS delivered through this proxy. In this use case, users are on this proxy
 //  domain when using CMS.  We inject a P button and simulated the prod /polaris handover endpoint using this function.
-async function polarisAuthRedirect(r) {
-  const redirectHostAddress = r.variables.redirectHostAddress ?? ""
-  const cookie = encodeURIComponent(r.headersIn.Cookie ?? "")
-  const referer = encodeURIComponent(r.headersIn.Referer ?? "")
-  const polarisUiUrl = encodeURIComponent(r.args["polaris-ui-url"] ?? "")
-  const q = r.args["q"] ?? ""
+function polarisAuthRedirect(r) {
+  const serializedArgs = qs.stringify(args)
+  const clonedArgs = qs.parse(serializedArgs)
+  clonedArgs.cookie = r.headersIn.Cookie
+  clonedArgs.referer = r.headersIn.Referer
 
-  const redirect = r.args["r"] ?? ""
+  const querystring = qs.stringify(clonedArgs)
 
-  r.return(
-    302,
-    `${redirectHostAddress}/init?cookie=${cookie}&referer=${referer}&polaris-ui-url=${polarisUiUrl}&q=${q}&r=${redirect}`
-  )
+  r.return(302, `/init?${querystring}`)
 }
 
 function taskListAuthRedirect(r) {
