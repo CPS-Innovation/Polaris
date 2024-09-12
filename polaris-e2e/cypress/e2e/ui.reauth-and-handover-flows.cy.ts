49ddc110d8d44a879e6ab63e9df369642536b002
Stef <stef@stachow.co.uk>
2024-09-12 13:38:46 +0100

    Feature/28039 inbound handover to polaris UI (#1394)
    * wip - broken tests
    
    * #28039 - completed unit and e2e tests
    
    * tidyups
    
    * tidyups
    
    * #28039 - make parallelism of e2e safe by combining reauth and handover tests (they both operate on the same URN)

---
+++ polaris-e2e/cypress/e2e/ui.reauth-and-handover-flows.cy.ts
@@ -0,0 +1,87 @@
+/// <reference types="cypress" />
+
+const { TARGET_URN, TARGET_CASE_ID } = Cypress.env()
+describe(
+  "Reauth and handover flows",
+  { tags: ["@ci", "@notrunprodci", "@ci-chunk-2"] },
+  () => {
+    
+    describe("Reauth flow", () => {
+      it("can send the user around the reauth flow", () => {
+        cy.on("uncaught:exception", () => false)
+
+        // have we logged in OK?
+        cy.loginToAD()
+          .visit(`/polaris-ui/case-search-results?urn=${TARGET_URN}`)
+          // we expect to not be logged-in to CMS...
+          .contains("CMS_AUTH_ERROR")
+          // ... and now we do log in
+          .loginToCms()
+
+        cy.visit(`/polaris-ui/case-search-results?urn=${TARGET_URN}`)
+
+        // if we find the target URN we have made a successful round trip to DDEI
+        cy.findByTestId(`link-${TARGET_URN}`)
+      })
+    })
+
+    describe("Inbound handover", () => {
+      it("can receive a well formed triage handover and redirect the user to the requested case", () => {
+        cy.on("uncaught:exception", () => false)
+
+        const contextObject = {
+          caseId: +TARGET_CASE_ID,
+          taskId: 1,
+          taskTypeId: 2,
+        }
+
+        const contextQueryParam = `${encodeURIComponent(
+          JSON.stringify(contextObject)
+        )}`
+
+        cy.fullLogin().visit(`/polaris-ui/go?ctx=${contextQueryParam}`)
+
+        // temporary logic to assert that context is handed-down to the page
+        cy.contains('{"contextType":"triage","taskId":1,"taskTypeId":2}')
+      })
+
+      it("can receive a well formed triage handover when the user is not logged in to CMS and inform the user", () => {
+        cy.on("uncaught:exception", () => false)
+
+        const contextObject = {
+          caseId: +TARGET_CASE_ID,
+          taskId: 1,
+          taskTypeId: 2,
+        }
+
+        const contextQueryParam = `${encodeURIComponent(
+          JSON.stringify(contextObject)
+        )}`
+
+        cy.loginToAD().visit(`/polaris-ui/go?ctx=${contextQueryParam}`)
+
+        // temporary logic to assert that context is handed-down to the page
+        cy.contains("CMS_AUTH_ERROR")
+      })
+
+      it("can receive a well formed triage handover and not continue if the call to the lookup urn api is not successful", () => {
+        cy.on("uncaught:exception", () => false)
+
+        const contextObject = {
+          caseId: -999,
+          taskId: 1,
+          taskTypeId: 2,
+        }
+
+        const contextQueryParam = `${encodeURIComponent(
+          JSON.stringify(contextObject)
+        )}`
+
+        cy.fullLogin().visit(`/polaris-ui/go?ctx=${contextQueryParam}`)
+
+        // temporary logic to assert that context is handed-down to the page
+        cy.contains("API_ERROR")
+      })
+    })
+  }
+)
