767b2930e38a1e8333b89073ebca6f5c5132935d
Stef Stachow <stef@stachow.co.uk>
2024-09-13 09:10:48 +0100

    #28102 - highlight that reauth e2e test cannot be run in prod

---
+++ polaris-e2e/cypress/e2e/ui.reauth-flow.cy.ts
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+const { TARGET_URN } = Cypress.env()
+
+describe(
+  "Reauth flow",
+  // Note: this is not run in prod because the e2e test agent cannot see the cms.cps.gov.uk/polaris
+  //  handover endpoint
+  { tags: ["@ci", "@notrunprodci", "@ci-chunk-2"] },
+  () => {
+    it("can send the user around the reauth flow", () => {
+      cy.on("uncaught:exception", () => false)
+
+      // have we logged in OK?
+      cy.loginToAD()
+        .visit(`/polaris-ui/case-search-results?urn=${TARGET_URN}`)
+        // we expect to not be logged-in to CMS...
+        .contains("CMS_AUTH_ERROR")
+        // ... and now we do log in
+        .loginToCms()
+
+      cy.visit(`/polaris-ui/case-search-results?urn=${TARGET_URN}`)
+
+      // if we find the target URN we have made a successful round trip to DDEI
+      cy.findByTestId(`link-${TARGET_URN}`)
+    })
+  }
+)
+
+export {}
