acaa1e981680280ad10dcdcfaabf216a2789c191
Rhys Bridges <rhys@hampton.io>
2024-09-30 13:08:53 +0100

    #27910 thumbnail generator tidyup

--- polaris-pipeline/pdf-thumbnail-generator/TelementryEvents/DeleteCaseThumbnailEvent.cs
+++ polaris-pipeline/pdf-thumbnail-generator/TelementryEvents/DeleteCaseThumbnailEvent.cs
@@ -15,22 +15,13 @@ namespace pdf_thumbnail_generator.TelemetryEvents
     public Guid CorrelationId;
     public string InstanceId;
     public DateTime StartTime;
-    public DateTime RemovedCaseIndexTime;
-    public DateTime IndexSettledTime;
     public DateTime BlobsDeletedTime;
     public DateTime GotTerminateInstancesTime;
     public DateTime TerminatedInstancesTime;
     public DateTime TerminatedInstancesSettledTime;
     public DateTime GotPurgeInstancesTime;
     public DateTime EndTime;
-    public long AttemptedRemovedDocumentCount;
-    public long SuccessfulRemovedDocumentCount;
-    public long FailedRemovedDocumentCount;
-    public bool DidWaitForIndexToSettle;
-    public bool DidIndexSettle;
-    public List<long> WaitRecordCounts;
     public int TerminatedInstancesCount;
-    public bool DidClearBlobs;
     public bool DidOrchestrationsTerminate;
     public int PurgeInstancesCount;
     public int PurgedInstancesCount;
@@ -55,26 +46,16 @@ namespace pdf_thumbnail_generator.TelemetryEvents
                     { nameof(InstanceId), InstanceId.ToString() },
                     { nameof(StartTime), StartTime.ToString("o") },
                     { nameof(EndTime), EndTime.ToString("o") },
-                    { nameof(DidWaitForIndexToSettle), DidWaitForIndexToSettle.ToString() },
-                    { nameof(DidIndexSettle), DidIndexSettle.ToString() },
-                    { nameof(WaitRecordCounts), string.Join(",", WaitRecordCounts ?? Enumerable.Empty<long>()) },
-                    { nameof(DidClearBlobs), DidClearBlobs.ToString() },
                     { nameof(DidOrchestrationsTerminate), DidOrchestrationsTerminate.ToString() },
           },
           new Dictionary<string, double?>
           {
                     { durationSeconds, GetDurationSeconds( StartTime, EndTime) },
-                    { IndexDeletedDurationSeconds, GetDurationSeconds(StartTime, RemovedCaseIndexTime) },
-                    { IndexSettledDurationSeconds, GetDurationSeconds(RemovedCaseIndexTime, IndexSettledTime) },
-                    { BlobsDeletedDurationSeconds, GetDurationSeconds(IndexSettledTime, BlobsDeletedTime) },
                     { GetInstancesToTerminateDurationSeconds, GetDurationSeconds(BlobsDeletedTime, GotTerminateInstancesTime) },
                     { TerminateInstancesDurationSeconds, GetDurationSeconds(GotTerminateInstancesTime, TerminatedInstancesTime) },
                     { TerminatedInstancesSettledDurationSeconds, GetDurationSeconds(TerminatedInstancesTime, TerminatedInstancesSettledTime) },
                     { GetInstancesToPurgeDurationSeconds, GetDurationSeconds(TerminatedInstancesSettledTime, GotPurgeInstancesTime) },
                     { PurgedInstancesDurationSeconds, GetDurationSeconds(GotPurgeInstancesTime, EndTime) },
-                    { nameof(AttemptedRemovedDocumentCount), AttemptedRemovedDocumentCount },
-                    { nameof(SuccessfulRemovedDocumentCount), SuccessfulRemovedDocumentCount },
-                    { nameof(FailedRemovedDocumentCount), FailedRemovedDocumentCount },
                     { nameof(TerminatedInstancesCount), TerminatedInstancesCount },
                     { nameof(PurgeInstancesCount), PurgeInstancesCount },
                     { nameof(PurgedInstancesCount), PurgedInstancesCount },
