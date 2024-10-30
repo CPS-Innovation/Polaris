535c033c3e3fd16e9868b2a4f099952e385914d8
renjithabby <renjithabby@gmail.com>
2024-10-30 11:57:21 +0000

    minor refactor to update the type for showing redaction warning message

--- polaris-ui/src/app/features/cases/presentation/case-details/pdf-viewer/RedactionWarning.tsx
+++ polaris-ui/src/app/features/cases/presentation/case-details/pdf-viewer/RedactionWarning.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import classes from "./RedactionWarning.module.scss";
 import { PresentationFlags } from "../../../domain/gateway/PipelineDocument";
 type Props = {
-  documentWriteStatus: PresentationFlags["write"];
+  documentWriteStatus: PresentationFlags["write"] | "IsPageRotationModeOn";
 };
 
 export const RedactionWarning: React.FC<Props> = ({ documentWriteStatus }) => {
-  const getWarningText = (documentWriteStatus: PresentationFlags["write"]) => {
+  const getWarningText = (
+    documentWriteStatus: PresentationFlags["write"] | "IsPageRotationModeOn"
+  ) => {
     switch (documentWriteStatus) {
       case "IsRedactionServiceOffline":
         return "Redaction is currently unavailable and undergoing maintenance.";
@@ -19,7 +21,7 @@ export const RedactionWarning: React.FC<Props> = ({ documentWriteStatus }) => {
       case "IsDispatched":
         return "This is a dispatched document.";
       case "IsPageRotationModeOn":
-        return "Redaction is unavailable in page rotation mode, please turn off page rotation to continue with redaction. ";
+        return "Redaction is unavailable in page rotation mode, please turn off page rotation to continue with redaction.";
     }
   };
   return (
