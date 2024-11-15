b5ac4d5d828b753c517bb3205d2d239370cca7e3
aceltuk <mark.jones3@cps.gov.uk>
2024-11-15 17:27:20 +0000

    Corrected typo

--- polaris-terraform/search-index-terraform/lookups.tf
+++ polaris-terraform/search-index-terraform/lookups.tf
@@ -1,4 +1,4 @@
 ﻿data "azurerm_search_service" "ss" {
-  name = "ss-polaris-pipline${local.resource_suffix}"
+  name = "ss-polaris-pipeline${local.resource_suffix}"
   resource_group_name = "rg-polaris-pipeline${local.resource_suffix}"
 }
