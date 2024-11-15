dd7eb8b7de8c6a2aad0c6464930177f04194c892
aceltuk <mark.jones3@cps.gov.uk>
2024-11-15 13:57:19 +0000

    Correct deprecations and delay search-service evaluation

--- polaris-terraform/main-terraform/search-index.tf
+++ polaris-terraform/main-terraform/search-index.tf
@@ -1,14 +1,14 @@
-provider "restapi" {
-  uri                  = "https://${azurerm_search_service.ss.name}.search.windows.net"
-  debug                = true
-  write_returns_object = true
-  alias                = "restapi_headers"
-  headers = {
-    Content-Type = "application/json"
-    api-key      = azurerm_search_service.ss.primary_key
-  }
-  id_attribute = "name"
-}
+#provider "restapi" {
+#  uri                  = "https://${azurerm_search_service.ss.name}.search.windows.net"
+#  debug                = true
+#  write_returns_object = true
+#  alias                = "restapi_headers"
+#  headers = {
+#    Content-Type = "application/json"
+#    api-key      = azurerm_search_service.ss.primary_key
+#  }
+#  id_attribute = "name"
+#}
 
 #resource "restapi_object" "definition" {
 #  provider     = restapi.restapi_headers
