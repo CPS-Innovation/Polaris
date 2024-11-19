44721b0c83c60400801e308a24d78945e6c385d3
Mark Jones <aceltuk@users.noreply.github.com>
2024-11-19 17:51:01 +0000

    Change deployed name of the PDF Thumbnail Generator to be less than 32 chars long and thus avoid naming collisions. (#1501)

--- polaris-terraform/main-terraform/service-plan-pdf-thumbnail-generator.tf
+++ polaris-terraform/main-terraform/service-plan-pdf-thumbnail-generator.tf
@@ -3,7 +3,7 @@
 resource "azurerm_service_plan" "asp_polaris_pipeline_ep_pdf_thumbnail_generator" {
   #checkov:skip=CKV_AZURE_212:Ensure App Service has a minimum number of instances for fail over
   #checkov:skip=CKV_AZURE_225:Ensure the App Service Plan is zone redundant
-  name                         = "asp-pdf-thumbnail-generator-ep-${local.pipeline_resource_name}"
+  name                         = "asp-pdf-thumb-gen-ep-${local.pipeline_resource_name}"
   location                     = azurerm_resource_group.rg_polaris_pipeline.location
   resource_group_name          = azurerm_resource_group.rg_polaris_pipeline.name
   os_type                      = "Windows"
