d259bb76eefddf3a1411dae4510fa2795047b8d8
renjithabby <renjithabby@gmail.com>
2024-11-20 11:11:48 +0000

    more unit tests

--- polaris-ui/src/app/features/cases/hooks/use-case-details-state/reducer.test.ts
+++ polaris-ui/src/app/features/cases/hooks/use-case-details-state/reducer.test.ts
@@ -2045,6 +2045,78 @@ describe("useCaseDetailsState reducer", () => {
     });
   });
 
+  describe("UPDATE_PIPELINE_REFRESH", () => {
+    it("can update pipelineRefreshData", () => {
+      const existingState = {
+        pipelineRefreshData: {
+          startPipelineRefresh: false,
+          lastProcessingCompleted: "2023-04-05T15:02:17.601Z",
+          localLastRefreshTime: "2023-04-05T15:02:17.601Z",
+        },
+      } as unknown as CombinedState;
+
+      const result = reducer(existingState as CombinedState, {
+        type: "UPDATE_PIPELINE_REFRESH",
+        payload: {
+          startPipelineRefresh: true,
+        },
+      });
+
+      expect(result).toEqual({
+        pipelineRefreshData: {
+          startPipelineRefresh: true,
+          lastProcessingCompleted: "2023-04-05T15:02:17.601Z",
+          localLastRefreshTime: "2023-04-05T15:02:17.601Z",
+        },
+      });
+    });
+  });
+  describe("UPDATE_CONVERSION_STATUS", () => {
+    it("can update conversion status of already existing document in the localDocumentState", () => {
+      const existingState = {
+        localDocumentState: {
+          "1": { conversionStatus: "DocumentConverted" },
+        },
+      } as unknown as CombinedState;
+
+      const result = reducer(existingState as CombinedState, {
+        type: "UPDATE_CONVERSION_STATUS",
+        payload: {
+          documentId: "1",
+          status: "EncryptionOrPasswordProtection",
+        },
+      });
+
+      expect(result).toEqual({
+        localDocumentState: {
+          "1": { conversionStatus: "EncryptionOrPasswordProtection" },
+        },
+      });
+    });
+    it("can add new documents with conversion status in the localDocumentState", () => {
+      const existingState = {
+        localDocumentState: {
+          "1": { conversionStatus: "DocumentConverted" },
+        },
+      } as unknown as CombinedState;
+
+      const result = reducer(existingState as CombinedState, {
+        type: "UPDATE_CONVERSION_STATUS",
+        payload: {
+          documentId: "2",
+          status: "EncryptionOrPasswordProtection",
+        },
+      });
+
+      expect(result).toEqual({
+        localDocumentState: {
+          "1": { conversionStatus: "DocumentConverted" },
+          "2": { conversionStatus: "EncryptionOrPasswordProtection" },
+        },
+      });
+    });
+  });
+
   describe("SHOW_ERROR_MODAL", () => {
     it("can show the error modal", () => {
       const existingState = {
