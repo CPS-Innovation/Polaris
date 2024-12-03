737ff347ce0dc9ce103cf651cdd47dc8efd40c06
shellisj-spc <James.Shellis@cps.gov.uk>
2024-12-03 16:19:16 +0000

    add LF (#1525)

--- polaris-devops-pipelines/scale-set/build-agent.sh
+++ polaris-devops-pipelines/scale-set/build-agent.sh
@@ -101,3 +101,6 @@ wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 sudo chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --channel 6.0 --quality GA
 ./dotnet-install.sh --channel 7.0 --quality GA
+
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> ~/.bashrc
