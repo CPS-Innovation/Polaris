454124cfdadac5944674918954f1f4bfadceee20
rafalcps <rafal.drozdowski@cps.gov.uk>
2025-01-03 12:28:33 +0000

    selects radio state on load

--- polaris-ui/src/app/features/cases/presentation/case-details/change-radio/Change-radio.tsx
+++ polaris-ui/src/app/features/cases/presentation/case-details/change-radio/Change-radio.tsx
@@ -1,86 +1,78 @@
 import React, { useReducer } from "react";
-import { Input, Radios, LinkButton } from "../../../../../common/presentation/components";
-import './Change-radio.scss';
+import {
+  Input,
+  Radios,
+  LinkButton,
+} from "../../../../../common/presentation/components";
+import "./Change-radio.scss";
 
 type ChangeRadioProps = {
-    handleClose: () => void;
-}
+  handleClose: () => void;
+};
 
-const ChangeRadio: React.FC<ChangeRadioProps> = ({
-    handleClose
-}) => {
+const ChangeRadio: React.FC<ChangeRadioProps> = ({ handleClose }) => {
+  const initialState = {
+    RadioButtonsChange: "Used",
+  };
 
-    const initialState = {
-        RadioButtonsChange: 'used'
-    }
-
-    const reducer = (state: "used" | "unused", action: any) => {
-        switch (action.type) {
-            case 'USED':
-                return {
-                    RadioButtonsChange: 'used'
-                }
-            case 'UNUSED':
-                return {
-                    RadioButtonsChange: 'unused'
-                }
-            default:
-                return state
+  const reducer = (state: "Used" | "Unused", action: any) => {
+    switch (action.type) {
+      case "Used":
+        return {
+          RadioButtonsChange: "Used",
         };
-    };
-
-    const [state, dispatch] = useReducer(reducer, initialState)
-
-    const handleRadioButtonsChange = (value: boolean) => {
-        dispatch({ type: value.toString().toUpperCase(), payload: null })
+      case "Unused":
+        return {
+          RadioButtonsChange: "Unused",
+        };
+      default:
+        return state;
     }
+  };
 
-    return (
-        <div>
-            <LinkButton
-                className={"govuk-back-link"}
-                onClick={handleClose}
-            >
-                Back
-            </LinkButton>
-            <Radios
-                fieldset={{
-                    legend: {
-                        children: (
-                            <span>
-                                What is the document status?
-                            </span>
-                        ),
-                    },
-                }}
-                // className={
-                //     formDataErrors.documentNewNameErrorText
-                //         ? "govuk-form-group--error"
-                //         : ""
-                // }
-                key={"reclassify-change-radio=buttons"}
-                onChange={(arg: any) => {
-                    arg &&
-                        handleRadioButtonsChange(arg);
-                }
-                }
-                value={state.used}
-                name="reclassify-change-radio=buttons"
-                items={[
-                    {
-                        children: "Used",
-                        value: "Used",
-                    },
-                    {
-                        children: "Unused",
-                        value: "Unused",
-                    },
-                ]}
-                data-testid="reclassify-change"
-            />
-            {console.log("end: ", state.RadioButtonsChange)}
-        </div>
-    )
-}
+  const [state, dispatch] = useReducer<any>(reducer, initialState);
 
-export { ChangeRadio }
+  const handleRadioButtonsChange = (value: string | undefined) => {
+    console.log("value: ", value);
+    dispatch({ type: value });
+  };
+  console.log("state.RadioButtonsChange", state?.RadioButtonsChange);
+  return (
+    <div>
+      <LinkButton className={"govuk-back-link"} onClick={handleClose}>
+        Back
+      </LinkButton>
+      <Radios
+        fieldset={{
+          legend: {
+            children: <span>What is the document status?</span>,
+          },
+        }}
+        // className={
+        //     formDataErrors.documentNewNameErrorText
+        //         ? "govuk-form-group--error"
+        //         : ""
+        // }
+        key={"reclassify-change-radio-buttons"}
+        onChange={(arg: string | undefined) => {
+          arg && handleRadioButtonsChange(arg);
+        }}
+        value={state?.RadioButtonsChange}
+        name="reclassify-change-radio=buttons"
+        items={[
+          {
+            children: "Used",
+            value: "Used",
+          },
+          {
+            children: "Unused",
+            value: "Unused",
+          },
+        ]}
+        data-testid="reclassify-change"
+      />
+    </div>
+  );
+};
+
+export { ChangeRadio };
