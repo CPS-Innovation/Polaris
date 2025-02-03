842d05cbc8ae47381e476fcf05feb2412eed7873
rafalcps <rafal.drozdowski@cps.gov.uk>
2025-02-03 11:07:25 +0000

    removes statement indicated by quality check report

--- polaris-ui/src/app/features/cases/presentation/case-details/index.tsx
+++ polaris-ui/src/app/features/cases/presentation/case-details/index.tsx
@@ -170,7 +170,6 @@ export const Page: React.FC<Props> = ({ backLinkProps, context }) => {
     documentsState,
     documentRefreshData,
     errorModal,
-    documentIssueModal,
     redactionLog,
     featureFlags,
     storedUserData,
