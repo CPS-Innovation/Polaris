8221c62f7eee7b5bff8f73f61bb22aed10f79ae9
harrisonstephen-cps <stephen.harrison1@cps.gov.uk>
2025-03-13 14:02:32 +0000

    FCT2-4797: Added Delete Page Tests - Initial work
    Delivers: #FCT2-4797

---
+++ polaris-e2e/cypress/e2e/ui.delete-page.cy.ts
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+const {
+    DELETE_PAGE_TARGET_URN,
+    DELETE_PAGE_CASE_ID,
+    DELETE_PAGE_DOCUMENT_ID,
+    PRE_SEARCH_DELAY_MS,
+  } = Cypress.env();
+  
+  describe("Delete Page", { tags: ["@ci", "@ci-chunk-1"] }, () => {
+    it("can view a case", () => {
+      cy.on("uncaught:exception", () => false);
+  
+      cy.fullLogin();
+  
+     // cy.clearCaseTracker(DELETE_PAGE_TARGET_URN, DELETE_PAGE_CASE_ID);
+      cy.visit("/polaris-ui");
+  
+      cy.findByTestId("input-search-urn").type(`${DELETE_PAGE_TARGET_URN}{enter}`);
+  
+      // open case details page
+      cy.findByTestId(`link-${DELETE_PAGE_TARGET_URN}`).click();
+
+      // open our target document
+      cy.findByTestId("btn-accordion-open-close-all").click();
+      cy.findByTestId(`link-document-${DELETE_PAGE_DOCUMENT_ID}`).should(
+        "contain",
+        "Timothy"
+      ).click();
+  
+
+    });
+  });
+  
+  export {};
+  
