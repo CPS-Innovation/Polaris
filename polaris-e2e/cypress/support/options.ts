9a0fa5503a0fca3dccf045d5f43127d69ce76ab4
Adrian Perciun <Adrian.Perciun@cps.gov.uk>
2025-03-18 15:11:03 +0000

    increase timeout on slow calls and run tests sequencially

--- polaris-e2e/cypress/support/options.ts
+++ polaris-e2e/cypress/support/options.ts
@@ -4,6 +4,6 @@ export const RAPID_RETRY_WAIT_UNTIL_OPTIONS = {
   timeout: 600 * 1000,
 }
 export const SLOW_WAIT_UNTIL_OPTIONS = {
-  interval: 5 * 1000,
+  interval: 10 * 1000,
   timeout: 600 * 1000,
 }
