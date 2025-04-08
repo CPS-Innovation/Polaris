ba87fc5e98c62fbf8d675f6fa6576f95d23581d6
harrisonstephen-cps <stephen.harrison1@cps.gov.uk>
2025-04-08 14:47:47 +0100

    FCT2-4808: Document Name Search
    Delivers: #FCT2-4808

--- polaris-ui/src/app/features/cases/presentation/case-details/results/ResultsModal.tsx
+++ polaris-ui/src/app/features/cases/presentation/case-details/results/ResultsModal.tsx
@@ -53,7 +53,7 @@ export const ResultsModal: React.FC<Props> = ({
 
   const waitStatus = useMandatoryWaitPeriod(
     restProps.pipelineState.status === "complete" &&
-      restProps.searchState.results.status === "succeeded",
+    restProps.searchState.results.status === "succeeded",
     PAUSE_PERIOD_MS,
     MANDATORY_WAIT_PERIOD
   );
@@ -89,22 +89,7 @@ export const ResultsModal: React.FC<Props> = ({
       ariaLabel="Search Modal"
       ariaDescription="Find your search results"
     >
-      {waitStatus === "wait" &&
-      searchState.submittedSearchTerm !==
-        searchState.lastSubmittedSearchTerm ? (
-        <div className={classes.loadingContent}>
-          <PleaseWait
-            percentageCompleted={loadingPercentage}
-            showLoadingPercentage={showLoadingPercentage}
-          />
-
-          {showLoadingPercentage && (
-            <TrackerSummary pipelineState={restProps.pipelineState} />
-          )}
-        </div>
-      ) : (
-        <Content {...restProps} />
-      )}
+      <Content {...restProps} />
     </Modal>
   );
 };
