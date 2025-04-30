6692d32f78f5262edd74e9a5d76d1f15104ef51f
Stef Stachow <stef@stachow.co.uk>
2025-04-30 21:31:15 +0100

    #FCT2-6065 - removed prototype cps-global-nav codebase

--- cps-global-nav/src/context/get-location-config/helpers/build-sorted-full-path.ts
+++
@@ -1,5 +0,0 @@
-export const buildSortedFullPath = ({ pathname, search }: Location) => {
-  const urlParams = new URLSearchParams(search);
-  urlParams.sort();
-  return pathname + (urlParams.size ? "?" : "") + urlParams.toString();
-};
--- cps-global-nav/src/experimental/generate-guid.ts
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from "uuid";
-
-declare global {
-  var __POLARIS_INSTRUMENTATION_GUID__: string;
-}
-
-export const generateGuid = () => {
-  return window.__POLARIS_INSTRUMENTATION_GUID__ || uuidv4();
-};
