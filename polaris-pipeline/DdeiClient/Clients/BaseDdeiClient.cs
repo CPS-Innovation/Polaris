77f0be85f50ea7605abea78e4408e8e8d8e3b98b
Jordan Davis <jordan.davis@cps.gov.uk>
2025-05-07 11:14:23 +0100

    added call to mds for getpdfbyPCD (#1642)

--- polaris-pipeline/DdeiClient/Clients/BaseDdeiClient.cs
+++ polaris-pipeline/DdeiClient/Clients/BaseDdeiClient.cs
@@ -22,20 +22,20 @@ namespace DdeiClient.Clients;
 
 public abstract class BaseDdeiClient : IDdeiClient
 {
-    private readonly HttpClient _httpClient;
-    private readonly IDdeiArgFactory _caseDataServiceArgFactory;
-    private readonly ICaseDetailsMapper _caseDetailsMapper;
-    private readonly ICaseDocumentMapper<DdeiDocumentResponse> _caseDocumentMapper;
-    private readonly ICaseDocumentNoteMapper _caseDocumentNoteMapper;
-    private readonly ICaseDocumentNoteResultMapper _caseDocumentNoteResultMapper;
-    private readonly ICaseExhibitProducerMapper _caseExhibitProducerMapper;
-    private readonly ICaseWitnessMapper _caseWitnessMapper;
-    private readonly ICaseIdentifiersMapper _caseIdentifiersMapper;
-    private readonly ICmsMaterialTypeMapper _cmsMaterialTypeMapper;
-    private readonly ICaseWitnessStatementMapper _caseWitnessStatementMapper;
-    private readonly IJsonConvertWrapper _jsonConvertWrapper;
-    private readonly IDdeiClientRequestFactory _ddeiClientRequestFactory;
-    private readonly ILogger _logger;
+    protected readonly HttpClient HttpClient;
+    protected readonly IDdeiArgFactory CaseDataServiceArgFactory;
+    protected readonly ICaseDetailsMapper CaseDetailsMapper;
+    protected readonly ICaseDocumentMapper<DdeiDocumentResponse> CaseDocumentMapper;
+    protected readonly ICaseDocumentNoteMapper CaseDocumentNoteMapper;
+    protected readonly ICaseDocumentNoteResultMapper CaseDocumentNoteResultMapper;
+    protected readonly ICaseExhibitProducerMapper CaseExhibitProducerMapper;
+    protected readonly ICaseWitnessMapper CaseWitnessMapper;
+    protected readonly ICaseIdentifiersMapper CaseIdentifiersMapper;
+    protected readonly ICmsMaterialTypeMapper CmsMaterialTypeMapper;
+    protected readonly ICaseWitnessStatementMapper CaseWitnessStatementMapper;
+    protected readonly IJsonConvertWrapper JsonConvertWrapper;
+    protected readonly IDdeiClientRequestFactory DdeiClientRequestFactory;
+    protected readonly ILogger Logger;
 
     protected BaseDdeiClient(
         HttpClient httpClient,
@@ -53,31 +53,31 @@ public abstract class BaseDdeiClient : IDdeiClient
         IJsonConvertWrapper jsonConvertWrapper,
         ILogger logger)
     {
-        _httpClient = httpClient.ExceptionIfNull();
-        _caseDataServiceArgFactory = caseDataServiceArgFactory.ExceptionIfNull();
-        _caseDetailsMapper = caseDetailsMapper.ExceptionIfNull();
-        _caseDocumentMapper = caseDocumentMapper.ExceptionIfNull();
-        _caseDocumentNoteMapper = caseDocumentNoteMapper.ExceptionIfNull();
-        _caseDocumentNoteResultMapper = caseDocumentNoteResultMapper.ExceptionIfNull();
-        _caseExhibitProducerMapper = caseExhibitProducerMapper.ExceptionIfNull();
-        _caseWitnessMapper = caseWitnessMapper.ExceptionIfNull();
-        _caseIdentifiersMapper = caseIdentifiersMapper.ExceptionIfNull();
-        _cmsMaterialTypeMapper = cmsMaterialTypeMapper.ExceptionIfNull();
-        _caseWitnessStatementMapper = caseWitnessStatementMapper.ExceptionIfNull();
-        _jsonConvertWrapper = jsonConvertWrapper.ExceptionIfNull();
-        _ddeiClientRequestFactory = ddeiClientRequestFactory.ExceptionIfNull();
-        _logger = logger.ExceptionIfNull();
+        HttpClient = httpClient.ExceptionIfNull();
+        CaseDataServiceArgFactory = caseDataServiceArgFactory.ExceptionIfNull();
+        CaseDetailsMapper = caseDetailsMapper.ExceptionIfNull();
+        CaseDocumentMapper = caseDocumentMapper.ExceptionIfNull();
+        CaseDocumentNoteMapper = caseDocumentNoteMapper.ExceptionIfNull();
+        CaseDocumentNoteResultMapper = caseDocumentNoteResultMapper.ExceptionIfNull();
+        CaseExhibitProducerMapper = caseExhibitProducerMapper.ExceptionIfNull();
+        CaseWitnessMapper = caseWitnessMapper.ExceptionIfNull();
+        CaseIdentifiersMapper = caseIdentifiersMapper.ExceptionIfNull();
+        CmsMaterialTypeMapper = cmsMaterialTypeMapper.ExceptionIfNull();
+        CaseWitnessStatementMapper = caseWitnessStatementMapper.ExceptionIfNull();
+        JsonConvertWrapper = jsonConvertWrapper.ExceptionIfNull();
+        DdeiClientRequestFactory = ddeiClientRequestFactory.ExceptionIfNull();
+        Logger = logger.ExceptionIfNull();
     }
 
     public virtual async Task VerifyCmsAuthAsync(DdeiBaseArgDto arg) =>
         // Will throw in the same way as any other call if auth is not correct.
-        await CallDdei(_ddeiClientRequestFactory.CreateVerifyCmsAuthRequest(arg));
+        await CallDdei(DdeiClientRequestFactory.CreateVerifyCmsAuthRequest(arg));
 
     public virtual async Task<CaseIdentifiersDto> GetUrnFromCaseIdAsync(DdeiCaseIdOnlyArgDto arg)
     {
-        _ = _logger;
-        var result = await CallDdei<DdeiCaseIdentifiersDto>(_ddeiClientRequestFactory.CreateUrnLookupRequest(arg));
-        return _caseIdentifiersMapper.MapCaseIdentifiers(result);
+        _ = Logger;
+        var result = await CallDdei<DdeiCaseIdentifiersDto>(DdeiClientRequestFactory.CreateUrnLookupRequest(arg));
+        return CaseIdentifiersMapper.MapCaseIdentifiers(result);
     }
 
     public virtual async Task<IEnumerable<CaseDto>> ListCasesAsync(DdeiUrnArgDto arg)
@@ -85,53 +85,53 @@ public abstract class BaseDdeiClient : IDdeiClient
         var caseIdentifiers = await ListCaseIdsAsync(arg);
 
         var calls = caseIdentifiers.Select(async caseIdentifier =>
-            await GetCaseInternalAsync(_caseDataServiceArgFactory.CreateCaseArgFromUrnArg(arg, caseIdentifier.Id)));
+            await GetCaseInternalAsync(CaseDataServiceArgFactory.CreateCaseArgFromUrnArg(arg, caseIdentifier.Id)));
 
         var cases = await Task.WhenAll(calls);
-        return cases.Select(@case => _caseDetailsMapper.MapCaseDetails(@case));
+        return cases.Select(@case => CaseDetailsMapper.MapCaseDetails(@case));
     }
 
     public virtual async Task<CaseDto> GetCaseAsync(DdeiCaseIdentifiersArgDto arg)
     {
         var @case = await GetCaseInternalAsync(arg);
-        return _caseDetailsMapper.MapCaseDetails(@case);
+        return CaseDetailsMapper.MapCaseDetails(@case);
     }
 
     public virtual async Task<IEnumerable<PcdRequestCoreDto>> GetPcdRequestsAsync(DdeiCaseIdentifiersArgDto arg)
     {
-        var pcdRequests = await CallDdei<IEnumerable<DdeiPcdRequestCoreDto>>(_ddeiClientRequestFactory.CreateGetPcdRequestsRequest(arg));
-        return _caseDetailsMapper.MapCorePreChargeDecisionRequests(pcdRequests);
+        var pcdRequests = await CallDdei<IEnumerable<DdeiPcdRequestCoreDto>>(DdeiClientRequestFactory.CreateGetPcdRequestsRequest(arg));
+        return CaseDetailsMapper.MapCorePreChargeDecisionRequests(pcdRequests);
     }
 
     public virtual async Task<PcdRequestDto> GetPcdRequestAsync(DdeiPcdArgDto arg)
     {
-        var pcdRequest = await CallDdei<DdeiPcdRequestDto>(_ddeiClientRequestFactory.CreateGetPcdRequest(arg));
-        return _caseDetailsMapper.MapPreChargeDecisionRequest(pcdRequest);
+        var pcdRequest = await CallDdei<DdeiPcdRequestDto>(DdeiClientRequestFactory.CreateGetPcdRequest(arg));
+        return CaseDetailsMapper.MapPreChargeDecisionRequest(pcdRequest);
     }
 
     public virtual async Task<DefendantsAndChargesListDto> GetDefendantAndChargesAsync(DdeiCaseIdentifiersArgDto arg)
     {
-        var response = await CallDdei(_ddeiClientRequestFactory.CreateGetDefendantAndChargesRequest(arg));
+        var response = await CallDdei(DdeiClientRequestFactory.CreateGetDefendantAndChargesRequest(arg));
         var content = await response.Content.ReadAsStringAsync();
-        var defendantAndCharges = _jsonConvertWrapper.DeserializeObject<IEnumerable<DdeiCaseDefendantDto>>(content);
+        var defendantAndCharges = JsonConvertWrapper.DeserializeObject<IEnumerable<DdeiCaseDefendantDto>>(content);
         var etag = response.Headers.ETag?.Tag;
 
-        return _caseDetailsMapper.MapDefendantsAndCharges(defendantAndCharges, arg.CaseId, etag);
+        return CaseDetailsMapper.MapDefendantsAndCharges(defendantAndCharges, arg.CaseId, etag);
     }
 
     public virtual async Task<IEnumerable<CmsDocumentDto>> ListDocumentsAsync(DdeiCaseIdentifiersArgDto arg)
     {
         var ddeiResults = await CallDdei<List<DdeiDocumentResponse>>(
-            _ddeiClientRequestFactory.CreateListCaseDocumentsRequest(arg)
+            DdeiClientRequestFactory.CreateListCaseDocumentsRequest(arg)
         );
 
         return ddeiResults
-            .Select(ddeiResult => _caseDocumentMapper.Map(ddeiResult));
+            .Select(ddeiResult => CaseDocumentMapper.Map(ddeiResult));
     }
 
     public virtual async Task<FileResult> GetDocumentAsync(DdeiDocumentIdAndVersionIdArgDto arg)
     {
-        var response = await CallDdei(_ddeiClientRequestFactory.CreateGetDocumentRequest(arg));
+        var response = await CallDdei(DdeiClientRequestFactory.CreateGetDocumentRequest(arg));
         var fileName = response.Content.Headers.GetValues("Content-Disposition").ToList()[0];
 
         return new FileResult
@@ -144,7 +144,7 @@ public abstract class BaseDdeiClient : IDdeiClient
     public virtual async Task<Stream> GetDocumentFromFileStoreAsync(string path, string cmsAuthValues, Guid correlationId)
     {
         var response = await CallDdei(
-            _ddeiClientRequestFactory.CreateDocumentFromFileStoreRequest(new DdeiFileStoreArgDto
+            DdeiClientRequestFactory.CreateDocumentFromFileStoreRequest(new DdeiFileStoreArgDto
             {
                 Path = path,
                 CmsAuthValues = cmsAuthValues,
@@ -158,7 +158,7 @@ public abstract class BaseDdeiClient : IDdeiClient
     public virtual async Task<CheckoutDocumentDto> CheckoutDocumentAsync(DdeiDocumentIdAndVersionIdArgDto arg)
     {
         var response = await CallDdei(
-            _ddeiClientRequestFactory.CreateCheckoutDocumentRequest(arg),
+            DdeiClientRequestFactory.CreateCheckoutDocumentRequest(arg),
             HttpStatusCode.Conflict);
 
         return response.StatusCode == HttpStatusCode.Conflict ?
@@ -175,12 +175,12 @@ public abstract class BaseDdeiClient : IDdeiClient
 
     public virtual async Task CancelCheckoutDocumentAsync(DdeiDocumentIdAndVersionIdArgDto arg)
     {
-        await CallDdei(_ddeiClientRequestFactory.CreateCancelCheckoutDocumentRequest(arg));
+        await CallDdei(DdeiClientRequestFactory.CreateCancelCheckoutDocumentRequest(arg));
     }
 
     public virtual async Task<HttpResponseMessage> UploadPdfAsync(DdeiDocumentIdAndVersionIdArgDto arg, Stream stream)
     {
-        return await CallDdei(_ddeiClientRequestFactory.CreateUploadPdfRequest(arg, stream),
+        return await CallDdei(DdeiClientRequestFactory.CreateUploadPdfRequest(arg, stream),
         [
             HttpStatusCode.Gone,
             HttpStatusCode.RequestEntityTooLarge
@@ -189,28 +189,28 @@ public abstract class BaseDdeiClient : IDdeiClient
 
     public virtual async Task<IEnumerable<DocumentNoteDto>> GetDocumentNotesAsync(DdeiDocumentArgDto arg)
     {
-        var ddeiResults = await CallDdei<List<DdeiDocumentNoteResponse>>(_ddeiClientRequestFactory.CreateGetDocumentNotesRequest(arg));
+        var ddeiResults = await CallDdei<List<DdeiDocumentNoteResponse>>(DdeiClientRequestFactory.CreateGetDocumentNotesRequest(arg));
 
-        return ddeiResults.Select(ddeiResult => _caseDocumentNoteMapper.Map(ddeiResult)).ToArray();
+        return ddeiResults.Select(ddeiResult => CaseDocumentNoteMapper.Map(ddeiResult)).ToArray();
     }
 
     public virtual async Task<DocumentNoteResult> AddDocumentNoteAsync(DdeiAddDocumentNoteArgDto arg)
     {
-        var response = await CallDdei<DdeiDocumentNoteAddedResponse>(_ddeiClientRequestFactory.CreateAddDocumentNoteRequest(arg));
+        var response = await CallDdei<DdeiDocumentNoteAddedResponse>(DdeiClientRequestFactory.CreateAddDocumentNoteRequest(arg));
 
-        return _caseDocumentNoteResultMapper.Map(response);
+        return CaseDocumentNoteResultMapper.Map(response);
     }
 
     public virtual async Task<DocumentRenamedResultDto> RenameDocumentAsync(DdeiRenameDocumentArgDto arg)
     {
-        var response = await CallDdei<DdeiDocumentRenamedResponse>(_ddeiClientRequestFactory.CreateRenameDocumentRequest(arg));
+        var response = await CallDdei<DdeiDocumentRenamedResponse>(DdeiClientRequestFactory.CreateRenameDocumentRequest(arg));
 
         return new DocumentRenamedResultDto { Id = response.Id, OperationName = response.OperationName };
     }
 
     public virtual async Task<DocumentReclassifiedResultDto> ReclassifyDocumentAsync(DdeiReclassifyDocumentArgDto arg)
     {
-        var response = await CallDdei<DdeiDocumentReclassifiedResponse>(_ddeiClientRequestFactory.CreateReclassifyDocumentRequest(arg));
+        var response = await CallDdei<DdeiDocumentReclassifiedResponse>(DdeiClientRequestFactory.CreateReclassifyDocumentRequest(arg));
 
         return new DocumentReclassifiedResultDto
         {
@@ -225,52 +225,52 @@ public abstract class BaseDdeiClient : IDdeiClient
 
     public virtual async Task<IEnumerable<ExhibitProducerDto>> GetExhibitProducersAsync(DdeiCaseIdentifiersArgDto arg)
     {
-        var ddeiResults = await CallDdei<List<DdeiDocumentExhibitProducerResponse>>(_ddeiClientRequestFactory.CreateGetExhibitProducersRequest(arg));
+        var ddeiResults = await CallDdei<List<DdeiDocumentExhibitProducerResponse>>(DdeiClientRequestFactory.CreateGetExhibitProducersRequest(arg));
 
-        return ddeiResults.Select(ddeiResult => _caseExhibitProducerMapper.Map(ddeiResult)).ToArray();
+        return ddeiResults.Select(ddeiResult => CaseExhibitProducerMapper.Map(ddeiResult)).ToArray();
     }
 
     public virtual async Task<IEnumerable<CaseWitnessDto>> GetWitnessesAsync(DdeiCaseIdentifiersArgDto arg)
     {
-        var ddeiResults = await CallDdei<List<DdeiCaseWitnessResponse>>(_ddeiClientRequestFactory.CreateCaseWitnessesRequest(arg));
+        var ddeiResults = await CallDdei<List<DdeiCaseWitnessResponse>>(DdeiClientRequestFactory.CreateCaseWitnessesRequest(arg));
 
-        return ddeiResults.Select(ddeiResult => _caseWitnessMapper.Map(ddeiResult)).ToArray();
+        return ddeiResults.Select(ddeiResult => CaseWitnessMapper.Map(ddeiResult)).ToArray();
     }
 
     public virtual async Task<IEnumerable<MaterialTypeDto>> GetMaterialTypeListAsync(DdeiBaseArgDto arg)
     {
-        var ddeiResults = await CallDdei<List<DdeiMaterialTypeListResponse>>(_ddeiClientRequestFactory.CreateGetMaterialTypeListRequest(arg));
+        var ddeiResults = await CallDdei<List<DdeiMaterialTypeListResponse>>(DdeiClientRequestFactory.CreateGetMaterialTypeListRequest(arg));
 
-        return ddeiResults.Select(ddeiResult => _cmsMaterialTypeMapper.Map(ddeiResult)).ToArray();
+        return ddeiResults.Select(ddeiResult => CmsMaterialTypeMapper.Map(ddeiResult)).ToArray();
     }
 
     public virtual async Task<IEnumerable<WitnessStatementDto>> GetWitnessStatementsAsync(DdeiWitnessStatementsArgDto arg)
     {
-        var ddeiResults = await CallDdei<List<DdeiCaseWitnessStatementsResponse>>(_ddeiClientRequestFactory.CreateGetWitnessStatementsRequest(arg));
+        var ddeiResults = await CallDdei<List<DdeiCaseWitnessStatementsResponse>>(DdeiClientRequestFactory.CreateGetWitnessStatementsRequest(arg));
 
-        return ddeiResults.Select(ddeiResult => _caseWitnessStatementMapper.Map(ddeiResult)).ToArray();
+        return ddeiResults.Select(ddeiResult => CaseWitnessStatementMapper.Map(ddeiResult)).ToArray();
     }
 
     public virtual async Task<bool> ToggleIsUnusedDocumentAsync(DdeiToggleIsUnusedDocumentDto toggleIsUnusedDocumentDto) =>
-        (await CallDdei(_ddeiClientRequestFactory.CreateToggleIsUnusedDocumentRequest(toggleIsUnusedDocumentDto)))
+        (await CallDdei(DdeiClientRequestFactory.CreateToggleIsUnusedDocumentRequest(toggleIsUnusedDocumentDto)))
         .IsSuccessStatusCode;
 
     protected virtual async Task<IEnumerable<DdeiCaseIdentifiersDto>> ListCaseIdsAsync(DdeiUrnArgDto arg) =>
-        await CallDdei<IEnumerable<DdeiCaseIdentifiersDto>>(_ddeiClientRequestFactory.CreateListCasesRequest(arg));
+        await CallDdei<IEnumerable<DdeiCaseIdentifiersDto>>(DdeiClientRequestFactory.CreateListCasesRequest(arg));
 
     protected virtual async Task<DdeiCaseDetailsDto> GetCaseInternalAsync(DdeiCaseIdentifiersArgDto arg) =>
-        await CallDdei<DdeiCaseDetailsDto>(_ddeiClientRequestFactory.CreateGetCaseRequest(arg));
+        await CallDdei<DdeiCaseDetailsDto>(DdeiClientRequestFactory.CreateGetCaseRequest(arg));
 
     protected virtual async Task<T> CallDdei<T>(HttpRequestMessage request)
     {
         using var response = await CallDdei(request);
         var content = await response.Content.ReadAsStringAsync();
-        return _jsonConvertWrapper.DeserializeObject<T>(content);
+        return JsonConvertWrapper.DeserializeObject<T>(content);
     }
 
     protected virtual async Task<HttpResponseMessage> CallDdei(HttpRequestMessage request, params HttpStatusCode[] expectedUnhappyStatusCodes)
     {
-        var response = await _httpClient.SendAsync(request);
+        var response = await HttpClient.SendAsync(request);
         try
         {
             if (response.IsSuccessStatusCode || expectedUnhappyStatusCodes.Contains(response.StatusCode))
