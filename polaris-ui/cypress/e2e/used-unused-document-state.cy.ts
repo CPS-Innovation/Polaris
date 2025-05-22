50294fd115c054a48963fd4ad175b1f3574933f5
Rafal <rafal.drozdowski@cps.gov.uk>
2025-05-22 11:24:40 +0100

    #28389 adds a fail request unit test

--- polaris-ui/cypress/e2e/used-unused-document-state.cy.ts
+++ polaris-ui/cypress/e2e/used-unused-document-state.cy.ts
@@ -1,9 +1,12 @@
 import { getUsedUnusedDocumentState } from "../../src/mock-api/data/getDocumentsList.cypress";
-import { GET_DOCUMENTS_LIST_ROUTE } from "../../src/mock-api/routes";
+import {
+  GET_DOCUMENTS_LIST_ROUTE,
+  TOGGLE_USED_DOCUMENT_ROUTE,
+} from "../../src/mock-api/routes";
 
 describe("Checks if the used/unused functionality", () => {
   it("should change document state as used / unused on option select", () => {
-    cy.visit("/case-details/42MZ7213221/13401/?isUsed=true");
+    cy.visit("/case-details/42MZ7213221/13401/?isUnused=true");
     cy.findByTestId("btn-accordion-open-close-all").click();
     cy.findByTestId("document-housekeeping-actions-dropdown-10").click();
 
@@ -13,7 +16,7 @@ describe("Checks if the used/unused functionality", () => {
       .find("li")
       .eq(2);
 
-    lastElement.should("have.text", "Mark as used");
+    lastElement.should("have.text", "Mark as unused");
     lastElement.click();
 
     const documentList = getUsedUnusedDocumentState("10", true, 1);
@@ -23,25 +26,36 @@ describe("Checks if the used/unused functionality", () => {
       "successfully saved to CMS"
     );
 
-    cy.realPress("Escape");
-
     cy.overrideRoute(GET_DOCUMENTS_LIST_ROUTE, {
       body: documentList[0],
       timeMs: 1000,
     });
-
-    cy.visit("/case-details/42MZ7213221/13401?isUsed=true");
-    cy.findByTestId("btn-accordion-open-close-all").click();
-    cy.findByTestId("document-housekeeping-actions-dropdown-10").click();
-    cy.findByTestId("dropdown-panel")
-      .find("ul")
-      .find("li")
-      .eq(2)
-      .should("have.text", "Mark as used");
   });
 
-  // it("should show an error message on failure", () => {
-  //   throw new Error();
-  //   cy.findByText("Failed to change the document state.")
-  // });
+  it("should show an error message on failure", () => {
+    cy.visit("/case-details/42MZ7213221/13401/?isUnused=true");
+    cy.findByTestId("btn-accordion-open-close-all").click();
+    cy.findByTestId("document-housekeeping-actions-dropdown-10").click();
+
+    cy.overrideRoute(
+      TOGGLE_USED_DOCUMENT_ROUTE,
+      {
+        type: "break",
+        httpStatusCode: 500,
+        timeMs: 500,
+      },
+      "post"
+    );
+
+    const lastElement = cy
+      .findByTestId("dropdown-panel")
+      .find("ul")
+      .find("li")
+      .eq(2);
+
+    lastElement.click();
+
+    cy.findByText("Something went wrong!");
+    cy.findByText("Failed to change the document state.");
+  });
 });
