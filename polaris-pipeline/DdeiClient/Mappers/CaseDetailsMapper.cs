cb39cb20d4bf15f16f08d63aeaca4c403673a5bb
harrisonstephen-cps <stephen.harrison1@cps.gov.uk>
2025-06-10 13:44:01 +0100

    FCT2-6809: Integrate GetCase endpoint into CWA
    Delivers: #FCT2-6809

--- polaris-pipeline/DdeiClient/Mappers/CaseDetailsMapper.cs
+++ polaris-pipeline/DdeiClient/Mappers/CaseDetailsMapper.cs
@@ -64,7 +64,7 @@ namespace Ddei.Mappers
                 DefendantsAndCharges = defendants,
                 HeadlineCharge = headlineCharge,
                 PreChargeDecisionRequests = preChargeDecisionRequests,
-                // Witnesses = witnesses
+                Witnesses = witnesses
             };
         }
 
