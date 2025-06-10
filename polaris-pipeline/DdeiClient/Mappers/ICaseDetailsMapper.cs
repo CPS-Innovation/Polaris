e2b61281ff4ff79ccb8d20b467218e92679acfce
harrisonstephen-cps <stephen.harrison1@cps.gov.uk>
2025-06-10 15:56:29 +0100

    FCT2-6809: Integrate GetCase endpoint into CWA
    Delivers: #FCT2-6809

--- polaris-pipeline/DdeiClient/Mappers/ICaseDetailsMapper.cs
+++ polaris-pipeline/DdeiClient/Mappers/ICaseDetailsMapper.cs
@@ -10,11 +10,12 @@ namespace Ddei.Mappers
     public interface ICaseDetailsMapper
     {
         CaseDto MapCaseDetails(DdeiCaseDetailsDto caseDetails);
-        CaseDto MapCaseDetails((CaseSummaryDto Summary, IEnumerable<PcdRequestDto> PreChargeDecisionRequests, IEnumerable<DefendantAndChargesDto> DefendantsAndCharges, IEnumerable<BaseCaseWitnessResponse> Witnesses) caseDetails);
+        CaseDto MapCaseDetails(CaseDetailsDto caseDetails);
         DefendantsAndChargesListDto MapDefendantsAndCharges(IEnumerable<DdeiCaseDefendantDto> defendants, int caseId, string etag);
         PcdRequestDto MapPreChargeDecisionRequest(DdeiPcdRequestDto pcdr);
         IEnumerable<PcdRequestCoreDto> MapCorePreChargeDecisionRequests(IEnumerable<DdeiPcdRequestCoreDto> pcdRequests);
         IEnumerable<PcdRequestDto> MapPreChargeDecisionRequests(IEnumerable<DdeiPcdRequestDto> pcdRequests);
         CaseSummaryDto Map(MdsCaseSummaryDto ddeiResult);
+        IEnumerable<WitnessDto> MapWitnesses(IEnumerable<BaseCaseWitnessResponse> witnesses);
     }
 }
