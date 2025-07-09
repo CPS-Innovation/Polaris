38b465811fd1cc6af45297e9072cf27288267e6e
Jordan Davis (NTT) <Jordan.Davis@cps.gov.uk>
2025-07-09 15:58:18 +0000

    fix to unit tests

--- polaris-gateway.tests/Functions/ReclassifyDocumentTests.cs
+++ polaris-gateway.tests/Functions/ReclassifyDocumentTests.cs
@@ -3,14 +3,13 @@ using Common.Dto.Response.Document;
 using Common.Telemetry;
 using Ddei.Domain.CaseData.Args;
 using Ddei.Factories;
-using DdeiClient.Clients.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Newtonsoft.Json;
 using PolarisGateway.Functions;
-using PolarisGateway.Mappers;
+using PolarisGateway.Services.DdeiOrchestration;
 using System;
 using System.IO;
 using System.Text;
@@ -23,8 +22,7 @@ public class ReclassifyDocumentTests
 {
     private readonly Mock<ILogger<ReclassifyDocument>> _loggerMock;
     private readonly Mock<IDdeiArgFactory> _ddeiArgFactoryMock;
-    private readonly Mock<IDdeiAuthClient> _ddeiAuthClientMock;
-    private readonly Mock<IReclassifyDocumentRequestMapper> _reclassifyDocumentRequestMapperMock;
+    private readonly Mock<IDdeiReclassifyDocumentOrchestrationService> _orchestrationServiceMock;
     private readonly Mock<ITelemetryClient> _telemetryClientMock;
     private readonly ReclassifyDocument _reclassifyDocument;
 
@@ -32,10 +30,9 @@ public class ReclassifyDocumentTests
     {
         _loggerMock = new Mock<ILogger<ReclassifyDocument>>();
         _ddeiArgFactoryMock = new Mock<IDdeiArgFactory>();
-        _ddeiAuthClientMock = new Mock<IDdeiAuthClient>();
-        _reclassifyDocumentRequestMapperMock = new Mock<IReclassifyDocumentRequestMapper>();
+        _orchestrationServiceMock = new Mock<IDdeiReclassifyDocumentOrchestrationService>();
         _telemetryClientMock = new Mock<ITelemetryClient>();
-        _reclassifyDocument = new ReclassifyDocument(_loggerMock.Object, _ddeiAuthClientMock.Object, _ddeiArgFactoryMock.Object, _reclassifyDocumentRequestMapperMock.Object, _telemetryClientMock.Object);
+        _reclassifyDocument = new ReclassifyDocument(_loggerMock.Object, _ddeiArgFactoryMock.Object, _telemetryClientMock.Object, _orchestrationServiceMock.Object);
     }
 
     [Fact]
@@ -47,7 +44,7 @@ public class ReclassifyDocumentTests
         var caseId = 1;
         var documentId = "12345";
         var ddeiReclassifyDocumentArgDto = new DdeiReclassifyDocumentArgDto();
-        var reclassifyDocumentDto = new DdeiReclassifyDocumentArgDto()
+        var reclassifyDocumentDto = new ReclassifyDocumentDto()
         {
             DocumentTypeId = 1001,
             Other = new ReclassificationOther
@@ -59,15 +56,19 @@ public class ReclassifyDocumentTests
         req.Body = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(reclassifyDocumentDto)));
         req.ContentType = "application/json";
 
-        var reclassifyResult = new DocumentReclassifiedResultDto
+        var reclassifyResult = new DocumentReclassifiedResult
         {
-            DocumentId = 12345,
-            ReclassificationType = "OTHER",
-            OriginalDocumentTypeId = 2001,
-            DocumentTypeId = 1001
+            IsSuccess = true,
+            Result = new DocumentReclassifiedResultDto
+            {
+                DocumentId = 12345,
+                ReclassificationType = "OTHER",
+                OriginalDocumentTypeId = 2001,
+                DocumentTypeId = 1001
+            }
         };
-        _ddeiArgFactoryMock.Setup(s => s.CreateReclassifyDocumentArgDto(It.IsAny<string>(), It.IsAny<Guid>(), caseUrn, caseId, documentId, It.IsAny<ReclassifyDocumentDto>())).Returns(ddeiReclassifyDocumentArgDto);
-        _ddeiAuthClientMock.Setup(s => s.ReclassifyDocumentAsync(It.IsAny<DdeiReclassifyDocumentArgDto>())).ReturnsAsync(reclassifyResult);
+        _ddeiArgFactoryMock.Setup(s => s.CreateReclassifyDocumentArgDto(It.IsAny<string>(), It.IsAny<Guid>(), caseUrn, caseId, documentId, reclassifyDocumentDto)).Returns(ddeiReclassifyDocumentArgDto);
+        _orchestrationServiceMock.Setup(s => s.ReclassifyDocument(It.IsAny<DdeiReclassifyDocumentArgDto>())).ReturnsAsync(reclassifyResult);
 
         //act
         var result = await _reclassifyDocument.Run(req, caseUrn, caseId, documentId);
