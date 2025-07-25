6fcd4c0ed0a1096ccef04c91afb3c4c31f04a20c
Jordan Davis <jordan.davis@cps.gov.uk>
2025-07-25 15:48:25 +0000

    added tests for get cases (#1725)

---
+++ polaris-gateway.integration-tests/Functions/GetCasesTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System.Net;
+
+namespace polaris_gateway.integration_tests.Functions;
+
+public class GetCasesTests : BaseFunctionIntegrationTest
+{
+    [SetUp]
+    public void Setup()
+    {
+        BaseSetup(TestContext.Parameters);
+    }
+
+    [Test]
+    public async Task GetCases_ShouldReturnOK()
+    {
+        //arrange
+        var urn = "urn";
+
+        //act
+        var result = await PolarisGatewayApiClient.GetCases(urn, TestContext.CurrentContext.CancellationToken);
+
+        //assert
+        Assert.That(result.HttpStatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(result.ResponseObject,Is.Not.Null);
+        var responseObjectList = result.ResponseObject.ToList();
+        Assert.That(responseObjectList.Count, Is.EqualTo(1));
+        Assert.That(responseObjectList[0].UniqueReferenceNumber, Is.EqualTo(urn));
+    }
+}
