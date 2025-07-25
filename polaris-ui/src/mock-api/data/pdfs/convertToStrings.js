c77c92a16d3b8be5e81e173412fbaecd84cd4416
rafalcps <rafal.drozdowski@cps.gov.uk>
2025-07-25 14:26:01 +0100

    amends regex to string check

--- polaris-ui/src/mock-api/data/pdfs/convertToStrings.js
+++ polaris-ui/src/mock-api/data/pdfs/convertToStrings.js
@@ -10,7 +10,7 @@ fs.readdirSync(__dirname).forEach((fileName) => {
     return;
   }
 
-  const regexp = /[/\./\=/\\]/gm;
+  const regexp = /[/./-/_/\\]/gm;
   const file = fileName.replace(regexp, "");
   const fileNameNoExtension = path.parse(file).name;
 
