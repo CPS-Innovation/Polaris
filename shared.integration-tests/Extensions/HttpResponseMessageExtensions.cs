1ff1cbfc979099b58ecc3850b3ac1304a1a6b1ba
Jordan Davis (NTT) <Jordan.Davis@cps.gov.uk>
2025-07-29 12:28:02 +0100

    added GetDocumentNote tests

--- shared.integration-tests/Extensions/HttpResponseMessageExtensions.cs
+++ shared.integration-tests/Extensions/HttpResponseMessageExtensions.cs
@@ -7,6 +7,9 @@ public static class HttpResponseMessageExtensions
     public static async Task<T?> GetContentResponseAsync<T>(this HttpResponseMessage httpResponseMessage, CancellationToken cancellationToken = default)
     {
         var json = await httpResponseMessage.Content.ReadAsStringAsync(cancellationToken);
-        return JsonSerializer.Deserialize<T>(json);
+        return JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
     }
 }
