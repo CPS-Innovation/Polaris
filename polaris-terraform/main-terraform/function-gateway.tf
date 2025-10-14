f639f72666720b3663a12f3d60fe1da88965dabd
Girma Bewketu (I-10) <Girma.Bewketu@cps.gov.uk>
2025-10-14 14:35:44 +0000

    remove quotes from value

--- polaris-terraform/main-terraform/function-gateway.tf
+++ polaris-terraform/main-terraform/function-gateway.tf
@@ -63,7 +63,7 @@ resource "azurerm_linux_function_app" "fa_polaris" {
     "WEBSITE_WARMUP_PATH"                             = "/api/status"
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE"             = "true"
     "MasterDataServiceClient__BaseAddress"            = "https://fa-${local.mds_resource_name}.azurewebsites.net/api/"
-    "MasterDataServiceClient__FunctionKey"            = "data.azurerm_key_vault_secret.kvs_fa_mds_host_keys.value"
+    "MasterDataServiceClient__FunctionKey"            = data.azurerm_key_vault_secret.kvs_fa_mds_host_keys.value
   }
 
   sticky_settings {
