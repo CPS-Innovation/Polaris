3140af9d9ca42ceaa8a9f6da2005534f4bba43a1
Jordan Davis <jordan.davis@cps.gov.uk>
2025-10-28 10:09:36 +0000

    updated aspose packages and moved PDF conversion to aspose words (#1774)

--- polaris-pipeline/pdf-generator/Services/PdfService/PdfOrchestratorService.cs
+++ polaris-pipeline/pdf-generator/Services/PdfService/PdfOrchestratorService.cs
@@ -71,6 +71,7 @@ namespace pdf_generator.Services.PdfService
                     case FileType.HTM:
                     case FileType.MHT:
                     case FileType.MHTML:
+                    case FileType.PDF:
                         converterType = PdfConverterType.AsposeWords;
                         conversionResult = _wordsPdfService.ReadToPdfStream(inputStream, documentId, correlationId);
                         break;
@@ -118,11 +119,6 @@ namespace pdf_generator.Services.PdfService
                     case FileType.MSG:
                         converterType = PdfConverterType.AsposeEmail;
                         conversionResult = _emailPdfService.ReadToPdfStream(inputStream, documentId, correlationId);
-                        break;
-
-                    case FileType.PDF:
-                        converterType = PdfConverterType.AsposePdf;
-                        conversionResult = await _pdfRendererService.ReadToPdfStreamAsync(inputStream, documentId, correlationId);
                         break;
 
                     case FileType.XPS:
