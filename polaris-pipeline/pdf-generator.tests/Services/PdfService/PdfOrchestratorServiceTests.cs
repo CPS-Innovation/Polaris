733f048c55f82995e0aaf6768801b197fb3057b2
Jordan Davis (NTT) <Jordan.Davis@cps.gov.uk>
2025-10-30 09:24:28 +0000

    Revert "updated aspose packages and moved PDF conversion to aspose words (#1774)"
    This reverts commit 3140af9d9ca42ceaa8a9f6da2005534f4bba43a1.

--- polaris-pipeline/pdf-generator.tests/Services/PdfService/PdfOrchestratorServiceTests.cs
+++ polaris-pipeline/pdf-generator.tests/Services/PdfService/PdfOrchestratorServiceTests.cs
@@ -211,7 +211,7 @@ namespace pdf_generator.tests.Services.PdfService
         }
 
         [Fact]
-        public void ReadToPdfStream_CallsEmailServiceWhenFileTypeIsMsg()
+        public void ReadToPdfStreamAsync_CallsEmailServiceWhenFileTypeIsMsg()
         {
             _pdfOrchestratorService.ReadToPdfStreamAsync(_inputStream, FileType.MSG, _documentId, _correlationId);
 
@@ -223,7 +223,7 @@ namespace pdf_generator.tests.Services.PdfService
         {
             _pdfOrchestratorService.ReadToPdfStreamAsync(_inputStream, FileType.PDF, _documentId, _correlationId);
 
-            _mockWordsPdfService.Verify(service => service.ReadToPdfStream(It.IsAny<MemoryStream>(), It.IsAny<string>(), It.IsAny<Guid>()));
+            _mockPdfRendererService.Verify(service => service.ReadToPdfStreamAsync(It.IsAny<MemoryStream>(), It.IsAny<string>(), It.IsAny<Guid>()));
         }
 
         [Fact]
