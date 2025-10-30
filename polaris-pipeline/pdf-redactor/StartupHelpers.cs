2819c8d84aa8e3ae9e8aaba721f68ee121461b16
Jordan Davis <jordan.davis@cps.gov.uk>
2025-10-30 15:13:21 +0000

    fix to license in pdf generator (#1776)
    * fix to license in pdf generator
    
    * fix to formatting of startup helpers

--- polaris-pipeline/pdf-redactor/StartupHelpers.cs
+++ polaris-pipeline/pdf-redactor/StartupHelpers.cs
@@ -1,24 +1,21 @@
-using Aspose.Pdf.Operators;
 using Microsoft.Extensions.Logging;
-using pdf_redactor.Domain.Exceptions;
 
-namespace pdf_redactor
+namespace pdf_redactor;
+
+internal static class StartupHelpers
 {
-  internal static class StartupHelpers
-  {
 
     internal static void SetAsposeLicence(ILogger<Program> logger)
     {
-      try
-      {
-        const string licenceFileName = "Aspose.Total.NET.lic";
-        new Aspose.Pdf.License().SetLicense(licenceFileName);
-      }
-      catch (Exception ex)
-      {
-                //throw new AsposeLicenseException(exception.Message);
-                logger.LogError($"Aspose license not found or invalid. Running in evaluation mode. Error: {ex.Message}");
-            }
+        try
+        {
+            const string licenceFileName = "Aspose.Total.NET.lic";
+            new Aspose.Pdf.License().SetLicense(licenceFileName);
         }
-  }
+        catch (Exception ex)
+        {
+            //throw new AsposeLicenseException(exception.Message);
+            logger.LogError($"Aspose license not found or invalid. Running in evaluation mode. Error: {ex.Message}");
+        }
+    }
 }
