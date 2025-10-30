791e86e0f5b56b6721dc917e54ba5235d13832e7
Jordan Davis (NTT) <Jordan.Davis@cps.gov.uk>
2025-10-30 14:29:42 +0000

    fix to formatting of startup helpers

--- polaris-pipeline/pdf-thumbnail-generator/StartupHelpers.cs
+++ polaris-pipeline/pdf-thumbnail-generator/StartupHelpers.cs
@@ -1,23 +1,20 @@
-using Aspose.Pdf.Operators;
 using Microsoft.Extensions.Logging;
-using pdf_thumbnail_generator.Domain.Exceptions;
 
-namespace pdf_thumbnail_generator
-{ 
-    internal static class StartupHelpers 
+namespace pdf_thumbnail_generator;
+
+internal static class StartupHelpers 
+{
+    internal static void SetAsposeLicence(ILogger<Program> logger) 
     {
-        internal static void SetAsposeLicence(ILogger<Program> logger) 
+        try
         {
-            try
-            {
-                const string licenceFileName = "Aspose.Total.NET.lic";
-                new Aspose.Pdf.License().SetLicense(licenceFileName);
-            }
-            catch (Exception ex)
-            {
-                // throw new AsposeLicenseException(exception.Message);
-                logger.LogError($"Aspose license not found or invalid. Running in evaluation mode. Error: {ex.Message}");
-            }
+            const string licenceFileName = "Aspose.Total.NET.lic";
+            new Aspose.Pdf.License().SetLicense(licenceFileName);
+        }
+        catch (Exception ex)
+        {
+            // throw new AsposeLicenseException(exception.Message);
+            logger.LogError($"Aspose license not found or invalid. Running in evaluation mode. Error: {ex.Message}");
         }
     }
 }
