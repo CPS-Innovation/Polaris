191b1a429fdb7d331b1ae66dbbaf06fb1f721173
Girma Bewketu (I-10) <Girma.Bewketu@cps.gov.uk>
2025-12-23 10:45:33 +0000

    Fixing env  name

--- polaris-terraform/main-terraform/function-gateway.tf
+++ polaris-terraform/main-terraform/function-gateway.tf
@@ -64,7 +64,7 @@ resource "azurerm_linux_function_app" "fa_polaris" {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE"             = "true"
     "MasterDataServiceClient__BaseAddress"            = "https://fa-${local.mds_resource_name}.azurewebsites.net/api/"
     "MasterDataServiceClient__FunctionKey"            = data.azurerm_key_vault_secret.kvs_fa_mds_host_keys.value
-    "UMAClient__BaseAddress"                          = "https://fa-hsk-class-svc-${env_name}.azurewebsites.net/"
+    "UMAClient__BaseAddress"                          = "https://fa-hsk-class-svc-${local.env_name}.azurewebsites.net/"
     "UMAClient__FunctionKey"                          = "data.azurerm_key_vault_secret.kvs_fa_uma_host_keys.value"
   }
 
