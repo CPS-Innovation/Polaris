f401c40c89400492f869b2bf96d477ac5b7952aa
girmacps <girma.bewketu@cps.gov.uk>
2026-01-27 15:09:41 +0000

    HKUI functions Integration tests (#1836)
    * Adding endpoint components
    
    * Adding Asbose licence details for PG
    
    * refresh mdsclient
    
    * more asbose licence tasks to the correct place
    
    * fix broken unit tests
    
    * Set up project - get auth working with cwa and mds client
    
    * refactor project name
    
    * complete hkui int tests
    
    * allign namespaces

---
+++ hkui-functions-integration-tests/GetCaseHistoryEventTests.cs
@@ -0,0 +1,69 @@
+// <copyright file="GetCaseHistoryEventTests.cs" company="TheCrownProsecutionService">
+// Copyright (c) The Crown Prosecution Service. All rights reserved.
+// </copyright>
+
+namespace HkuiFunctionsIntegrationTests;
+
+using System.Globalization;
+using Cps.Fct.Hk.Ui.Interfaces;
+using Cps.MasterDataService.Infrastructure.ApiClient;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PolarisGateway.Functions.HouseKeeping;
+using Xunit.Abstractions;
+
+/// <summary>
+/// Integration tests for GetCaseHistoryEvent function.
+/// </summary>
+public class GetCaseHistoryEventTests : TestBase
+{
+    private readonly GetCaseHistoryEvent sut;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetCaseHistoryEventTests"/> class.
+    /// </summary>
+    /// <param name="testOutputHelper">The test output helper used to log test output.</param>
+    public GetCaseHistoryEventTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+        var loggerMock = new Mock<ILogger<GetCaseHistoryEvent>>();
+
+        this.sut = new GetCaseHistoryEvent(
+            logger: loggerMock.Object,
+            this.communicationService!);
+    }
+
+    /// <summary>
+    /// GetCaseHistoryEvent - success run as expected,.
+    /// </summary>
+    /// <returns>A task representing the asynchronous test operation.</returns>
+    [Fact]
+    public async Task GetCaseHistoryEvent_Should_ReturnAsExpected()
+    {
+        // Arrange
+        this.baseCaseId = 2158751;
+        this.ValidateTestDependencies();
+
+        (int caseId, int _, string username, string password) = this.GetTestConfiguration();
+        AuthenticationContext authContext = await this.GetAuthenticationContextAsync(username, password);
+        this.baseRequest = this.CreateHttpRequestWithCookie(this.baseCaseId, authContext);
+
+        // Act
+        IActionResult response = await this.sut.Run(this.baseRequest, this.baseCaseId);
+
+        // Assert
+        OkObjectResult okResult = Assert.IsType<OkObjectResult>(response);
+        List<HistoryEvent> result = Assert.IsType<List<HistoryEvent>>(okResult.Value);
+
+        // Assert
+        result.Count.Should().BeGreaterThanOrEqualTo(17);
+        var preChargeDecision = result.Where(x => x.Type == HistoryEventType.PreChargeDecision).OrderBy(x => x.Id);
+        preChargeDecision.Count().Should().BeGreaterThanOrEqualTo(2);
+        preChargeDecision.First().Name.Should().Be("Pre-charge Decision");
+        preChargeDecision.First().Id.Should().Be(4365090);
+        preChargeDecision.First().AuthorOrVenue.Should().Be("Tim Bates");
+    }
+}
---
+++ hkui-functions-integration-tests/GetPreChargeDecisionByHistoryIdTests.cs
@@ -0,0 +1,104 @@
+// <copyright file="GetPreChargeDecisionByHistoryIdTests.cs" company="TheCrownProsecutionService">
+// Copyright (c) The Crown Prosecution Service. All rights reserved.
+// </copyright>
+
+namespace HkuiFunctionsIntegrationTests;
+
+using System.Globalization;
+using Cps.Fct.Hk.Ui.Interfaces;
+using Cps.MasterDataService.Infrastructure.ApiClient;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PolarisGateway.Functions.HouseKeeping;
+using Xunit.Abstractions;
+
+/// <summary>
+/// Integration tests for GetPreChargeDecisionByHistoryId function.
+/// </summary>
+public class GetPreChargeDecisionByHistoryIdTests : TestBase
+{
+    private readonly GetPreChargeDecisionByHistoryId sut;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetPreChargeDecisionByHistoryIdTests"/> class.
+    /// </summary>
+    /// <param name="testOutputHelper">The test output helper used to log test output.</param>
+    public GetPreChargeDecisionByHistoryIdTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+        var loggerMock = new Mock<ILogger<GetPreChargeDecisionByHistoryId>>();
+
+        this.sut = new GetPreChargeDecisionByHistoryId(
+            logger: loggerMock.Object,
+            this.communicationService);
+    }
+
+    /// <summary>
+    /// GetPreChargeDecisionByHistoryId - success run as expected,.
+    /// </summary>
+    /// <returns>A task representing the asynchronous test operation.</returns>
+    [Fact]
+    public async Task GetPreChargeDecisionByHistoryId_Should_ReturnAsExpected()
+    {
+        // Arrange
+        this.baseCaseId = 2158751;
+        (int caseId, int _, string username, string password) = this.GetTestConfiguration();
+        AuthenticationContext authContext = await this.GetAuthenticationContextAsync(username, password);
+        this.baseRequest = this.CreateHttpRequestWithCookie(this.baseCaseId, authContext);
+
+        // Act
+        IActionResult response = await this.sut.Run(this.baseRequest, this.baseCaseId, 4365090);
+
+        // Assert
+        OkObjectResult okResult = Assert.IsType<OkObjectResult>(response);
+        PreChargeDecisionOutcome result = Assert.IsType<PreChargeDecisionOutcome>(okResult.Value);
+
+        // Assert
+        Assert.Equal(result.CaseId, this.baseCaseId);
+        result.CaseId.Should().Be(2158751);
+        result.Id.Should().Be(4365090);
+        result.IsCompleted.Should().Be(true);
+        result.EventDate.Should().Be("26/02/2025");
+        result.Author.Should().Be("Tim Bates");
+        result.DecisionRequestedDate.Should().Be("20/02/2025");
+        result.Method.Should().Be("Area");
+        result.InvestigationStage.Should().Be("Post Interview");
+        result.DecisionMadeBy.Should().Be("Tim Bates");
+        result.ActionPlan.Should().Be(true);
+        result.PoliceCovidUrgency.Should().Be("Other");
+        result.PcdHistoryActionPlan.Should().HaveCountGreaterThan(0);
+        result.PcdHistoryActionPlan.FirstOrDefault().Should().NotBeNull();
+        result.PcdHistoryActionPlan.First().ActionType.Should().Be("Action Plan");
+        result.PcdHistoryActionPlan.First().EntryDate.Should().Be("26/02/2025");
+        result.PcdHistoryActionPlan.First().Status.Should().Be("Sent on PCD Response");
+        result.PcdHistoryActionPlan.First().Suspect.Should().Be("All");
+        result.PcdHistoryActionPlan.First().ActionPoint.Should().Be("Requested:\n- Key Witness Details;\n(Test action plan during EA to make sure a dummy is not raised.)");
+
+        result.Urn.Should().Be("16XL5110225");
+        result.DefendantDecisions.Count().Should().Be(2);
+    }
+
+    /// <summary>
+    /// GetPreChargeDecisionByHistoryId - Return not found event result.,.
+    /// </summary>
+    /// <returns>A task representing the asynchronous test operation.</returns>
+    [Fact]
+    public async Task GetPreChargeDecisionByHistoryId_WheNotFound_Should_Return404()
+    {
+        // Arrange
+        this.baseCaseId = 2158751;
+        (int caseId, int _, string username, string password) = this.GetTestConfiguration();
+        AuthenticationContext authContext = await this.GetAuthenticationContextAsync(username, password);
+        this.baseRequest = this.CreateHttpRequestWithCookie(this.baseCaseId, authContext);
+
+        // Act
+        IActionResult response = await this.sut.Run(this.baseRequest, this.baseCaseId, 4365081);
+
+        // Assert
+        var result = Assert.IsType<StatusCodeResult>(response);
+        result.StatusCode.Should().BeGreaterThanOrEqualTo(404);
+    }
+}
