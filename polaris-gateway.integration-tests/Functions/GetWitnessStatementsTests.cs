8799352fec562ab8d29468431e558b3237119d49
nagcps <nag.bandla@cps.gov.uk>
2026-02-11 10:35:50 +0000

    Feature/fct2 8083 integration tests (#1850)
    * added starting tests, need more testing
    
    * added local settings
    
    * refactor
    
    * refactored tests
    
    * refactored existings tests
    
    * revert unnecessary changes
    
    * fixed more tests
    
    ---------
    
    Co-authored-by: Jordan Davis (NTT) <Jordan.Davis@cps.gov.uk>

--- polaris-gateway.integration-tests/Functions/GetWitnessStatementsTests.cs
+++ polaris-gateway.integration-tests/Functions/GetWitnessStatementsTests.cs
@@ -25,7 +25,7 @@ public class GetWitnessStatementsTests : BaseFunctionIntegrationTest
         //assert
         Assert.That(result.HttpStatusCode, Is.EqualTo(HttpStatusCode.NotFound));
     }
-    
+
     [Test]
     public async Task GetWitnessStatements_WitnessIdIs0_ShouldReturnBadRequestResult()
     {
@@ -40,20 +40,20 @@ public class GetWitnessStatementsTests : BaseFunctionIntegrationTest
         //assert
         Assert.That(result.HttpStatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
     }
-    
+
     [Test]
     public async Task GetWitnessStatements_ShouldReturnWitnessStatements()
     {
         //arrange
-        var urn = "urn";
-        var caseId = 1;
-        var witnessId = 2;
+        var urn = "16XL8836126";
+        var caseId = 2179140;
+        var witnessId = 2810230;
 
         //act
         var result = await PolarisGatewayApiClient.GetWitnessStatementsAsync(urn, caseId, witnessId, TestContext.CurrentContext.CancellationToken);
 
         //assert
         Assert.That(result.HttpStatusCode, Is.EqualTo(HttpStatusCode.OK));
-        Assert.That(result.ResponseObject.Count(), Is.EqualTo(1));
+        Assert.That(result.ResponseObject.Count(), Is.AtLeast(2));
     }
 }
---
+++ polaris-gateway.integration-tests/Functions/ReclassifyDocumentTests.cs
@@ -0,0 +1,84 @@
+﻿using Common.Dto.Request;
+using NUnit.Framework;
+using System.Net;
+
+namespace polaris_gateway.integration_tests.Functions;
+
+public class ReclassifyDocumentTests : BaseFunctionIntegrationTest
+{
+    [SetUp]
+    public void SetUp()
+    {
+        BaseSetup(TestContext.Parameters);
+    }
+
+    [Test]
+    public async Task ReclassifyDocument_CaseIdIs0_ShouldReturnNotFound()
+    {
+        //arrange
+        var request = new ReclassifyDocumentDto();
+        var urn = 1;
+        var caseId = 0;
+        var documentId = "CMS-12345";
+
+        //act
+        var result = await PolarisGatewayApiClient.ReclassifyDocumentAsync(urn, caseId, documentId, request, TestContext.CurrentContext.CancellationToken);
+
+        //assert
+        Assert.That(result.HttpStatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+    
+    [Test]
+    public async Task ReclassifyDocument_RequestIsInvalid_ShouldReturnBadRequest()
+    {
+        //arrange
+        var request = new ReclassifyDocumentDto();
+        var urn = 1;
+        var caseId = 2;
+        var documentId = "CMS-12345";
+
+        //act
+        var result = await PolarisGatewayApiClient.ReclassifyDocumentAsync(urn, caseId, documentId, request, TestContext.CurrentContext.CancellationToken);
+
+        //assert
+        Assert.That(result.HttpStatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+    }
+
+    [Test]
+    public async Task ReclassifyDocument_DocumentIdNotFoundInListDocument_ShouldReturnInternalServiceError()
+    {
+        //arrange
+        var request = new ReclassifyDocumentDto()
+        {
+            DocumentTypeId = 1,
+        };
+        var urn = 1;
+        var caseId = 2;
+        var documentId = "CMS-12345";
+
+        //act
+        var result = await PolarisGatewayApiClient.ReclassifyDocumentAsync(urn, caseId, documentId, request, TestContext.CurrentContext.CancellationToken);
+
+        //assert
+        Assert.That(result.HttpStatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
+    }
+    
+    [Test]
+    public async Task ReclassifyDocument_DocumentTypeIdNotFound_ShouldReturnInternalServiceError()
+    {
+        //arrange
+        var request = new ReclassifyDocumentDto()
+        {
+            DocumentTypeId = -100,
+        };
+        var urn = 1;
+        var caseId = 2;
+        var documentId = "CMS-8834853";
+
+        //act
+        var result = await PolarisGatewayApiClient.ReclassifyDocumentAsync(urn, caseId, documentId, request, TestContext.CurrentContext.CancellationToken);
+
+        //assert
+        Assert.That(result.HttpStatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
+    }
+}
