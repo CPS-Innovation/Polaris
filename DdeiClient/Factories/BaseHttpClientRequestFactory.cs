b058667aad1cdccf7fe282cce639568315e553f9
Tahmeed Ahmed (NTT) <Tahmeed.Ahmed@cps.gov.uk>
2026-02-12 15:47:50 +0000

    refactor to CmsBaseHttpClientRequestFactory

--- DdeiClient/Factories/BaseHttpClientRequestFactory.cs
+++ DdeiClient/Factories/BaseHttpClientRequestFactory.cs
@@ -4,7 +4,7 @@ using System.Net;
 
 namespace DdeiClient.Factories;
 
-public abstract class BaseHttpClientRequestFactory
+public abstract class CmsBaseHttpClientRequestFactory
 {
     protected const string CorrelationId = "Correlation-Id";
 
