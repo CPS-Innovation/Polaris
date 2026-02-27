0dd21ba5edf19b345f244ba58712d2c628af6363
Gopi Sivanappan Vasanthi (NTT) <GopiSivanappan.Vasanthi@cps.gov.uk>
2026-02-27 15:15:38 +0000

    FCT2-5567 - Swagger Attributes added for Get Document List in Case Work App

--- polaris-gateway/Functions/GetDocumentList.cs
+++ polaris-gateway/Functions/GetDocumentList.cs
@@ -1,13 +1,20 @@
 using Common.Configuration;
+using Common.Dto.Response.Documents;
+using Common.Telemetry;
+using Ddei.Factories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Ddei.Factories;
-using PolarisGateway.Services.DdeiOrchestration;
 using Microsoft.Azure.Functions.Worker;
-using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using PolarisGateway.Services.DdeiOrchestration;
 using System;
-using Common.Telemetry;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace PolarisGateway.Functions;
 
@@ -33,6 +40,12 @@ public class GetDocumentList : BaseFunction
 
     [Function(nameof(GetDocumentList))]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [OpenApiOperation(operationId: nameof(GetDocumentList), tags: ["Documents"], Summary = "Get Document List", Description = "Getting the list of documents")]
+    [OpenApiSecurity("Correlation-Id", SecuritySchemeType.ApiKey, Name = "Correlation-Id", In = OpenApiSecurityLocationType.Header, Description = "Must be a valid GUID")]
+    [OpenApiParameter(name: "caseUrn", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "Case URN", Description = "The URN identifier of the case")]
+    [OpenApiParameter("caseId", In = ParameterLocation.Path, Type = typeof(int), Description = "The Id of the case.", Required = true)]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(IEnumerable<DocumentDto>), Summary = "Document List", Description = "Returns list of documennts")]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NoContent, Summary = "Invalid request", Description = "Missing or invalid parameters")]
     public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = RestApi.Documents)] HttpRequest req, string caseUrn, int caseId)
     {
         var correlationId = EstablishCorrelation(req);
