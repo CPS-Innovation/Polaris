32332b4d1368bc1ecf47398f241725080dfcb0f1
Gopi Sivanappan Vasanthi (NTT) <GopiSivanappan.Vasanthi@cps.gov.uk>
2026-03-02 11:23:49 +0000

    FCT2-5567 - Open api attributes added to Case Search Index Count end point

--- polaris-gateway/Functions/PolarisPipelineCaseSearchIndexCount.cs
+++ polaris-gateway/Functions/PolarisPipelineCaseSearchIndexCount.cs
@@ -3,9 +3,14 @@ using Common.Telemetry;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using PolarisGateway.Clients.Coordinator;
 using PolarisGateway.Extensions;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PolarisGateway.Functions;
@@ -30,6 +35,14 @@ public class PolarisPipelineCaseSearchIndexCount : BaseFunction
     [Function(nameof(PolarisPipelineCaseSearchIndexCount))]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [OpenApiOperation(operationId: nameof(PolarisPipelineCaseSearchIndexCount), tags: ["Cases"], Summary = "Polaris Pipeline Case", Description = "Returns case information using caseURN and caseId")]
+    [OpenApiSecurity("Correlation-Id", SecuritySchemeType.ApiKey, Name = "Correlation-Id", In = OpenApiSecurityLocationType.Header, Description = "Must be a valid GUID")]
+    [OpenApiParameter(name: "caseUrn", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "Case URN", Description = "The URN identifier of the case")]
+    [OpenApiParameter("caseId", In = ParameterLocation.Path, Type = typeof(int), Description = "The Id of the case to add a new action plan.", Required = true)]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(HttpResponseMessage), Summary = "Case found", Description = "Returns case details")]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NoContent, Summary = "Invalid request", Description = "Missing or invalid parameters")]
+
+
     public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = RestApi.CaseSearchCount)] HttpRequest req, string caseUrn, int caseId)
     {
         var correlationId = EstablishCorrelation(req);
