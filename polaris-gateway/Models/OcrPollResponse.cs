015af6aa0327576b504a9ce7f1068ffd95b877c7
Gopi Sivanappan Vasanthi (NTT) <GopiSivanappan.Vasanthi@cps.gov.uk>
2026-03-02 10:41:23 +0000

    FCT2-5567 - Open api attributes added to GetWitnesses end point

---
+++ polaris-gateway/Models/OcrPollResponse.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarisGateway.Models
+{
+    public class OcrPollResponse
+    {
+        [OpenApiProperty(Description = "URL to poll for OCR completion result")]
+        public string NextUrl { get; set; } = default!;
+    }
+}
