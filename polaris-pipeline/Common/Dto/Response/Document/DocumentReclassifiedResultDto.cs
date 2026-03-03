6e3607e976bcef699af34cd3d3c25fef35c1ada9
Richard Fowles <richard.fowles@cps.gov.uk>
2026-03-03 16:30:44 +0000

    Adding parent and child IDs

--- polaris-pipeline/Common/Dto/Response/Document/DocumentReclassifiedResultDto.cs
+++ polaris-pipeline/Common/Dto/Response/Document/DocumentReclassifiedResultDto.cs
@@ -3,6 +3,8 @@ namespace Common.Dto.Response.Document
     public class DocumentReclassifiedResultDto
     {
         public long DocumentId { get; set; }
+        // Do we need this?
+        public long ParentId => DocumentId;
         public int DocumentTypeId { get; set; }
         public int OriginalDocumentTypeId { get; set; }
         public string ReclassificationType { get; set; }
