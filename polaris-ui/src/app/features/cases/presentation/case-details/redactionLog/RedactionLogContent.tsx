e2e9444612e289901f5a5e2c72e364b9f41b34a9
Tahmeed Ahmed (NTT) <Tahmeed.Ahmed@cps.gov.uk>
2026-03-10 18:59:22 +0000

    implemented accessible mandatory fields message

--- polaris-ui/src/app/features/cases/presentation/case-details/redactionLog/RedactionLogContent.tsx
+++ polaris-ui/src/app/features/cases/presentation/case-details/redactionLog/RedactionLogContent.tsx
@@ -630,6 +630,11 @@ export const RedactionLogContent: React.FC<RedactionLogContentProps> = ({
         }}
       >
         <div className={classes.selectInputWrapper}>
+          <p id="mandatoryInfo" className={classes.fullWidthMessage}>
+            Please select all mandatory fields showing below for either CPS Area
+            or Central Casework:
+          </p>
+
           <section className={classes.selectSection}>
             <Controller
               name="cpsArea"
@@ -655,6 +660,7 @@ export const RedactionLogContent: React.FC<RedactionLogContentProps> = ({
                     }}
                     id="select-cps-area"
                     data-testid="select-cps-area"
+                    aria-describedby="mandatoryInfo"
                     formGroup={{
                       className: classes.select,
                     }}
@@ -912,7 +918,7 @@ export const RedactionLogContent: React.FC<RedactionLogContentProps> = ({
             className={classes.saveBtn}
             data-testid="btn-save-redaction-log"
           >
-            Save and Close
+            Save and ClosesZ
           </Button>
 
           {handleCloseRedactionLog && (
