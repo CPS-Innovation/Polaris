935add4cb8320c0c6e76ce4d5a0100061ee83e26
Gopi Sivanappan Vasanthi (NTT) <GopiSivanappan.Vasanthi@cps.gov.uk>
2026-03-17 14:48:25 +0000

    Initial Commit - GetCaseSummaryAsync replaced from MasterDataService instead of MDS Client.

--- polaris-gateway/Services/MdsOrchestration/MdsCaseOrchestrationService.cs
+++ polaris-gateway/Services/MdsOrchestration/MdsCaseOrchestrationService.cs
@@ -1,40 +1,38 @@
+using Common.Dto.Request;
+using Common.Dto.Request.HouseKeeping;
 using Common.Dto.Response;
 using Common.Dto.Response.Case;
 using Common.Extensions;
+using Cps.MasterDataService.Infrastructure.ApiClient;
 using Ddei.Domain.CaseData.Args.Core;
 using Ddei.Factories;
 using Ddei.Mappers;
 using DdeiClient.Clients.Interfaces;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace PolarisGateway.Services.MdsOrchestration;
 
-public class MdsCaseOrchestrationService : IMdsCaseOrchestrationService
+public class MdsCaseOrchestrationService(IMdsClient mdsClient,
+                            IMasterDataServiceClient masterDataServiceClient,
+                            IMdsArgFactory mdsArgFactory,
+                            ICaseDetailsMapper caseDetailsMapper) : IMdsCaseOrchestrationService
 {
-    private readonly IMdsClient _mdsClient;
-    private readonly IMdsArgFactory _mdsArgFactory;
-    private readonly ICaseDetailsMapper _caseDetailsMapper;
+    private readonly IMdsClient _mdsClient = mdsClient;
+    private readonly IMasterDataServiceClient _masterDataServiceClient = masterDataServiceClient;
+    private readonly IMdsArgFactory _mdsArgFactory = mdsArgFactory;
+    private readonly ICaseDetailsMapper _caseDetailsMapper = caseDetailsMapper;
 
-    public MdsCaseOrchestrationService(
-            IMdsClient mdsClient,
-            IMdsArgFactory mdsArgFactory,
-            ICaseDetailsMapper caseDetailsMapper
-        )
-    {
-        _mdsClient = mdsClient.ExceptionIfNull();
-        _mdsArgFactory = mdsArgFactory.ExceptionIfNull();
-        _caseDetailsMapper = caseDetailsMapper.ExceptionIfNull();
-    }
-
-    public async Task<CaseDto> GetCase(MdsCaseIdentifiersArgDto arg)
+    public async Task<Common.Dto.Response.Case.CaseDto> GetCase(MdsCaseIdentifiersArgDto arg)
     {
         var @case = await GetCaseDetails(arg);
         return _caseDetailsMapper.MapCaseDetails(@case);
     }
 
-    public async Task<IEnumerable<CaseDto>> GetCases(MdsUrnArgDto arg)
+    public async Task<IEnumerable<Common.Dto.Response.Case.CaseDto>> GetCases(MdsUrnArgDto arg)
     {
         var caseIdentifiers = await _mdsClient.ListCaseIdsAsync(arg);
 
@@ -47,14 +45,34 @@ public class MdsCaseOrchestrationService : IMdsCaseOrchestrationService
 
     private async Task<CaseDetailsDto> GetCaseDetails(MdsCaseIdentifiersArgDto arg)
     {
+        var request = new GetCaseSummaryRequest(arg.CaseId, arg.CorrelationId);
+
+        var cmsAuthValuesSer = JsonConvert.DeserializeObject<AuthenticationResponse>(arg.CmsAuthValues);
+
+        var cmsAuthValues = new CmsAuthValues(
+            cmsAuthValuesSer.Cookies,
+            cmsAuthValuesSer.Token,   
+            arg.CorrelationId);
+
+        var getCaseSummaryTaskNew = _masterDataServiceClient.GetCaseSummaryAsync(request, cmsAuthValues);
+
         var getCaseSummaryTask = _mdsClient.GetCaseSummaryAsync(_mdsArgFactory.CreateCaseIdArg(arg.CmsAuthValues, arg.CorrelationId, arg.CaseId, arg.Urn));
         var getDefendantsAndChargesTask = _mdsClient.GetDefendantAndChargesAsync(arg);
         var witnessesTask = _mdsClient.GetWitnessesAsync(arg);
         var getPcdRequestTask = _mdsClient.GetPcdRequestsAsync(arg);
 
-        await Task.WhenAll(getCaseSummaryTask, getDefendantsAndChargesTask, witnessesTask, getPcdRequestTask);
+        await Task.WhenAll(getCaseSummaryTask, getDefendantsAndChargesTask, witnessesTask, getPcdRequestTask, getCaseSummaryTaskNew);
 
-        var summary = getCaseSummaryTask.Result;
+        var summarynew = getCaseSummaryTaskNew.Result;
+        var summary = new CaseSummaryDto()
+        {
+           Urn = summarynew.Urn,
+           LeadDefendantFirstNames = summarynew.LeadDefendantFirstNames,
+           LeadDefendantSurname = summarynew.LeadDefendantSurname,
+           NumberOfDefendants = summarynew.NumberOfDefendants,
+           OwningUnit = summarynew.UnitName,
+           Id = summarynew.CaseId,
+        }; 
         var defendantsAndCharges = getDefendantsAndChargesTask.Result.DefendantsAndCharges;
         var witnesses = MapWitnesses(witnessesTask.Result);
         var preChargeDecisionRequests = getPcdRequestTask.Result;
