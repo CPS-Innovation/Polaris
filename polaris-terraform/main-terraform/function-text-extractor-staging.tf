4d654c6b145716bd413c1fb451636c787647350b
Gopi Sivanappan Vasanthi (NTT) <GopiSivanappan.Vasanthi@cps.gov.uk>
2026-05-14 15:24:15 +0100

    Modified .netversion for terraforms

--- polaris-terraform/main-terraform/function-text-extractor-staging.tf
+++ polaris-terraform/main-terraform/function-text-extractor-staging.tf
@@ -51,7 +51,7 @@ resource "azurerm_linux_function_app_slot" "fa_text_extractor_staging1" {
     health_check_eviction_time_in_min      = "2"
     use_32_bit_worker                      = false
     application_stack {
-      dotnet_version              = "8.0"
+      dotnet_version              = "10.0"
       use_dotnet_isolated_runtime = true
     }
   }
