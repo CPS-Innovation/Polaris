cbc40693c4d07dcca92250f19c0607bac4227f79
Kesh Ramduny <Keshav.Ramduny@cps.gov.uk>
2026-07-14 14:51:07 +0100

    FCT2-19721 - Fix for Redaction converting PDF to image and reducing quality.

--- polaris-pipeline/pdf-redactor/Services/DocumentRedaction/Aspose/RedactionImplementations/DirectImplementation.cs
+++ polaris-pipeline/pdf-redactor/Services/DocumentRedaction/Aspose/RedactionImplementations/DirectImplementation.cs
@@ -1,26 +1,39 @@
 using Aspose.Pdf;
 using Aspose.Pdf.Annotations;
 
-namespace pdf_redactor.Services.DocumentRedaction.Aspose.RedactionImplementations
+namespace pdf_redactor.Services.DocumentRedaction.Aspose.RedactionImplementations;
+
+public class DirectImplementation : IRedactionImplementation
 {
-    public class DirectImplementation : IRedactionImplementation
+    public void AttachAnnotation(Page page, Rectangle rect)
     {
-        public void AttachAnnotation(Page page, Rectangle rect)
+        var redaction = new RedactionAnnotation(page, rect)
         {
-            var hardAnnotation = new RedactionAnnotation(page, rect)
+            FillColor = Color.Black,
+            Color = Color.Black,
+        };
+
+        page.Annotations.Add(redaction, true);
+    }
+
+    public void FinaliseAnnotations(ref Document document, Guid correlationId)
+    {
+        foreach (Page page in document.Pages)
+        {
+            var redactions = page.Annotations
+                .Where(a => a.AnnotationType == AnnotationType.Redaction)
+                .Cast<RedactionAnnotation>()
+                .ToList();
+
+            foreach (var redaction in redactions)
             {
-                FillColor = Color.Black,
-            };
-
-            page.Annotations.Add(hardAnnotation, true);
-            hardAnnotation.Redact();
+                redaction.Redact();
+            }
         }
+    }
 
-        public void FinaliseAnnotations(ref Document doc, Guid correlationId) { }
-
-        public (ProviderType, string?) GetProviderType()
-        {
-            return (ProviderType.DirectRedaction, null);
-        }
+    public (ProviderType, string?) GetProviderType()
+    {
+        return (ProviderType.DirectRedaction, null);
     }
 }
