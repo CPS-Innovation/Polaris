bfd585ca587c1cc55834606fcaf4b133568c7286
Daniel Barber <daniel.barber@cps.gov.uk>
2026-07-16 13:51:29 +0100

    updated staging slot with QA updates

--- polaris-terraform/main-terraform/function-gateway-staging.tf
+++ polaris-terraform/main-terraform/function-gateway-staging.tf
@@ -77,7 +77,8 @@ resource "azurerm_linux_function_app_slot" "fa_polaris_staging1" {
         "https://as-web-${local.global_resource_name}.azurewebsites.net",
         "https://${local.global_resource_name}-cmsproxy.azurewebsites.net",
         "https://${local.global_resource_name}-notprod.cps.gov.uk",
-        var.env == "dev" ? "https://cps-tst.outsystemsenterprise.com" : "",
+        var.env == "qa" ? "https://cps-tst.outsystemsenterprise.com" : "",
+        var.env == "qa" ? "https://cps-tst1.outsystemsenterprise.com" : "",
         var.env == "dev" ? "http://localhost:3000" : ""
       ]
       support_credentials = true
