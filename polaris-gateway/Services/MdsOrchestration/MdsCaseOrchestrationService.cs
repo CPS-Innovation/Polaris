846ebb021cae2a6a85854c94dd893e2da22cb845
Gopi Sivanappan Vasanthi (NTT) <GopiSivanappan.Vasanthi@cps.gov.uk>
2026-07-22 04:46:37 +0100

    All PR feedback implemented

--- polaris-gateway/Services/MdsOrchestration/MdsCaseOrchestrationService.cs
+++ polaris-gateway/Services/MdsOrchestration/MdsCaseOrchestrationService.cs
@@ -1,81 +1,87 @@
-using Common.Dto.Request;
-using Common.Dto.Request.HouseKeeping;
-using Common.Dto.Response;
-using Common.Dto.Response.Case;
-using Ddei.Domain.CaseData.Args.Core;
-using Ddei.Factories;
-using Ddei.Mappers;
-using DdeiClient.Clients.Interfaces;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace PolarisGateway.Services.MdsOrchestration;
-
-public class MdsCaseOrchestrationService(IMdsClient mdsClient,
-                            IMasterDataServiceClient masterDataServiceClient,
-                            IMdsArgFactory mdsArgFactory,
-                            ICaseDetailsMapper caseDetailsMapper) : IMdsCaseOrchestrationService
+// -----------------------------------------------------------------------------
+// <copyright file="MdsCaseOrchestrationService.cs" company="TheCrownProsecutionService">
+// Copyright (c) The Crown Prosecution Service. All rights reserved.
+// </copyright>
+// <summary>
+//     Contains the MdsCaseOrchestrationService implementation.
+// </summary>
+// -----------------------------------------------------------------------------
+namespace PolarisGateway.Services.MdsOrchestration
 {
-    private readonly IMdsClient _mdsClient = mdsClient;
-    private readonly IMasterDataServiceClient _masterDataServiceClient = masterDataServiceClient;
-    private readonly IMdsArgFactory _mdsArgFactory = mdsArgFactory;
-    private readonly ICaseDetailsMapper _caseDetailsMapper = caseDetailsMapper;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Common.Dto.Request;
+    using Common.Dto.Request.HouseKeeping;
+    using Common.Dto.Response;
+    using Common.Dto.Response.Case;
+    using Ddei.Domain.CaseData.Args.Core;
+    using Ddei.Factories;
+    using Ddei.Mappers;
+    using DdeiClient.Clients.Interfaces;
 
-    public async Task<CaseDto> GetCase(MdsCaseIdentifiersArgDto arg, CancellationToken cancellationToken = default)
+    public class MdsCaseOrchestrationService(IMdsClient mdsClient,
+                                IMasterDataServiceClient masterDataServiceClient,
+                                IMdsArgFactory mdsArgFactory,
+                                ICaseDetailsMapper caseDetailsMapper)
+                                : IMdsCaseOrchestrationService
     {
-        var @case = await GetCaseDetails(arg, cancellationToken);
-        return _caseDetailsMapper.MapCaseDetails(@case);
-    }
+        private readonly IMdsClient mdsClient = mdsClient;
+        private readonly IMasterDataServiceClient masterDataServiceClient = masterDataServiceClient;
+        private readonly IMdsArgFactory mdsArgFactory = mdsArgFactory;
+        private readonly ICaseDetailsMapper caseDetailsMapper = caseDetailsMapper;
 
-    public async Task<IEnumerable<CaseDto>> GetCases(MdsUrnArgDto arg, CancellationToken cancellationToken = default)
-    {
-        var caseIdentifiers = await _mdsClient.ListCaseIdsAsync(arg, cancellationToken);
-
-        var calls = caseIdentifiers.Select(async caseIdentifier =>
-            await GetCaseDetails(_mdsArgFactory.CreateCaseArgFromUrnArg(arg, caseIdentifier.Id), cancellationToken));
-
-        var cases = await Task.WhenAll(calls);
-        return cases.Select(@case => _caseDetailsMapper.MapCaseDetails(@case));
-    }
-
-    private async Task<CaseDetailsDto> GetCaseDetails(MdsCaseIdentifiersArgDto arg, CancellationToken cancellationToken = default)
-    {
-        var caseSummaryRequest = new GetCaseSummaryRequest(arg.CaseId, arg.CorrelationId);
-
-        var cmsAuthValues = new CmsAuthValues(arg.CmsAuthValues, arg.CorrelationId);
-
-        var getCaseSummaryTask = _masterDataServiceClient.GetCaseSummaryAsync(caseSummaryRequest, cmsAuthValues, cancellationToken);
-        var witnessesTask = _mdsClient.GetWitnessesAsync(arg, cancellationToken);
-        var getDefendantsAndChargesTask = _mdsClient.GetDefendantAndChargesAsync(arg, cancellationToken);
-        var getPcdRequestTask = _mdsClient.GetPcdRequestsAsync(arg, cancellationToken);
-
-        await Task.WhenAll(getCaseSummaryTask, getDefendantsAndChargesTask, witnessesTask, getPcdRequestTask);
-
-        var summarynew = getCaseSummaryTask.Result;
-        var summary = new CaseSummaryDto()
+        public async Task<CaseDto> GetCase(MdsCaseIdentifiersArgDto arg, CancellationToken cancellationToken = default)
         {
-           Urn = summarynew.Urn,
-           LeadDefendantFirstNames = summarynew.LeadDefendantFirstNames,
-           LeadDefendantSurname = summarynew.LeadDefendantSurname,
-           NumberOfDefendants = summarynew.NumberOfDefendants,
-        }; 
-        var defendantsAndCharges = getDefendantsAndChargesTask.Result.DefendantsAndCharges;
-        var witnesses = MapWitnesses(witnessesTask.Result);
-        var preChargeDecisionRequests = getPcdRequestTask.Result;
+            var @case = await this.GetCaseDetails(arg, cancellationToken);
+            return this.caseDetailsMapper.MapCaseDetails(@case);
+        }
 
-        return new CaseDetailsDto
+        public async Task<IEnumerable<CaseDto>> GetCases(MdsUrnArgDto arg, CancellationToken cancellationToken = default)
         {
-            Summary = summary,
-            DefendantsAndCharges = defendantsAndCharges,
-            Witnesses = witnesses,
-            PreChargeDecisionRequests = preChargeDecisionRequests,
-        };
+            var caseIdentifiers = await this.mdsClient.ListCaseIdsAsync(arg, cancellationToken);
+
+            var calls = caseIdentifiers.Select(async caseIdentifier =>
+                await this.GetCaseDetails(this.mdsArgFactory.CreateCaseArgFromUrnArg(arg, caseIdentifier.Id), cancellationToken));
+
+            var cases = await Task.WhenAll(calls);
+            return cases.Select(@case => this.caseDetailsMapper.MapCaseDetails(@case));
+        }
+
+        private async Task<CaseDetailsDto> GetCaseDetails(MdsCaseIdentifiersArgDto arg, CancellationToken cancellationToken = default)
+        {
+            var caseSummaryRequest = new GetCaseSummaryRequest(arg.CaseId, arg.CorrelationId);
+
+            var cmsAuthValues = new CmsAuthValues(arg.CmsAuthValues, arg.CorrelationId);
+
+            var getCaseSummaryTask = this.masterDataServiceClient.GetCaseSummaryAsync(caseSummaryRequest, cmsAuthValues, cancellationToken);
+            var witnessesTask = this.mdsClient.GetWitnessesAsync(arg, cancellationToken);
+            var getDefendantsAndChargesTask = this.mdsClient.GetDefendantAndChargesAsync(arg, cancellationToken);
+            var getPcdRequestTask = this.mdsClient.GetPcdRequestsAsync(arg, cancellationToken);
+
+            var caseSummaryResponse = await getCaseSummaryTask;
+            var summary = new CaseSummaryDto()
+            {
+                Urn = caseSummaryResponse.Urn,
+                LeadDefendantFirstNames = caseSummaryResponse.LeadDefendantFirstNames,
+                LeadDefendantSurname = caseSummaryResponse.LeadDefendantSurname,
+                NumberOfDefendants = caseSummaryResponse.NumberOfDefendants,
+            };
+            var defendantsAndCharges = await getDefendantsAndChargesTask;
+            var witnesses = this.MapWitnesses(await witnessesTask);
+            var preChargeDecisionRequests = await getPcdRequestTask;
+
+            return new CaseDetailsDto
+            {
+                Summary = summary,
+                DefendantsAndCharges = defendantsAndCharges.DefendantsAndCharges,
+                Witnesses = witnesses,
+                PreChargeDecisionRequests = preChargeDecisionRequests,
+            };
+        }
+
+        private IEnumerable<WitnessDto> MapWitnesses(IEnumerable<BaseCaseWitnessResponse> witnesses) =>
+            this.caseDetailsMapper.MapWitnesses(witnesses);
     }
-
-    private IEnumerable<WitnessDto> MapWitnesses(IEnumerable<BaseCaseWitnessResponse> witnesses) =>
-        _caseDetailsMapper.MapWitnesses(witnesses);
-
-
 }
