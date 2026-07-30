55f437e7414c607e9e52cfa64b33613a6772e8cd
Gayatri <gayatri.patel@cps.gov.uk>
2026-07-30 16:34:49 +0100

    FCT 20780 - Creating text-extractor endpoints without URN

---
+++ polaris-pipeline/text-extractor/Functions/CaseIndexCount.cs
@@ -0,0 +1,48 @@
+using Common.Configuration;
+using Common.Extensions;
+using Common.Handlers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using text_extractor.Services.CaseSearchService;
+
+namespace text_extractor.Functions
+{
+    /// <summary>
+    /// Represents a function that retrieves the count of indexed entries for a specific case based on the provided case ID. 
+    /// This function is designed to be triggered by an HTTP GET request and is intended to be accessed via the Housekeeping UI front-end.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="CaseIndexCount"/> class.
+    /// </remarks>
+    /// <param name="logger">The logger instance used to log information and errors.</param>
+    /// <param name="searchIndexService">The service used to get case index count for a specified case.</param>
+    /// <param name="exceptionHandler">Handler to manage exceptions.</param>
+    public class CaseIndexCount(ILogger<CaseIndexCount> logger,
+        ISearchIndexService searchIndexService,
+        IExceptionHandler exceptionHandler) : BaseFunction
+    {
+        private readonly ILogger<CaseIndexCount> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        private readonly ISearchIndexService _searchIndexService = searchIndexService ?? throw new ArgumentNullException(nameof(searchIndexService));
+        private readonly IExceptionHandler _exceptionHandler = exceptionHandler ?? throw new ArgumentNullException(nameof(exceptionHandler));
+        private const string LoggingName = nameof(CaseIndexCount);
+
+        [Function(nameof(CaseIndexCount))]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = RestApi.CaseIndexCount)] HttpRequest request, int caseId)
+        {
+            var correlationId = request.Headers.GetCorrelationId();
+
+            try
+            {
+                var result = await _searchIndexService.GetCaseIndexCount(caseId, correlationId);
+
+                return CreateJsonResult(result);
+            }
+            catch (Exception exception)
+            {
+                return _exceptionHandler.HandleExceptionNew(exception, correlationId, LoggingName, _logger);
+            }
+        }
+    }
+}
---
+++ polaris-pipeline/text-extractor/Functions/StoreCaseIndexes.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.Extensions.Logging;
+using Common.Configuration;
+using Common.Dto.Response;
+using Common.Exceptions;
+using Common.Extensions;
+using Common.Handlers;
+using Common.Wrappers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using text_extractor.Services.CaseSearchService;
+
+namespace text_extractor.Functions
+{
+    /// <summary>
+    /// Represents a function that stores OCR results for a specific case, material, and document based on the provided case ID, material ID, and document ID.
+    /// This function is designed to be triggered by an HTTP POST request and is intended to be accessed via the Housekeeping UI front-end.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="StoreCaseIndexes"/> class.
+    /// </remarks>
+    /// <param name="searchIndexService">The service used to store OCR results for a specific case, material, and document.</param>
+    /// <param name="exceptionHandler">Handler to manage exceptions.</param>
+    /// <param name="logger">The logger instance used to log information and errors.</param>
+    /// <param name="jsonConvertWrapper">JSON wrapper to handle serialization.</param>
+    public class StoreCaseIndexes(
+           ISearchIndexService searchIndexService,
+           IExceptionHandler exceptionHandler,
+           ILogger<StoreCaseIndexes> logger,
+           IJsonConvertWrapper jsonConvertWrapper) : BaseFunction
+    {
+        private readonly ISearchIndexService _searchIndexService = searchIndexService;
+        private readonly IExceptionHandler _exceptionHandler = exceptionHandler;
+
+        private readonly ILogger<StoreCaseIndexes> _log = logger;
+        private readonly IJsonConvertWrapper _jsonConvertWrapper = jsonConvertWrapper;
+        private const string LoggingName = "StoreCaseIndexes - Run";
+
+        [Function(nameof(StoreCaseIndexes))]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = RestApi.Extract)] HttpRequest request,
+            int caseId, string materialId, long documentId)
+        {
+            Guid currentCorrelationId = default;
+            try
+            {
+                currentCorrelationId = request.Headers.GetCorrelationId();
+
+                if (request.Body == null)
+                {
+                    throw new BadRequestException("Request body has no content", nameof(request));
+                }
+
+                var streamReader = new StreamReader(request.Body);
+                var content = await streamReader.ReadToEndAsync();
+                var ocrResults = _jsonConvertWrapper.DeserializeObject<AnalyzeResults>(content);
+
+                var storedLinesCount = await _searchIndexService.SendStoreResultsAsync
+                    (
+                        ocrResults,
+                        caseId,
+                        materialId,
+                        documentId,
+                        currentCorrelationId
+                    );
+
+                var result = new StoreCaseIndexesResult
+                {
+                    IsSuccess = true,
+                    IndexStoredTime = DateTime.UtcNow,
+                    LineCount = storedLinesCount
+                };
+
+                return CreateJsonResult(result);
+            }
+            catch (Exception exception)
+            {
+                return _exceptionHandler.HandleExceptionNew(exception, currentCorrelationId, LoggingName, _log, new StoreCaseIndexesResult { IsSuccess = false });
+            }
+        }
+    }
+}
