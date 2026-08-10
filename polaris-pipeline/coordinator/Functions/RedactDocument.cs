2ce0fa209cf80410db21153885b4bc217316a958
Tahmeed Ahmed (NTT) <Tahmeed.Ahmed@cps.gov.uk>
2026-08-10 16:56:52 +0100

    created new endpoints in coordinator app

---
+++ polaris-pipeline/coordinator/Functions/RedactDocument.cs
@@ -0,0 +1,157 @@
+// <copyright file="RedactDocument.cs" company="TheCrownProsecutionService">
+// Copyright (c) The Crown Prosecution Service. All rights reserved.
+// </copyright>
+
+namespace coordinator.Functions;
+
+using Common.Configuration;
+using Common.Domain.Document;
+using Common.Dto.Request;
+using Common.Exceptions;
+using Common.Extensions;
+using Common.Services.BlobStorage;
+using coordinator.Clients.PdfRedactor;
+using Ddei.Factories;
+using DdeiClient.Clients.Interfaces;
+using DdeiClient.Services.CaseUrnResolver;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class RedactDocument
+{
+    private readonly IValidator<RedactPdfRequestWithDocumentDto> requestValidator;
+    private readonly IPdfRedactorClient redactionClient;
+    private readonly IPolarisBlobStorageService polarisBlobStorageService;
+    private readonly IMdsArgFactory mdsArgFactory;
+    private readonly IMdsClient mdsClient;
+    private readonly ICaseUrnResolver caseUrnResolver;
+
+    public RedactDocument(
+        IValidator<RedactPdfRequestWithDocumentDto> requestValidator,
+        IPdfRedactorClient redactionClient,
+        Func<string, IPolarisBlobStorageService> blobStorageServiceFactory,
+        IMdsArgFactory mdsArgFactory,
+        IConfiguration configuration,
+        IMdsClient mdsClient,
+        ICaseUrnResolver caseUrnResolver)
+    {
+        this.requestValidator = requestValidator.ExceptionIfNull();
+        this.redactionClient = redactionClient.ExceptionIfNull();
+        this.polarisBlobStorageService = blobStorageServiceFactory(configuration[StorageKeys.BlobServiceContainerNameDocuments] ?? string.Empty).ExceptionIfNull();
+        this.mdsArgFactory = mdsArgFactory.ExceptionIfNull();
+        this.mdsClient = mdsClient.ExceptionIfNull();
+        this.caseUrnResolver = caseUrnResolver.ExceptionIfNull();
+    }
+
+    [Function(nameof(RedactDocument))]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> HttpStart(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = RestApi.RedactDocument)]
+        HttpRequest req,
+        int caseId,
+        string materialId,
+        long documentId,
+        CancellationToken cancellationToken)
+    {
+        var currentCorrelationId = req.Headers.GetCorrelationId();
+        CmsAuthValues cmsAuthValues = req.BuildCmsAuthValues();
+
+        var caseUrn = await this.caseUrnResolver.ResolveCaseUrnAsync(caseId, cmsAuthValues, cancellationToken);
+
+        var redactPdfRequest = await req.ReadFromJsonAsync<RedactPdfRequestDto>();
+
+        using var documentStream = await this.polarisBlobStorageService.GetBlobAsync(new BlobIdType(caseId, materialId, documentId, BlobType.Pdf));
+
+        using var memoryStream = new MemoryStream();
+        await documentStream.CopyToAsync(memoryStream);
+        var bytes = memoryStream.ToArray();
+
+        Stream redactedDocumentStream = null;
+
+        if (redactPdfRequest.RedactionDefinitions.Count != 0)
+        {
+            var base64Document = Convert.ToBase64String(bytes);
+
+            var redactionRequest = new RedactPdfRequestWithDocumentDto
+            {
+                Document = base64Document,
+                RedactionDefinitions = redactPdfRequest.RedactionDefinitions,
+            };
+
+            var validationResult = await this.requestValidator.ValidateAsync(redactionRequest);
+            if (!validationResult.IsValid)
+            {
+                throw new BadRequestException(validationResult.FlattenErrors(), nameof(redactPdfRequest));
+            }
+
+            redactedDocumentStream = await this.redactionClient.RedactPdfAsync(caseUrn, caseId, materialId, documentId, redactionRequest, currentCorrelationId);
+            if (redactedDocumentStream == null)
+            {
+                string error = $"Error Saving redaction details to the document for {caseId}, materialId {materialId}";
+                throw new Exception(error);
+            }
+        }
+
+        Stream modifiedDocumentStream = null;
+
+        if (redactPdfRequest.DocumentModifications.Count != 0)
+        {
+            byte[] bytesToModify = null;
+
+            if (redactedDocumentStream != null)
+            {
+                using var redactedMemoryStream = new MemoryStream();
+                await redactedDocumentStream.CopyToAsync(redactedMemoryStream);
+                bytesToModify = redactedMemoryStream.ToArray();
+            }
+            else
+            {
+                bytesToModify = bytes;
+            }
+
+            var base64DocumentToModify = Convert.ToBase64String(bytesToModify);
+
+            var modificationRequest = new ModifyDocumentWithDocumentDto
+            {
+                Document = base64DocumentToModify,
+                DocumentModifications = redactPdfRequest.DocumentModifications,
+                VersionId = redactPdfRequest.VersionId,
+            };
+
+            modifiedDocumentStream = await this.redactionClient.ModifyDocument(caseUrn, caseId, materialId, documentId, modificationRequest, currentCorrelationId);
+            if (modifiedDocumentStream == null)
+            {
+                string error = $"Error modifying document for {caseId}, materialId {materialId}";
+                throw new Exception(error);
+            }
+        }
+
+        var arg = this.mdsArgFactory.CreateDocumentVersionArgDto(
+            cmsAuthValues.CmsAuthFullValue,
+            correlationId: currentCorrelationId,
+            caseUrn,
+            caseId: caseId,
+            DocumentNature.ToNumericDocumentId(materialId, DocumentNature.Types.Document),
+            documentId);
+
+        var ddeiResult = await this.mdsClient.UploadPdfAsync(arg, modifiedDocumentStream ?? redactedDocumentStream);
+
+        if (ddeiResult.StatusCode == HttpStatusCode.Gone || ddeiResult.StatusCode == HttpStatusCode.RequestEntityTooLarge)
+        {
+            return new StatusCodeResult((int)ddeiResult.StatusCode);
+        }
+
+        return new OkResult();
+    }
+}
---
+++ polaris-pipeline/coordinator/Functions/RefreshCase.cs
@@ -0,0 +1,75 @@
+﻿// <copyright file="RefreshCase.cs" company="TheCrownProsecutionService">
+// Copyright (c) The Crown Prosecution Service. All rights reserved.
+// </copyright>
+
+namespace coordinator.Functions;
+
+using Common.Configuration;
+using Common.Dto.Request;
+using Common.Extensions;
+using coordinator.Domain;
+using coordinator.Durable.Payloads;
+using coordinator.Durable.Providers;
+using Ddei.Factories;
+using DdeiClient.Clients.Interfaces;
+using DdeiClient.Services.CaseUrnResolver;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class RefreshCase(
+    IOrchestrationProvider orchestrationProvider,
+    IMdsArgFactory mdsArgFactory,
+    IDdeiAuthClient ddeiAuthClient,
+    ICaseUrnResolver caseUrnResolver)
+{
+    [Function(nameof(RefreshCase))]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status423Locked)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Run
+    (
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = RestApi.Case)] HttpRequest req,
+        int caseId,
+        CancellationToken cancellationToken,
+        [DurableClient] DurableTaskClient orchestrationClient
+    )
+    {
+        var currentCorrelationId = req.Headers.GetCorrelationId();
+        CmsAuthValues cmsAuthValues = req.BuildCmsAuthValues();
+
+        var caseUrn = await caseUrnResolver.ResolveCaseUrnAsync(caseId, cmsAuthValues, cancellationToken);
+
+        // #28217 - in this case we need to pre-emptively check the CMS auth values.  The policy
+        //  as it stands when calling DDEI is that we do not check beforehand and let the call itself fail
+        //  if auth is missing/expired. One rationale for this is that checking auth could be an expensive operation
+        // (it probably isn't though). Also we do try to not create traffic over and above that which the user would
+        //  have created if she were using CMS to do the same work.
+        //  In this case, when triggering an orchestration we hand over the auth to the durable process. It
+        //  is only when the client does the follow-up polling calls do things blow up.  On balance, the cleanest
+        //  thing to do is to check the auth values here.  If they blow up then the client can send the user round
+        //  the reauth loop at this point. It is much more difficult to recover if the blow-up occurs later on in polling
+        //  (especially in the in-situ reauth flow).
+        //  This effect is exacerbated by the fact that after #23763 we start to regularly call this operation over and above
+        //  case load and after mutations.
+        //  However this code will be refactored out as part of #28158 so ¯\_(ツ)_/¯
+        //  VerifyCmsAuthAsync will throw an exception if the auth values are invalid, and the HandleUnhandledException
+        //  process will deal with translating to a 401 Unauthorized response.
+        await ddeiAuthClient.VerifyCmsAuthAsync(
+            mdsArgFactory.CreateCmsCaseDataArgDto(cmsAuthValues.CmsAuthFullValue, currentCorrelationId));
+
+        var casePayload = new CasePayload(caseUrn, caseId, cmsAuthValues.CmsAuthFullValue, currentCorrelationId);
+
+        var isAccepted = await orchestrationProvider.RefreshCaseAsync(orchestrationClient, currentCorrelationId, caseId, casePayload, req);
+
+        return new ObjectResult(new RefreshCaseResponse(caseUrn, caseId))
+        {
+            StatusCode = isAccepted ? StatusCodes.Status200OK : StatusCodes.Status423Locked,
+        };
+    }
+}
