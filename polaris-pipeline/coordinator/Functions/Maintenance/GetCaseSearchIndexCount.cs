ab3216ea855f9e36a928866a3848cbbf54e7d9d3
TahmeedAhmed-NTT-CPS <Tahmeed.Ahmed@cps.gov.uk>
2026-08-13 09:44:31 +0100

    created new endpoints in coordinator app (#2145)
    * created new endpoints in coordinator app
    
    * sonarqube issues addressed
    
    * refactor exception for sonarqube

---
+++ polaris-pipeline/coordinator/Functions/Maintenance/GetCaseSearchIndexCount.cs
@@ -0,0 +1,36 @@
+// <copyright file="GetCaseSearchIndexCount.cs" company="TheCrownProsecutionService">
+// Copyright (c) The Crown Prosecution Service. All rights reserved.
+// </copyright>
+
+namespace coordinator.Functions.Maintenance;
+
+using System.Threading.Tasks;
+using Common.Configuration;
+using Common.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using coordinator.Clients.TextExtractor;
+using Microsoft.Azure.Functions.Worker;
+using DdeiClient.Services.CaseUrnResolver;
+using Common.Dto.Request;
+using System.Threading;
+
+public class GetCaseSearchIndexCount(ITextExtractorClient textExtractorClient, ICaseUrnResolver caseUrnResolver)
+{
+    [Function(nameof(GetCaseSearchIndexCount))]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> HttpStart([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = RestApi.CaseSearchCount)] HttpRequest req, int caseId, CancellationToken cancellationToken)
+    {
+        var currentCorrelationId = req.Headers.GetCorrelationId();
+
+        CmsAuthValues cmsAuthValues = req.BuildCmsAuthValues();
+
+        var caseUrn = await caseUrnResolver.ResolveCaseUrnAsync(caseId, cmsAuthValues, cancellationToken);
+
+        var searchIndexCount = await textExtractorClient.GetCaseIndexCount(caseUrn, caseId, currentCorrelationId);
+
+        return new OkObjectResult(searchIndexCount);
+    }
+}
