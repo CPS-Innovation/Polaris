8525804fbc4ce4ea2728b20e5cac1a4558a45740
Tahmeed Ahmed (NTT) <Tahmeed.Ahmed@cps.gov.uk>
2026-09-02 11:10:20 +0100

    revert cancellation token removal

--- polaris-gateway/Functions/GetPdf.cs
+++ polaris-gateway/Functions/GetPdf.cs
@@ -68,7 +68,7 @@ public class GetPdf : BaseFunction
             IsOcrProcessed: isOcrProcessed,
             ForceRefresh: forceRefresh);
 
-        var getPdfResult = await this.pdfArtefactService.GetPdfAsync(request, cmsAuthValues.CmsAuthFullValue, correlationId, isLegacy: false);
+        var getPdfResult = await this.pdfArtefactService.GetPdfAsync(request, cmsAuthValues.CmsAuthFullValue, correlationId, isLegacy: false, cancellationToken);
 
         if (getPdfResult.FileSizeExceedsLimit == true)
         {
