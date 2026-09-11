39898e4868a889efa799d1b6142dd5f6abe26e5f
Daniel Barber <daniel.barber@cps.gov.uk>
2026-09-11 15:11:32 +0100

    removal of Commit 9bfd9b8

--- polaris-terraform/main-terraform/cmsenv.js
+++ polaris-terraform/main-terraform/cmsenv.js
@@ -166,8 +166,6 @@ function __getCmsEnvInternal(cookie) {
     if (cookie.includes("cin2")) return "cin2";
     if (cookie.includes("cin4")) return "cin4";
     if (cookie.includes("cin5")) return "cin5";
-    if (cookie.includes("cpt")) return "cpt";
-    if (cookie.includes("mod")) return "cmo";
     return "default";
 }
 
--- polaris-terraform/main-terraform/variables.tf
+++ polaris-terraform/main-terraform/variables.tf
@@ -108,13 +108,6 @@ variable "cms_details" {
     default_upstream_cms_domain_name           = string
     default_upstream_cms_modern_domain_name    = string
     default_upstream_cms_services_domain_name  = string
-    cpt_upstream_cms_ip_corsham                = string
-    cpt_upstream_cms_modern_ip_corsham         = string
-    cpt_upstream_cms_ip_farnborough            = string
-    cpt_upstream_cms_modern_ip_farnborough     = string
-    cpt_upstream_cms_domain_name               = string
-    cpt_upstream_cms_modern_domain_name        = string
-    cpt_upstream_cms_services_domain_name      = string
     cin2_upstream_cms_ip_corsham               = string
     cin2_upstream_cms_modern_ip_corsham        = string
     cin2_upstream_cms_ip_farnborough           = string
@@ -384,8 +377,8 @@ variable "coordinator" {
 
 variable "cps_global_components" {
   type = object({
-    script_url          = string
-    blob_storage_domain = string
+    script_url      = string
+    blob_storage_domain  = string
   })
 }
 
